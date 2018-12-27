31dee283d4b45fcb1197811e62b146abd16f04ea
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2018-12-27 14:08:50 +0000

    fixed issue #4

---
+++ commands/purge.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args, color) => {
+  let language = require(`../messages/messages_en-US.json`);
+
+  if((!message.member.hasPermission("MANAGE_MESSAGES") && (message.author.id !== '151516555757223936'))) {
+    message.channel.send(`${language["purge"].noPermission}`);
+    return;
+  }
+
+  const argresult = args.join(" ");
+  if (!argresult) {
+    message.channel.send(`${language["purge"].notSpecified}`);
+    return;
+  }
+  if (isNaN(argresult)) {
+    message.channel.send(`${language["purge"].invalidNumber}`);
+    return;
+  }
+  if (args[0] > 100) {
+    message.channel.send(`${language["purge"].limitNumber}`);
+    return;
+  }
+
+  let messagecount = parseInt(args.join(" "));
+  message.channel.bulkDelete(messagecount).then(() => {
+    setTimeout(() => {
+      let purgedMessage = language["purge"].purged;
+      const purged = purgedMessage.replace("${messages}", messagecount);
+
+      message.channel.send(`${purged}`).then(m => {
+        setTimeout(() => {
+          m.delete();
+        }, 5000);
+      });
+    }, 2000);
+  });
+};
+
+module.exports.help = {
+  name: "purge"
+};
