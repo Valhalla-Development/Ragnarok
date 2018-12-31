5a136b51168131f45b3913d38dedb5457f33a32e
Ragnar Lothbrok <hudsonjd@live.co.uk>
2018-12-31 18:23:58 +0000

    Complete re-code

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,56 +1,41 @@
 const Discord = require("discord.js");
+const ms = require("ms");
 
 module.exports.run = async (client, message, args, color) => {
-  let language = require(`../messages/messages_en-US.json`);
+    let language = require(`../messages/messages_en-US.json`);
 
-  if (message.mentions.users.size < 1) {
-    message.channel.send(`${language["unmute"].noMention}`).then(msg => {
-      msg.delete(10000);
-    });
-    return;
-  }
+    if((!message.member.hasPermission("KICK_MEMBERS") && (message.author.id !== '151516555757223936'))) {
+        message.channel.send(`${language["mute"].noAuthorPermission}`).then(message => message.delete(5000));;
+        return;
+      }
+    
+      let cnt = message.content
+      if (cnt !== " ") {
+          message.delete(10) // ?
+      };
+      
 
-  if (message.mentions.members.size < 1) {
-    message.channel.send(`${language["unmute"].noMention}`).then(msg => {
-      msg.delete(10000);
-    });
-    return;
-  }
+    const log = message.guild.channels.find(x => x.name === "logs");
+    const mod = message.author;
+    let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if (!user) return message.reply(`${language["mute"].noUser}`).then(message => message.delete(5000));
+    let muterole = message.guild.roles.find(x => x.name === "Muted");
+    let muteChannel = message.guild.channels.find(x => x.name === "logs");
+    if (!muteChannel) return message.guild.createChannel("logs").then(channel => {
+        channel.setTopic(`Log channel`).then(message.channel.send(`${language["mute"].createdChannel}`).then(message => message.delete(5000)));
+      });
 
-  let user = message.mentions.users.first();
-  let member = message.mentions.members.first();
-  if((!message.member.hasPermission("MANAGE_GUILD") && (message.author.id !== '151516555757223936'))) {
-    message.channel.send(`${language["unmute"].noPermission}`).then(msg => {
-      msg.delete(10000);
-    });
-    return;
-  }
+    await (user.removeRole(muterole.id));
+    const unmuteembed = new Discord.RichEmbed()
+        .setAuthor(' Action | Un-Mute', `http://odinrepo.tk/speaker.png`)
+        .addField('User', `<@${user.id}>`)
+        .addField('Staff Member', `${mod}`)
+        .setColor("#ff0000")
+    log.send(unmuteembed)
 
-  const mutedRole = message.guild.roles.find("name", "Muted");
-
-  if (!mutedRole) {
-    message.channel.send(`${language["unmute"].noMutedRole}`).then(msg => {
-      msg.delete(10000);
-    });
-  }
-
-  if (message.member.roles.has(mutedRole.id)) {
-    member.removeRole(mutedRole.id).then(() => {
-      setTimeout(() => {
-        let unmutedMessage = language["unmute"].unmuted;
-        const unmuted = unmutedMessage.replace("${user}", user.tag);
-
-        message.channel.send(`${unmuted}`);
-      }, 1000);
-    });
-  } else {
-    message.channel.send(`${language["unmute"].noRoleAsigned}`).then(msg => {
-      msg.delete(10000);
-    });
-    return;
-  }
 };
 
+
 module.exports.help = {
-  name: "unmute"
+    name: "unmute"
 };
