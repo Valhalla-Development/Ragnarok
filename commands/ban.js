d474f4e95da42890988204ca7a4cc7dd71c09c87
Ragnar Lothbrok <hudsonjd@live.co.uk>
2019-01-16 01:53:44 +0000

    no longer forcefully creates a channel

--- commands/ban.js
+++ commands/ban.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 const config = JSON.parse(
   fs.readFileSync("./Storage/config.json", "utf8")
 );
+const SQLite = require('better-sqlite3')
+const db = new SQLite('./Storage/db/db.sqlite');
 
 module.exports.run = async (client, message, args, color) => {
   let language = require(`../messages/messages_en-US.json`);
@@ -19,30 +21,53 @@ module.exports.run = async (client, message, args, color) => {
     message.delete(10) // ?
   };
 
-  let logs = message.guild.channels.find(x => x.name === "logs");
-  if (!logs) return message.guild.createChannel("logs").then(channel => {
-    channel.setTopic(`Log channel`).then(message.channel.send(`${language["ban"].channelcreated}`).then(message => message.delete(5000)));
-  });
+  const id = db.prepare(`SELECT channel FROM logging WHERE guildid = ${message.guild.id};`).get();
+  if (!id) {
+    let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if (!user) return message.reply(`${language["ban"].notarget}`).then(message => message.delete(5000));
 
-  let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if (!user) return message.reply(`${language["ban"].notarget}`).then(message => message.delete(5000));
+    let reason = args.slice(1).join(' ');
+    if (!reason) reason = "No reason given";
 
-  let reason = args.slice(1).join(' ');
-  if (!reason) reason = "No reason given";
+    message.guild.member(user).ban(reason);
 
-  message.guild.member(user).ban(reason);
+    let logsEmbed = new Discord.RichEmbed()
+      .setTitle("User Banned")
+      .setFooter("User Ban Logs")
+      .setColor("#ff0000")
+      .setTimestamp()
+      .addField("Banned User:", `${user}, ID: ${user.id}`)
+      .addField("Reason:", reason)
+      .addField("Moderator:", `${message.author}, ID: ${message.author.id}`)
+      .addField("Time:", message.createdAt)
 
-  let logsEmbed = new Discord.RichEmbed()
-    .setTitle("User Banned")
-    .setFooter("User Ban Logs")
-    .setColor("#ff0000")
-    .setTimestamp()
-    .addField("Banned User:", `${user}, ID: ${user.id}`)
-    .addField("Reason:", reason)
-    .addField("Moderator:", `${message.author}, ID: ${message.author.id}`)
-    .addField("Time:", message.createdAt)
+    message.channel.send(logsEmbed);
 
-  logs.send(logsEmbed);
+  } else {
+    const logch = id.channel
+
+    let logs = client.channels.get(logch)
+
+    let chuser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if (!chuser) return message.reply(`${language["ban"].notarget}`).then(message => message.delete(5000));
+
+    let chreason = args.slice(1).join(' ');
+    if (!chreason) reason = "No reason given";
+
+    message.guild.member(user).ban(reason);
+
+    let logsEmbed = new Discord.RichEmbed()
+      .setTitle("User Banned")
+      .setFooter("User Ban Logs")
+      .setColor("#ff0000")
+      .setTimestamp()
+      .addField("Banned User:", `${chuser}, ID: ${chuser.id}`)
+      .addField("Reason:", chreason)
+      .addField("Moderator:", `${message.author}, ID: ${message.author.id}`)
+      .addField("Time:", message.createdAt)
+
+      client.channels.get(logs).send(logembed);
+    };
 };
 
 module.exports.help = {
