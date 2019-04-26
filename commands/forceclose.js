c1144dee80e4b49e76b736086e0a95b260662f3a
Jaylen Baxter <jaylen.baxter@outlook.com>
2019-04-25 18:55:57 -0600

    Rewrote 'forceclose' with a better awaitMessages() timer. It can also now be run globally throughout the server.

--- commands/forceclose.js
+++ commands/forceclose.js
@@ -1,15 +1,13 @@
 const Discord = require("discord.js");
-const ms = require("ms");
 const SQLite = require('better-sqlite3');
 const db = new SQLite('./Storage/db/db.sqlite');
 
 module.exports.run = async (client, message, args, color) => {
+  await message.delete();
   let language = require(`../messages/messages_en-US.json`);
 
-
-  message.delete();
-
-  const modRole = message.guild.roles.find(r => ["Support Team"].includes(r.name));
+  const suppRole = db.prepare(`SELECT role FROM ticketConfig WHERE guildid = ${message.guild.id}`).get();
+  const modRole = message.guild.roles.find(x => x.name === "Support Team") || message.guild.roles.find(r => r.id === suppRole.role);
   if (!modRole) {
     let nomodRole = new Discord.RichEmbed()
       .setColor(`36393F`)
@@ -18,7 +16,7 @@ module.exports.run = async (client, message, args, color) => {
     return;
   }
 
-  if (!message.member.roles.has(modRole.id)) {
+  if (!message.member.roles.has(modRole.id) && message.author.id !== message.guild.ownerID) {
     let donthaveroleMessage = language.tickets.donthaveRole;
     const role = donthaveroleMessage.replace(
       "${role}",
@@ -31,46 +29,43 @@ module.exports.run = async (client, message, args, color) => {
     return;
   }
 
-  if (!message.channel.name.startsWith(`ticket-`)) {
-    let badChannel = new Discord.RichEmbed()
-      .setColor(`36393F`)
-      .setDescription(`${language.tickets.wrongChannelClose}`);
-    message.channel.send(badChannel);
-    return;
-  }
-
-  if (message.channel.name.startsWith("ticket-")) {
-    let fclosecoll = args.join(" ");
+  let channelArgs = message.channel.name.split('-');
+  let foundTicket = db.prepare(`SELECT * FROM tickets WHERE guildid = ${message.guild.id} AND ticketid = (@ticketid)`).get({ticketid: args[0] || channelArgs[channelArgs.length - 1]});
+  if (foundTicket) {
+    const getChan = message.guild.channels.find(chan => chan.id === foundTicket.chanid);
     let forceclosetimer = new Discord.RichEmbed()
-    .setColor(`36393F`)
-    .setTitle(':x: Closing Ticket! :x:')
-    .setDescription(`${language.tickets.closeTimer}`);
-    message.channel.send(forceclosetimer);
-        message.channel
-        .awaitMessages(response => response.author.id === message.author.id, {
-            max: 1,
-            time: 10000,
-            errors: ["time"]
-        }).catch(e => {
-          message.channel.delete();
-          const logget = db.prepare(`SELECT channel FROM ticketlog WHERE guildid = ${message.guild.id};`).get();
-          if (!logget) {
-              return;
-          } else {
-              const logchan = logget.channel;
-              let loggingembed = new Discord.RichEmbed()
-                  .setColor(color)
-                  .setDescription(`<@${message.author.id}> has closed ticket \`#${message.channel.name}\``);
-              client.channels.get(logchan).send(loggingembed);
-              }            
-
-      });        
-
-
-    //setTimeout(function () {
-    //  message.channel.delete();
-    //}, ms('10s'));
-
+      .setColor(`#36393F`)
+      .setTitle(':x: Closing Ticket! :x:')
+      .setDescription(`${language.tickets.closeTimer}`);
+    getChan.send(forceclosetimer).then(timerMsg => {
+      getChan.awaitMessages(resp => resp.author.id === message.author.id || foundTicket.authorid, {
+        max: 1,
+        time: 10000,
+        errors: ['time']
+      }).then(() => {
+        let cancelTimer = new Discord.RichEmbed()
+          .setColor(`#36393F`)
+          .setDescription('Canceling Ticket Close');
+        timerMsg.edit(cancelTimer).then(cancelMsg => {
+          cancelMsg.delete(3500);
+        });
+      }).catch(() => {
+        getChan.delete();
+        db.prepare(`DELETE FROM tickets WHERE guildid = ${message.guild.id} AND ticketid = (@ticketid)`).run({ticketid: foundTicket.ticketid});
+        const logget = db.prepare(`SELECT log FROM ticketConfig WHERE guildid = ${message.guild.id};`).get();
+        const logchan = message.guild.channels.find(chan => chan.id === logget.log);
+        if (!logchan) return;
+        let loggingembed = new Discord.RichEmbed()
+          .setColor(color)
+          .setDescription(`<@${message.author.id}> has closed ticket \`#${message.channel.name}\``);
+        logchan.send(loggingembed);
+      });
+    });
+  } else {
+    let errEmbed = new Discord.RichEmbed()
+      .setColor(`#36393F`)
+      .setDescription('This ticket could not be found.')
+    message.channel.send(errEmbed);
   }
 };
 
