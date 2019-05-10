0cbced805b15a8cd2a7ed9b52ad4922e138b3ace
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2019-05-10 15:38:32 +0100

    (renamed from pleader)

---
+++ commands/leader.js
@@ -0,0 +1,27 @@
+const SQLite = require('better-sqlite3');
+const db= new SQLite('./Storage/db/db.sqlite');
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args, color) => {
+
+
+  const top10 = db.prepare("SELECT * FROM scores WHERE guild = ? ORDER BY points DESC LIMIT 10;").all(message.guild.id);
+  client.getScore = db.prepare("SELECT * FROM scores WHERE user = ? AND guild = ?");
+  client.setScore = db.prepare("INSERT OR REPLACE INTO scores (id, user, guild, points, level) VALUES (@id, @user, @guild, @points, @level);");
+
+  const embed = new Discord.RichEmbed()
+    .setTitle("Leaderboard")
+    .setAuthor(client.user.username, client.user.avatarURL)
+    .setDescription("Top 10 Points!")
+    .setColor(0x00AE86);
+
+  for (const data of top10) {
+    embed.addField(client.users.get(data.user).tag, `${data.points} points (level ${data.level})`);
+  }
+  return message.channel.send({
+    embed
+  });
+};
+module.exports.help = {
+  name: "pleader"
+};
