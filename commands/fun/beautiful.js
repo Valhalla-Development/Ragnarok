76c6befedb4c0295996bbf39cd88aefc1711f658
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2019-05-24 15:00:26 +0100

    new beautify command

---
+++ commands/fun/beautiful.js
@@ -0,0 +1,32 @@
+const { MessageAttachment } = require('discord.js');
+const { idiotToken } = require('../../storage/config.json');
+
+module.exports = {
+	config: {
+		name: 'beautiful',
+		usage: '${prefix}beautiful <@user>',
+		category: 'fun',
+		description: 'Slaps a user',
+		aliases: ['beauty', 'adore'],
+		accessableby: 'Everyone',
+	},
+	run: async (bot, message) => {
+		const Idiot = require('idiotic-api');
+		bot.API = new Idiot.Client(idiotToken, { dev: true });
+
+		message.delete();
+		const user = message.mentions.users.first();
+		if (!user) {
+			message.channel.send('You must tag a user!');
+			return;
+		}
+		await message.channel.send(
+			new MessageAttachment(
+				await bot.API.beautiful(
+					user.displayAvatarURL({ format: 'png', size: 128 })
+				),
+				'beautiful.png'
+			)
+		);
+	},
+};
