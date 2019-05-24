83c2b2a0d257f79cbb8924ac07e74acf7d2a65fb
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2019-05-24 15:00:51 +0100

    new event emmit command

---
+++ commands/owner/emit.js
@@ -0,0 +1,35 @@
+const { MessageEmbed } = require('discord.js');
+const SQLite = require('better-sqlite3');
+const db = new SQLite('./storage/db/db.sqlite');
+const { ownerID } = require('../../storage/config.json');
+
+module.exports = {
+	config: {
+		name: 'emit',
+		usage: '${prefix}emit',
+		category: 'Owner',
+		description: 'Emits an event',
+		accessableby: 'Owner',
+	},
+	run: async (bot, message, args) => {
+		if (message.author.id !== ownerID) return;
+
+		const prefixgrab = db
+			.prepare('SELECT prefix FROM setprefix WHERE guildid = ?')
+			.get(message.guild.id);
+		const prefix = prefixgrab.prefix;
+
+		if (args[0] == undefined) {
+			const noArgs = new MessageEmbed()
+				.setColor('RANDOM')
+				.setDescription(
+					`**Available Commands**:\n\n${prefix}emit guildMemberAdd`
+				);
+			message.channel.send(noArgs);
+		}
+		if (args[0] == 'guildMemberAdd') {
+			bot.emit('guildMemberAdd', message.member);
+			return;
+		}
+	},
+};
