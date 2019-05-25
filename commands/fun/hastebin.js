aa19593574b273aeb4fe3529a78408f36ebe391c
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2019-05-25 13:32:00 +0100

    new hastebin command

---
+++ commands/fun/hastebin.js
@@ -0,0 +1,32 @@
+const { MessageEmbed } = require('discord.js');
+const hastebin = require('hastebin-gen');
+
+module.exports = {
+	config: {
+		name: 'hastebin',
+		usage: '${prefix}hastebin <text>',
+		category: 'fun',
+		description: 'Posts args to hastebin',
+		accessableby: 'Everyone',
+	},
+	run: async (bot, message, args) => {
+		message.delete();
+		if (args[0] == undefined) {
+			const embed = new MessageEmbed()
+				.setColor('RANDOM')
+				.setDescription(':x: | You must input some text');
+			message.channel.send(embed);
+			return;
+		}
+
+		hastebin(args.join(' '))
+			.then(r => {
+				const hastEmb = new MessageEmbed()
+					.setColor('RANDOM')
+					.setURL(r)
+					.addField('Hastebin Link: ', `${r}`);
+				message.channel.send(hastEmb);
+			})
+			.catch(console.error);
+	},
+};
