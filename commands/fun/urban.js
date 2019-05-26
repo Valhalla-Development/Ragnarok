97698ab5ca26f2c8f9a1e1256ed8764f7eeda86b
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2019-05-26 15:03:27 +0100

    new urban command

---
+++ commands/fun/urban.js
@@ -0,0 +1,65 @@
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+const trim = (str, max) =>
+	str.length > max ? `${str.slice(0, max - 3)}...` : str;
+
+module.exports = {
+	config: {
+		name: 'urban',
+		usage: '${prefix}urban <text>',
+		category: 'fun',
+		description: 'Searches Urban Dictionary',
+		accessableby: 'Everyone',
+	},
+	run: async (bot, message, args) => {
+		message.delete();
+		if (!message.channel.nsfw) {
+			const nsfw = new MessageEmbed()
+				.setColor('RANDOM')
+				.setDescription(':x: | You must be in a NSFW channel!');
+			message.channel.send(nsfw).then(m => m.delete({ timeout: 5000 }));
+			return;
+		}
+		if (args[0] == undefined) {
+			const undembed = new MessageEmbed()
+				.setColor('RANDOM')
+				.setDescription(':x: | You must supply a search term!');
+			message.channel.send(undembed).then(m => m.delete({ timeout: 5000 }));
+			return;
+		}
+		const msg = await message.channel.send('Generating...');
+		message.channel.startTyping();
+
+		const query = args.slice(0).join('+');
+		const body = await fetch(
+			`http://api.urbandictionary.com/v0/define?term=${query}`
+		).then(response => response.json());
+
+		if (body.list.length < 1) {
+			message.channel.send(`No results found for **${args.join(' ')}**.`);
+			message.channel.stopTyping();
+			msg.delete();
+			return;
+		}
+
+		const [answer] = body.list;
+
+		const embed = new MessageEmbed()
+			.setColor('RANDOM')
+			.setFooter('Urban Dictionary', 'https://i.lensdump.com/i/88BhFP.png')
+			.setTimestamp()
+			.setTitle(answer.word)
+			.setURL(answer.permalink)
+			.addField('Definition', trim(answer.definition, 1024))
+			.addField('Example', trim(answer.example, 1024))
+			.addField(
+				'Rating',
+				`${answer.thumbs_up} thumbs up. ${answer.thumbs_down} thumbs down.`
+			);
+
+		message.channel.send(embed);
+
+		message.channel.stopTyping();
+		msg.delete();
+	},
+};
