30e7796ce8bfc4cc2d21627d7eb3a85b8dfa9fc3
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-02-27 17:09:40 +0000

    new apex command

---
+++ commands/fun/apex.js
@@ -0,0 +1,80 @@
+/* jshint -W069 */
+const { MessageEmbed } = require("discord.js");
+const { stripIndents } = require("common-tags");
+const API = require("apextab-api"),
+    ApexTab = API.Apextab_API;
+const SQLite = require('better-sqlite3');
+const db = new SQLite('./storage/db/db.sqlite');
+
+module.exports = {
+    config: {
+        name: "apex",
+        usage: "<user> <platform>",
+        category: "fun",
+        description: "Displays a user's apex stats!",
+        accessableby: "Everyone",
+    },
+    run: async (bot, message, args) => {
+        const prefixgrab = db
+            .prepare('SELECT prefix FROM setprefix WHERE guildid = ?')
+            .get(message.guild.id);
+        const prefix = prefixgrab.prefix;
+
+
+        const language = require('../../storage/messages.json');
+
+        const incorrectUsageMessage = language['apex'].incorrectUsage;
+        const incorrectUsage = incorrectUsageMessage.replace('${prefix}', prefix);
+
+        if (!args[0]) return message.channel.send(incorrectUsage);
+        if (!args[1]) return message.channel.send("Please supply a platform to check. `PC`, `Xbox` or `PS4`");
+
+        const platformCheck = {
+            pc: API.Platform.PC,
+            xbox: API.Platform.XBOX_ONE,
+            ps4: API.Platform.PS4
+        };
+        const platform = platformCheck[args[1].toLowerCase()];
+
+        try {
+            const results = await ApexTab.searchPlayer(args[0], platform ? platform : API.Platform.PC);
+
+            for (let playerResult of results.results) {
+                const player = await ApexTab.getPlayerById(playerResult.aid);
+                const {
+                    name,
+                    skillratio,
+                    visits,
+                    avatar,
+                    legend,
+                    level,
+                    kills,
+                    headshots,
+                    matches,
+                    globalrank,
+                    utime
+                } = player;
+
+                const embed = new MessageEmbed()
+                    .setColor(cyan)
+                    .setAuthor(`(Apex Legends) | ${name}`, avatar)
+                    .setThumbnail(avatar)
+                    .setDescription(stripIndents `
+                                **Active Legend:** ${legend || "Not Found."}
+                                **Global Rank:** ${globalrank || "Not Ranked."}
+                                **Level:** ${level || 0}
+                                **Skill Ratio:** ${skillratio || "0%"}
+                                **Matches:** ${matches || 0}
+                                **Kills:** ${kills || 0}
+                                **Headshots:** ${headshots || 0}
+                                **Visits:** ${visits || 0}
+                                **PlayTime:** ${Math.ceil(utime / (1000 * 60 * 60 * 24)) || 0} days
+                            `);
+
+                message.channel.send(embed);
+            }
+        } catch (err) {
+            return message.channel.send("Can't find a player by that name!");
+        }
+    }
+};
