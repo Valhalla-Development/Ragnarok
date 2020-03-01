7a1b64287abea542d79354d8b5927ebde0f01064
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-03-01 16:31:26 +0000

    new doctorwho command

---
+++ commands/fun/doctorwho.js
@@ -0,0 +1,38 @@
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+
+module.exports = {
+    config: {
+        name: 'doctorwho',
+        usage: '${prefix}doctorwho',
+        category: 'fun',
+        description: 'Fetches a random Doctor Who meme!',
+        accessableby: 'Everyone',
+    },
+    run: async (bot, message, args, color) => {
+
+        const msg = await message.channel.send('Generating...');
+        message.channel.startTyping();
+
+        fetch('https://www.reddit.com/r/DoctorWhumour/random.json')
+            .then(res => res.json())
+            .then(res => res[0].data.children)
+            .then(res =>
+                res.map(post => ({
+                    link: post.data.url,
+                    title: post.data.title,
+                }))
+            )
+            .then(res => res.map(render));
+
+        const render = post => {
+            const embed = new MessageEmbed()
+                .setTitle(post.title)
+                .setColor('Top')
+                .setImage(`${post.link}`);
+            message.channel.send(embed);
+        };
+        message.channel.stopTyping();
+        msg.delete();
+    }
+};
