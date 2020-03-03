137e24a3569120692f4bf7fc6c6258b477384521
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-03-03 22:38:20 +0000

    fixed typo

--- commands/music/music.js
+++ commands/music/music.js
@@ -19,7 +19,7 @@ module.exports = {
     if (args[0] === undefined) {
       const embed = new MessageEmbed()
         .setColor(0xcf40fa)
-        .addFields({ name: 'Music Commands', value: `[${prefix}play]() <search term/link> : Plays music\n[${prefix}pause]() : Pauses music playback\n[${prefix}resume]() : Resumes music playback\n[${prefix}nowplaying]() : Displays current song being playedn\n[${prefix}queue]() : Displays current song queue\n[${prefix}skip]() : Skips current song playing\n[${prefix}repeat]() : Repeats current song playing\n[${prefix}volume <1-100>]() : Adjusts volume\n[${prefix}leave]() : Makes the bot leave the voice channel` });
+        .addFields({ name: 'Music Commands', value: `[${prefix}play]() <search term/link> : Plays music\n[${prefix}pause]() : Pauses music playback\n[${prefix}resume]() : Resumes music playback\n[${prefix}nowplaying]() : Displays current song being played\n[${prefix}queue]() : Displays current song\n[${prefix}skip]() : Skips current song\n[${prefix}repeat]() : Repeats current song playing\n[${prefix}volume <1-100>]() : Adjusts volume\n[${prefix}leave]() : Makes the bot leave the voice channel` });
       message.channel.send({
         embed,
       });
