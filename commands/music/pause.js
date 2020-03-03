1e47a54a8f43c4c008936bc1dc580d468659346a
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-03-03 18:10:27 +0000

    init pause command

---
+++ commands/music/pause.js
@@ -0,0 +1,21 @@
+module.exports = {
+  config: {
+    name: 'pause',
+    usage: '${prefix}pause/resume',
+    category: 'music',
+    description: 'Pauses/resume the bot playback.',
+    accessableby: 'Everyone',
+    aliases: ['resume'],
+  },
+  run: (bot, message) => {
+    const player = bot.music.players.get(message.guild.id);
+    if (!player) return message.channel.send('No song/s currently playing in this guild.');
+
+    const { channel } = message.member.voice;
+    if (!channel || channel.id !== player.voiceChannel.id) return message.channel.send('You need to be in a voice channel to pause music.');
+
+
+    player.pause(player.playing);
+    return message.channel.send(`Player is now ${player.playing ? 'resumed' : 'paused'}.`);
+  },
+};
