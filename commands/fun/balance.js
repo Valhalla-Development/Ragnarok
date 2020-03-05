0259a405bd7bfd15d1df43dcd84b811aaec99a18
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-03-05 17:58:05 +0000

    moved balance to fun category

---
+++ commands/fun/balance.js
@@ -0,0 +1,48 @@
+const { MessageEmbed } = require('discord.js');
+const SQLite = require('better-sqlite3');
+const db = new SQLite('./storage/db/db.sqlite');
+
+module.exports = {
+  config: {
+    name: 'balance',
+    aliases: ['bal', 'coins'],
+    usage: '${prefix}balance',
+    category: 'fun',
+    description: 'Displays your balance',
+    accessableby: 'Everyone',
+  },
+  run: async (bot, message, args, color) => {
+    bot.getBalance = db.prepare(
+      'SELECT * FROM balance WHERE user = ? AND guild = ?',
+    );
+    bot.setBalance = db.prepare(
+      'INSERT OR REPLACE INTO balance (id, user, guild, balance) VALUES (@id, @user, @guild, @balance);',
+    );
+
+    const user = message.mentions.users.first() || message.author;
+
+    let balance;
+    if (message.guild) {
+      balance = bot.getBalance.get(user.id, message.guild.id);
+    }
+
+    let phrase;
+    if (user.id === message.author.id) {
+      phrase = 'You have no balance :(';
+    } else {
+      phrase = 'This user has no balance :(';
+    }
+    if (!balance) {
+      message.channel.send(phrase);
+      return;
+    }
+
+    const embed = new MessageEmbed()
+      .setAuthor(`${user.username}'s Balance`)
+      .setColor(color)
+      .setThumbnail(user.displayAvatarURL())
+      .addFields({ name: 'Balance', value: `\`${balance.balance.toLocaleString('en')}\`` });
+
+    message.channel.send(embed);
+  },
+};
