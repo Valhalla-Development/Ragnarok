7aba371e9a048453b7bc93d9a4f362b2914ae206
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-03-20 17:36:36 +0000

    rewrote progress bar (thanks for the help Wax)

--- commands/fun/level.js
+++ commands/fun/level.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 /* eslint-disable no-undef */
 /* eslint-disable no-mixed-operators */
 const { MessageAttachment } = require('discord.js');
@@ -48,12 +49,14 @@ module.exports = {
     let toLevel;
     let inlevel;
     let xplevel;
+    let xpPercent;
     if (!score) {
       level = '0';
       points = '0';
       toLevel = '100';
       inLevel = '0';
       xplevel = '0/100 XP';
+      xpPercent = 0;
     } else {
       level = score.level;
       points = score.points;
@@ -64,6 +67,7 @@ module.exports = {
       toLevel = Math.floor(nxtLvlXp - currentxpLvl);
       inlevel = Math.floor(points - currentxpLvl);
       xplevel = `${abbreviate(inlevel, 2)}/${abbreviate(toLevel, 2)} XP`;
+      xpPercent = inlevel / toLevel * 100;
     }
 
     const userRank = db.prepare('SELECT count(*) FROM scores WHERE points >= ? AND guild = ? AND user ORDER BY points DESC').all(points, message.guild.id);
@@ -96,33 +100,44 @@ module.exports = {
     const discrim = `#${user.discriminator}`;
     const avatarGrab = user.displayAvatarURL({ format: 'png' });
 
-    const xpPercent = inlevel / toLevel * 100;
-
-    function drawProgress(x, y, length) {
-      const cornerRadius = 37;
-      let innerLength = length - cornerRadius * 2;
-
-      if (innerLength < 0) innerLength = 0;
-
-      let actualCornerRadius = cornerRadius;
-      if (length < cornerRadius * 2) {
-        actualCornerRadius = length / 2;
+    class ProgressBar {
+      constructor(dimension, color, percentage) {
+        ({
+          x: this.x, y: this.y, width: this.w, height: this.h,
+        } = dimension);
+        this.color = color;
+        this.percentage = percentage / 100;
+        this.p;
       }
 
-      ctx.lineCap = 'round';
-      ctx.lineWidth = actualCornerRadius;
-      ctx.strokeStyle = '#FF1700';
-
-      const leftX = x + actualCornerRadius / 2;
-      const rightX = leftX + innerLength;
-
-      ctx.beginPath();
-      ctx.moveTo(leftX, y);
-      ctx.lineTo(rightX, y);
-      ctx.stroke();
+      draw() {
+        this.p = this.percentage * this.w;
+        if (this.p <= this.h) {
+          ctx.beginPath();
+          ctx.arc(this.h / 2 + this.x, this.h / 2 + this.y, this.h / 2, Math.PI - Math.acos((this.h - this.p) / this.h), Math.PI + Math.acos((this.h - this.p) / this.h));
+          ctx.save();
+          ctx.scale(-1, 1);
+          ctx.arc((this.h / 2) - this.p - this.x, this.h / 2 + this.y, this.h / 2, Math.PI - Math.acos((this.h - this.p) / this.h), Math.PI + Math.acos((this.h - this.p) / this.h));
+          ctx.restore();
+          ctx.closePath();
+        } else {
+          ctx.beginPath();
+          ctx.arc(this.h / 2 + this.x, this.h / 2 + this.y, this.h / 2, Math.PI / 2, 3 / 2 * Math.PI);
+          ctx.lineTo(this.p - this.h + this.x, 0 + this.y);
+          ctx.arc(this.p - (this.h / 2) + this.x, this.h / 2 + this.y, this.h / 2, 3 / 2 * Math.PI, Math.PI / 2);
+          ctx.lineTo(this.h / 2 + this.x, this.h + this.y);
+          ctx.closePath();
+        }
+        ctx.fillStyle = this.color;
+        ctx.fill();
+      }
     }
 
-    drawProgress(259.4, 201, 666.4 * xpPercent / 100);
+    progressbar = new ProgressBar({
+      x: 259.8, y: 182.9, width: 628.4, height: 36.5,
+    }, '#FF1700', xpPercent);
+    progressbar.draw();
+
 
     // Draw XP
     function drawXP(x, y, xp) {
