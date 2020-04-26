56d5c5e7b96604e802b505f5a71d81e74aca0756
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-04-26 16:17:02 +0100

    fix event not triggering if adsprot is disabled

--- events/guild/messageUpdate.js
+++ events/guild/messageUpdate.js
@@ -4,21 +4,19 @@ const db = new SQLite('./storage/db/db.sqlite');
 
 module.exports = async (bot, oldMessage, newMessage) => {
   const adsprot = db.prepare('SELECT count(*) FROM adsprot WHERE guildid = ?').get(newMessage.guild.id);
-  if (!adsprot['count(*)']) {
-    return;
-  }
-  if (newMessage.content.includes('https://') || newMessage.content.includes('http://') || newMessage.content.includes('discord.gg') || newMessage.content.includes('discord.me') || newMessage.content.includes('discord.io')) {
-    if (newMessage.member.hasPermission('MANAGE_GUILD')) {
-      return;
+  if (adsprot['count(*)']) {
+    if (newMessage.content.includes('https://') || newMessage.content.includes('http://') || newMessage.content.includes('discord.gg') || newMessage.content.includes('discord.me') || newMessage.content.includes('discord.io')) {
+      if (!newMessage.member.hasPermission('MANAGE_GUILD')) {
+        newMessage.delete();
+        newMessage.channel.send(`**Your message contained a link and it was deleted, <@${newMessage.author.id}>**`)
+          .then((msg) => {
+            msg.delete({ timeout: 10000 });
+          });
+      }
     }
-    newMessage.delete();
-    newMessage.channel.send(`**Your message contained a link and it was deleted, <@${newMessage.author.id}>**`)
-      .then((msg) => {
-        msg.delete({ timeout: 10000 });
-      });
   }
 
-
+  // This never runs if adsprot is disabled, damn
   const id = db
     .prepare(`SELECT channel FROM logging WHERE guildid = ${oldMessage.guild.id};`)
     .get();
