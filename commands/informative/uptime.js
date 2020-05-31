6c91a82d13c65b59cf5f639553d01f4a5cdcdec0
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-05-31 03:08:20 +0100

    uptime rewrite

--- commands/informative/uptime.js
+++ commands/informative/uptime.js
@@ -1,25 +1,6 @@
 /* eslint-disable prefer-const */
 const { MessageEmbed } = require('discord.js');
-const SQLite = require('better-sqlite3');
-const db = new SQLite('./storage/db/db.sqlite');
-
-function convertMS(ms) {
-  let d; let h; let m; let
-    s;
-  s = Math.floor(ms / 1000);
-  m = Math.floor(s / 60);
-  s %= 60;
-  h = Math.floor(m / 60);
-  m %= 60;
-  d = Math.floor(h / 24);
-  h %= 24;
-  return {
-    d,
-    h,
-    m,
-    s,
-  };
-}
+const ms = require('ms');
 
 module.exports = {
   config: {
@@ -30,21 +11,10 @@ module.exports = {
     accessableby: 'Everyone',
   },
   run: async (bot, message) => {
-    const prefixgrab = db
-      .prepare('SELECT prefix FROM setprefix WHERE guildid = ?')
-      .get(message.guild.id);
-
-    const { prefix } = prefixgrab;
-
-    if (!message.content.startsWith(prefix)) return;
-
-    const u = convertMS(bot.uptime);
-    const uptime = `${u.d} days : ${u.h} hours : ${u.m} minutes : ${u.s} seconds`;
-
     const botembed = new MessageEmbed()
       .setTitle('Uptime')
       .setColor('36393F')
-      .setDescription(`${uptime}`);
+      .setDescription(`My uptime is \`${ms(bot.uptime, { long: true })}\``);
 
     message.channel.send(botembed);
   },
