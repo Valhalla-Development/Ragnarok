6296bac0448a0c61db93965751458d9e56a7d51b
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-06-07 02:31:21 +0100

    rewrote a4k command

--- src/Commands/Hidden/a4k.js
+++ src/Commands/Hidden/a4k.js
@@ -3,6 +3,7 @@ const { MessageEmbed } = require('discord.js');
 const SQLite = require('better-sqlite3');
 const db = new SQLite('./Storage/DB/db.sqlite');
 const language = require('../../../Storage/messages.json');
+const fetch = require('node-fetch');
 
 module.exports = class extends Command {
 
@@ -13,49 +14,28 @@ module.exports = class extends Command {
 		const incorrectUsageMessage = language.a4k.incorrectUsage;
 		const incorrectUsage = incorrectUsageMessage.replace('${prefix}', prefix);
 
-
 		if (!args[0]) {
 			message.channel.send(incorrectUsage);
 			return;
 		}
 
-		const msg = await message.channel.send('Generating...');
-		message.channel.startTyping();
-
 		const searchTerm = args.join('%20');
-		const argsNonJoin = args.join(' ');
 
-		const embed = new MessageEmbed()
-			.setAuthor('Reddit - A4K', 'http://i.imgur.com/sdO8tAw.png')
-			.setColor('36393F')
-			.setDescription(`Search Results For - ${argsNonJoin}\n[Click Me!](https://www.reddit.com/r/Addons4Kodi/search/?q=${searchTerm}&restrict_sr=1)`);
-		message.channel.send(embed);
-
-
-		/* fetch(`https://www.reddit.com/r/Addons4Kodi/search.json?q=${searchTerm}&restrict_sr=1`)
-            .then(res => res.json())
-            .then(res => res.data.children)
-            .then(res =>
-                res.json(post => ({
-                    link: post.data.url,
-                    title: post.data.title,
-                    subreddit: post.data.subreddit
-                }))
-            )
-            .then(res => res.map(render));
-
-        const render = post => {
-            console.log(post)
-            message.channel.send(post.subreddit)
-
-            const embed = new MessageEmbed()
-                .setAuthor(post.subreddit, 'http://i.imgur.com/sdO8tAw.png')
-                .setColor('36393F')
-                .setDescription(`Search Results: [Click Me!](https://www.reddit.com/r/Addons4Kodi/search/?q=${searchTerm}&restrict_sr=1)\n\n`)
-            message.channel.send(embed);
-        }; */
-		message.channel.stopTyping();
-		msg.delete();
+		fetch(`https://www.reddit.com/r/Addons4Kodi/search.json?q=${searchTerm}&restrict_sr=1`)
+			.then(res => res.json())
+			.then(res => res.data.children)
+			.then(res => {
+				const embed = new MessageEmbed()
+					.setAuthor(res[0].data.subreddit, 'http://i.imgur.com/sdO8tAw.png')
+					.setColor('36393F')
+					.setDescription(`\`1:\` ${res[0].data.title} [Link](${res[0].data.url})\n
+					\`2:\` ${res[1].data.title} [Link](${res[1].data.url})\n
+					\`3:\` ${res[2].data.title} [Link](${res[2].data.url})\n
+					\`4:\` ${res[3].data.title} [Link](${res[3].data.url})\n
+					\`5:\` ${res[4].data.title} [Link](${res[4].data.url})\n
+					Search Results: [Link](https://www.reddit.com/r/Addons4Kodi/search/?q=${searchTerm}&restrict_sr=1)`);
+				message.channel.send(embed);
+			});
 	}
 
 };
