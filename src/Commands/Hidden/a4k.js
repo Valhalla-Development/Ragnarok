23f7a4a7734487407eddc8df635d174c5d7dea91
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-06-10 20:53:08 +0100

    Included search result text on a4k command

--- src/Commands/Hidden/a4k.js
+++ src/Commands/Hidden/a4k.js
@@ -21,19 +21,17 @@ module.exports = class extends Command {
 
 		const searchTerm = args.join('%20');
 
-		fetch(`https://www.reddit.com/r/Addons4Kodi/search.json?q=${searchTerm}&restrict_sr=1&limit=5`)
+		fetch(`https://www.reddit.com/r/Addons4Kodi/search.json?q=${searchTerm}&restrict_sr=1&limit=3`)
 			.then(res => res.json())
 			.then(res => res.data.children)
 			.then(res => {
 				const embed = new MessageEmbed()
-					.setAuthor(res[0].data.subreddit, 'http://i.imgur.com/sdO8tAw.png')
+					.setAuthor(`${res[0].data.subreddit} - Top 3 results for: ${args.join(' ')}`, 'http://i.imgur.com/sdO8tAw.png')
 					.setColor('36393F')
-					.setDescription(`\`1:\` ${res[0].data.title} [Link](${res[0].data.url})\n
-					\`2:\` ${res[1].data.title} [Link](${res[1].data.url})\n
-					\`3:\` ${res[2].data.title} [Link](${res[2].data.url})\n
-					\`4:\` ${res[3].data.title} [Link](${res[3].data.url})\n
-					\`5:\` ${res[4].data.title} [Link](${res[4].data.url})\n
-					Search Results: [Link](https://www.reddit.com/r/Addons4Kodi/search/?q=${searchTerm}&restrict_sr=1)`);
+					.setDescription(`[**${res[0].data.title}**](${res[0].data.url})\n \`\`\`${res[0].data.selftext.substring(0, 250)}...\`\`\`\n
+					[**${res[1].data.title}**](${res[1].data.url})\n  \`\`\`${res[1].data.selftext.substring(0, 250)}...\`\`\`\n
+					[**${res[2].data.title}**](${res[2].data.url})\n  \`\`\`${res[2].data.selftext.substring(0, 250)}...\`\`\`\n
+					[**__Search Results...__**](https://www.reddit.com/r/Addons4Kodi/search/?q=${searchTerm}&restrict_sr=1)`);
 				message.channel.send(embed);
 			}).catch(() => message.channel.send('No results found.'));
 	}
