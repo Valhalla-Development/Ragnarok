5f43399c7430a7190e35f41b7c5c7e69b217cf4f
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-06-11 19:22:58 +0100

    rename

---
+++ src/Commands/Informative/Ping.js
@@ -0,0 +1,19 @@
+const Command = require('../../Structures/Command');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['pong']
+		});
+	}
+
+	async run(message) {
+		const msg = await message.channel.send('Pinging...');
+
+		const latency = msg.createdTimestamp - message.createdTimestamp;
+
+		msg.edit(`Bot Latency: \`${latency}ms\`, API Latency: \`${Math.round(this.client.ws.ping)}ms\``);
+	}
+
+};
