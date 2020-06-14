c89d3a72083565f6857299266e718aecf9ee826a
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-06-14 04:11:03 +0100

    rewrote discord.js command

---
+++ src/Commands/Fun/Djs.js
@@ -0,0 +1,32 @@
+const Command = require('../../Structures/Command');
+const fetch = require('node-fetch');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			description: 'Displays search results in the D.js documentation.',
+			category: 'Fun',
+			usage: 'Djs <input>'
+		});
+	}
+
+	async run(message, args) {
+		const query = args.join(' ');
+		const url = `https://djsdocs.sorta.moe/v2/embed?src=stable&q=${encodeURIComponent(query)}`;
+		fetch(url)
+			.then(res => res.json())
+			.then(embed => {
+				if (embed && !embed.error) {
+					message.channel.send({ embed });
+				} else {
+					message.reply(`I found no results for ${query}.`);
+				}
+			})
+			.catch(err => {
+				console.error(err);
+				message.reply('An error occured :slight_frown:');
+			});
+	}
+
+};
