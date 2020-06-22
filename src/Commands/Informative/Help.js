52d2c9ddb09e94cf8d58f93ec94470657ebaa5b5
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-06-22 04:10:19 +0100

    Total rewrite of help command

---
+++ src/Commands/Informative/Help.js
@@ -0,0 +1,58 @@
+const { MessageEmbed } = require('discord.js');
+const Command = require('../../Structures/Command');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			description: 'Display command list / command usage',
+			category: 'Informative',
+			usage: 'Help <command>'
+		});
+	}
+
+	async run(message, [command]) {
+		const embed = new MessageEmbed()
+			.setColor('36393F')
+			.setAuthor(`${message.guild.name} Help`, message.guild.iconURL({ dynamic: true }))
+			.setThumbnail(this.client.user.displayAvatarURL())
+			.setFooter(`This guild's prefix is ${this.client.prefix}`, this.client.user.avatarURL({ dynamic: true }))
+			.setTimestamp();
+
+		if (command) {
+			const cmd = this.client.commands.get(command) || this.client.commands.get(this.aliases.get(command));
+
+			if (!cmd) return message.channel.send(`Invalid Command named. \`${command}`);
+
+			embed.setAuthor(`${this.client.utils.capitalise(cmd.name)} Command Help`, this.client.user.displayAvatarURL());
+			embed.setDescription([
+				`**◎ Aliases:** ${cmd.aliases.length ? cmd.aliases.map(alias => `\`${alias}\``).join(' ') : 'No Aliases'}`,
+				`**◎ Description:** ${cmd.description}`,
+				`**◎ Category:** ${cmd.category}`,
+				`**◎ Usage:** ${cmd.usage}`
+			]);
+
+			return message.channel.send(embed);
+		} else {
+			embed.setDescription([
+				`Hey, I'm [**__Ragnarok__**]! A multi-purpose bot!`,
+				`Run \`${this.client.prefix}help <command>\` to see command specific instructions`,
+				`All commands must be preceded by \`${this.client.prefix}\``,
+				`Command Parameters: \`<>\` is strict & \`[]\` is optional`
+			]);
+			let categories;
+			if (!this.client.owners.includes(message.author.id)) {
+				categories = this.client.utils.removeDuplicates(this.client.commands.filter(cmd => cmd.category !== 'Hidden').map(cmd => cmd.category));
+			} else {
+				categories = this.client.utils.removeDuplicates(this.client.commands.map(cmd => cmd.category));
+			}
+
+			for (const category of categories) {
+				embed.addField(`**${this.client.utils.capitalise(category)}**`, this.client.commands.filter(cmd =>
+					cmd.category === category).map(cmd => `\`${this.client.utils.capitalise(cmd.name)}\``).join(', '));
+			}
+			return message.channel.send(embed);
+		}
+	}
+
+};
