210827e60822e26d2c34d66ea279d1fc8ecb37ad
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-06-23 22:36:38 +0100

    changed invite command to be dynamic

--- src/Commands/Informative/Invite.js
+++ src/Commands/Informative/Invite.js
@@ -14,7 +14,7 @@ module.exports = class extends Command {
 	async run(message) {
 		const embed = new MessageEmbed()
 			.setColor('36393F')
-			.setDescription(':white_check_mark: [**Bot Invite Link**](https://discordapp.com/oauth2/authorize?client_id=508756879564865539&scope=bot&permissions=2050485471)');
+			.setDescription(`:white_check_mark: [**Bot Invite Link**](https://discordapp.com/oauth2/authorize?client_id=${this.client.user.id}&scope=bot&permissions=2050485471)`);
 		message.channel.send(embed);
 	}
 
