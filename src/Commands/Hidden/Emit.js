4687115757c90898008e65ad8ded9269f2276c49
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-06-23 22:05:13 +0100

    rewrote emit command

---
+++ src/Commands/Hidden/Emit.js
@@ -0,0 +1,47 @@
+const Command = require('../../Structures/Command');
+const { MessageEmbed } = require('discord.js');
+const SQLite = require('better-sqlite3');
+const db = new SQLite('./Storage/DB/db.sqlite');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			description: 'Emits a Discord.js event.',
+			category: 'Hidden',
+			usage: 'Emit <event>'
+		});
+	}
+
+	async run(message, args) {
+		if (!this.client.owners.includes(message.author.id)) return;
+
+		const prefixgrab = db
+			.prepare('SELECT prefix FROM setprefix WHERE guildid = ?')
+			.get(message.guild.id);
+		const { prefix } = prefixgrab;
+
+		if (args[0] === undefined) {
+			const noArgs = new MessageEmbed()
+				.setColor('36393F')
+				.setDescription(
+					`**Available Commands**:\n\n${prefix}emit guildMemberAdd\n${prefix}emit guildMemberRemove`
+				);
+			message.channel.send(noArgs);
+		}
+		if (args[0] === 'guildMemberAdd') {
+			this.client.emit('guildMemberAdd', message.member);
+			return;
+		}
+		if (args[0] === 'guildMemberRemove') {
+			this.client.emit('guildMemberRemove', message.member);
+			return;
+		}
+		if (args[0] === 'guildMemberUpdate') {
+			this.client.emit('guildMemberUpdate', message.member);
+			message.channel.send('success');
+			return;
+		}
+	}
+
+};
