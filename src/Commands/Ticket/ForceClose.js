75ef4a150e822c87144b3bb9a349185a49f7d954
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-07-12 17:48:51 +0100

    ported ticket -forceclose

---
+++ src/Commands/Ticket/ForceClose.js
@@ -0,0 +1,93 @@
+const Command = require('../../Structures/Command');
+const { MessageEmbed } = require('discord.js');
+const SQLite = require('better-sqlite3');
+const db = new SQLite('./Storage/DB/db.sqlite');
+const language = require('../../../Storage/messages.json');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['fclose'],
+			description: 'Forcefully closes a ticket.',
+			category: 'Ticket',
+			usage: '[ticketid]'
+		});
+	}
+
+	async run(message, args) {
+		const suppRole = db.prepare(`SELECT role FROM ticketConfig WHERE guildid = ${message.guild.id}`).get();
+
+		let modRole;
+		if (suppRole) {
+			modRole = message.guild.roles.cache.find((supId) => supId.id === suppRole.role);
+		} else {
+			modRole = message.guild.roles.cache.find((supNa) => supNa.name === 'Support Team');
+		}
+
+		if (!modRole) {
+			const nomodRole = new MessageEmbed()
+				.setColor(message.guild.me.displayHexColor || '36393F')
+				.setDescription(`${language.tickets.nomodRole}`);
+			message.channel.send(nomodRole);
+			return;
+		}
+
+		if (!message.member.roles.cache.has(modRole.id) && message.author.id !== message.guild.ownerID) {
+			const donthaveroleMessage = language.tickets.donthaveRole;
+			const role = donthaveroleMessage.replace('${role}', modRole);
+			const donthaveRole = new MessageEmbed()
+				.setColor(message.guild.me.displayHexColor || '36393F')
+				.setDescription(`${role}`);
+			message.channel.send(donthaveRole);
+			return;
+		}
+
+		const channelArgs = message.channel.name.split('-');
+		const foundTicket = db.prepare(`SELECT * FROM tickets WHERE guildid = ${message.guild.id} AND ticketid = (@ticketid)`).get({
+			ticketid: args[0] || channelArgs[channelArgs.length - 1]
+		});
+		if (foundTicket) {
+			const getChan = message.guild.channels.cache.find((chan) => chan.id === foundTicket.chanid);
+			const forceclosetimer = new MessageEmbed()
+				.setColor(message.guild.me.displayHexColor || '36393F')
+				.setTitle(':x: Closing Ticket! :x:')
+				.setDescription(`${language.tickets.closeTimer}`);
+			getChan.send(forceclosetimer).then((timerMsg) => {
+				getChan.awaitMessages((resp) => resp.author.id === message.author.id || foundTicket.authorid, {
+					max: 1,
+					time: 10000,
+					errors: ['time']
+				}).then(() => {
+					const cancelTimer = new MessageEmbed()
+						.setColor(message.guild.me.displayHexColor || '36393F')
+						.setDescription('Canceling Ticket Close');
+					timerMsg.edit(cancelTimer).then((cancelMsg) => {
+						cancelMsg.delete({
+							timeout: 5000
+						});
+					});
+				}).catch(() => {
+					getChan.delete();
+					db.prepare(`DELETE FROM tickets WHERE guildid = ${message.guild.id} AND ticketid = (@ticketid)`).run({
+						ticketid: foundTicket.ticketid
+					});
+					const logget = db.prepare(`SELECT log FROM ticketConfig WHERE guildid = ${message.guild.id};`).get();
+					if (!logget) return;
+					const logchan = message.guild.channels.cache.find((chan) => chan.id === logget.log);
+					if (!logchan) return;
+					const loggingembed = new MessageEmbed()
+						.setColor(message.guild.me.displayHexColor || '36393F')
+						.setDescription(`<@${message.author.id}> has forcefully closed ticket \`#${message.channel.name}\``);
+					logchan.send(loggingembed);
+				});
+			});
+		} else {
+			const errEmbed = new MessageEmbed()
+				.setColor(message.guild.me.displayHexColor || '36393F')
+				.setDescription('This ticket could not be found.');
+			message.channel.send(errEmbed);
+		}
+	}
+
+};
