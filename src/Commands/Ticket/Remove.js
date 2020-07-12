20c4a46db5576547883b9cbc31f06ebdc464901f
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-07-12 17:49:02 +0100

    ported ticket -remove

---
+++ src/Commands/Ticket/Remove.js
@@ -0,0 +1,81 @@
+const Command = require('../../Structures/Command');
+const { MessageEmbed } = require('discord.js');
+const SQLite = require('better-sqlite3');
+const db = new SQLite('./Storage/DB/db.sqlite');
+const language = require('../../../Storage/messages.json');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			description: 'Removes tagged user from a ticket.',
+			category: 'Ticket',
+			usage: '<@user> [ticketid]'
+		});
+	}
+
+	async run(message, args) {
+		const suppRole = db.prepare(`SELECT role FROM ticketConfig WHERE guildid = ${message.guild.id}`).get();
+
+		let modRole;
+		if (suppRole) {
+			modRole = message.guild.roles.cache.find((r) => r.id === suppRole.role);
+		} else {
+			modRole = message.guild.roles.cache.find((x) => x.name === 'Support Team');
+		}
+
+		if (!modRole) {
+			const nomodRole = new MessageEmbed()
+				.setColor(message.guild.me.displayHexColor || '36393F')
+				.setDescription(`${language.tickets.nomodRole}`);
+			message.channel.send(nomodRole);
+			return;
+		}
+
+		if (!message.member.roles.cache.has(modRole.id) && message.author.id !== message.guild.ownerID) {
+			const donthaveroleMessage = language.tickets.donthaveRole;
+			const role = donthaveroleMessage.replace('${role}', modRole);
+			const donthaveRole = new MessageEmbed()
+				.setColor(message.guild.me.displayHexColor || '36393F')
+				.setDescription(`${role}`);
+			message.channel.send(donthaveRole);
+			return;
+		}
+
+		const rUser = message.mentions.users.first();
+		if (!rUser) {
+			const nouser = new MessageEmbed()
+				.setColor(message.guild.me.displayHexColor || '36393F')
+				.setDescription(`${language.tickets.cantfindUser}`);
+			message.channel.send(nouser);
+			return;
+		}
+
+		const channelArgs = message.channel.name.split('-');
+		const foundTicket = db.prepare(`SELECT * FROM tickets WHERE guildid = ${message.guild.id} AND ticketid = (@ticketid)`).get({
+			ticketid: args[1] || channelArgs[channelArgs.length - 1]
+		});
+		if (foundTicket) {
+			const getChan = message.guild.channels.cache.find((chan) => chan.id === foundTicket.chanid);
+			getChan.createOverwrite(rUser, {
+				VIEW_CHANNEL: false
+			}).catch(console.error);
+			const removedMessage = language.tickets.removed;
+			const theuser = removedMessage.replace('${user}', rUser);
+			getChan.send(`${theuser}`);
+			const logget = db.prepare(`SELECT log FROM ticketConfig WHERE guildid = ${message.guild.id};`).get();
+			const logchan = message.guild.channels.cache.find((chan) => chan.id === logget.log);
+			if (!logchan) return;
+			const loggingembed = new MessageEmbed()
+				.setColor(message.guild.me.displayHexColor || '36393F')
+				.setDescription(`<@${message.author.id}> removed ${rUser} from ticket <#${getChan.id}>`);
+			logchan.send(loggingembed);
+		} else {
+			const errEmbed = new MessageEmbed()
+				.setColor(message.guild.me.displayHexColor || '36393F')
+				.setDescription('This ticket could not be found.');
+			message.channel.send(errEmbed);
+		}
+	}
+
+};
