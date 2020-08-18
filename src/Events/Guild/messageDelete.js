9c5e75370cddf3578aaeae5a6d6d889f2ebac6f4
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-08-19 00:29:27 +0100

    rewrote messageDelete event completely

--- src/Events/Guild/messageDelete.js
+++ src/Events/Guild/messageDelete.js
@@ -1,4 +1,5 @@
 const Event = require('../../Structures/Event');
+const RagnarokEmbed = require('../../Structures/RagnarokEmbed');
 const { MessageEmbed } = require('discord.js');
 const SQLite = require('better-sqlite3');
 const db = new SQLite('./Storage/DB/db.sqlite');
@@ -6,7 +7,27 @@ const db = new SQLite('./Storage/DB/db.sqlite');
 module.exports = class extends Event {
 
 	async run(message) {
-		if (message.author.bot) return;
+		if (!message.guild || message.author.bot) return;
+		// the following code is from menudocs, test it and probably replace your own code with this bruh
+		const attachments = message.attachments.size ? message.attachments.map(attachment => attachment.proxyURL) : null;
+		const embed = new RagnarokEmbed()
+			.setColor(message.guild.me.displayHexColor || '36393F')
+			.setAuthor(message.author.tag, this.client.user.displayAvatarURL({ dynamic: true }))
+			.setTitle('Message Deleted')
+			.setDescription([
+				`**◎ Message ID:** ${message.id}`,
+				`**◎ Channel:** ${message.channel}`,
+				`**◎ Author:** ${message.member.displayName}`,
+				`${attachments ? `**◎ Attachments:** ${attachments.join('\n')}` : ''}`
+			])
+			.setTimestamp();
+		if (message.content.length) {
+			embed.splitFields(`**◎ Deleted Message:** ${message.content}`);
+		}
+		const channel = message.guild.channels.cache.find(ch => ch.name === 'owner-testing');
+		if (channel) channel.send(embed);
+		// stolen code from menudocs ends here
+
 		const id = db.prepare(`SELECT channel FROM logging WHERE guildid = ${message.guild.id};`).get();
 		if (!id) return;
 		const logs = id.channel;
