89697589be6ee89fc66d8b7103ec00dcf0c9a448
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-08-21 15:41:05 +0100

    New AirReps command!

---
+++ src/Commands/Hidden/AirReps.js
@@ -0,0 +1,47 @@
+const Command = require('../../Structures/Command');
+const { MessageEmbed } = require('discord.js');
+const SQLite = require('better-sqlite3');
+const db = new SQLite('./Storage/DB/db.sqlite');
+const fetch = require('node-fetch');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			description: 'Fetches search results from r/AirReps',
+			category: 'Hidden',
+			usage: '<input>'
+		});
+	}
+
+	async run(message, args) {
+		const prefixgrab = db.prepare('SELECT prefix FROM setprefix WHERE guildid = ?').get(message.guild.id);
+		const { prefix } = prefixgrab;
+
+		if (!args[0]) {
+			const incorrectFormat = new MessageEmbed()
+				.setColor(message.guild.me.displayHexColor || '36393F')
+				.addField(`**${this.client.user.username} - A4K**`,
+					`**◎ Error:** Incorrect usage! Please use \`${prefix}a4k <search>\``);
+			message.channel.send(incorrectFormat).then((m) => m.delete({ timeout: 15000 }));
+			return;
+		}
+
+		const searchTerm = args.join('%20');
+
+		fetch(`https://www.reddit.com/r/AirReps/search.json?q=${searchTerm}&restrict_sr=1&limit=3`)
+			.then(res => res.json())
+			.then(res => res.data.children)
+			.then(res => {
+				const embed = new MessageEmbed()
+					.setAuthor(`${res[0].data.subreddit} - Top 3 results for: ${args.join(' ')}`, 'https://styles.redditmedia.com/t5_2oemly/styles/communityIcon_vzp0ymwfksz41.png?width=256&s=96596caa93f51c37505a2cecf33f2abdb8d93d87')
+					.setColor(message.guild.me.displayHexColor || '36393F')
+					.setDescription(`[**◎ ${res[0].data.title}**](${res[0].data.url})\n \`\`\`${res[0].data.selftext.substring(0, 250)}...\`\`\`\n
+					[**◎ ${res[1].data.title}**](${res[1].data.url})\n  \`\`\`${res[1].data.selftext.substring(0, 250)}...\`\`\`\n
+					[**◎ ${res[2].data.title}**](${res[2].data.url})\n  \`\`\`${res[2].data.selftext.substring(0, 250)}...\`\`\`\n
+					[**__Search Results...__**](https://www.reddit.com/r/AirReps/search/?q=${searchTerm}&restrict_sr=1)`);
+				message.channel.send(embed);
+			}).catch(() => message.channel.send('No results found.'));
+	}
+
+};
