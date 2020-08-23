58a81f4452ec7b7733d3a905a9e6275f12ea2342
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-08-23 03:05:37 +0100

    New Prefix command

---
+++ src/Commands/Informative/Prefix.js
@@ -0,0 +1,26 @@
+const Command = require('../../Structures/Command');
+const { MessageEmbed } = require('discord.js');
+const SQLite = require('better-sqlite3');
+const db = new SQLite('./Storage/DB/db.sqlite');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			description: 'Displays the current prefix for the',
+			category: 'Informative'
+		});
+	}
+
+	async run(message) {
+		const prefixgrab = db.prepare('SELECT prefix FROM setprefix WHERE guildid = ?').get(message.guild.id);
+		const { prefix } = prefixgrab;
+
+		const embed = new MessageEmbed()
+			.setColor(message.guild.me.displayHexColor || '36393F')
+			.addField(`**${this.client.user.username} - Prefix**`,
+				`**◎ My prefix for this guild is: \`${prefix}\``);
+		message.channel.send(embed);
+	}
+
+};
