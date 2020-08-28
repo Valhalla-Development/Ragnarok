a53c5a0c8e80a7147c007c68fbdafdcaa7cc9d59
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-08-28 20:23:14 +0100

    new rules command

---
+++ src/Commands/Hidden/Rules.js
@@ -0,0 +1,37 @@
+const Command = require('../../Structures/Command');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			description: 'Posts server rules',
+			category: 'Hidden'
+		});
+	}
+
+	async run(message) {
+		// Disable for all servers except The Treehouse :)
+		if (message.guild.id !== '495602800802398212') return;
+
+		if (!this.client.owners.includes(message.author.id)) return;
+
+		const channel = message.guild.channels.cache.find((chan) => chan.name === 'testing');
+
+		const embed = new MessageEmbed()
+			.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+			.setAuthor(`${message.guild.name} - Rules`, message.guild.iconURL({ dynamic: true }))
+			.setDescription([
+				`**◎ 1:** Any and all forms of racism will **NOT** be tolerated.`,
+				`\u3000`,
+				`**◎ 2:** Do **NOT** mass tag users/roles.`,
+				`\u3000`,
+				`**◎ 3:** Do **NOT** spam any channel.`,
+				`\u3000`,
+				`**◎ 4:** If using any bot in this guild, please keep testing to ${channel}.`,
+				`\u3000`
+			]);
+		message.channel.send(embed);
+	}
+
+};
