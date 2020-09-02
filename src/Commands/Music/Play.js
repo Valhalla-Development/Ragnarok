2c0cdcbb727df2644152837a9a7cfaa1c09be7a0
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-09-02 20:55:56 +0100

    Rewrote Play command

--- src/Commands/Music/Play.js
+++ src/Commands/Music/Play.js
@@ -1,10 +1,10 @@
 /* eslint-disable consistent-return */
 const Command = require('../../Structures/Command');
 const { MessageEmbed } = require('discord.js');
-const { Utils } = require('erela.js');
 const talkedRecently = new Set();
 const SQLite = require('better-sqlite3');
 const db = new SQLite('./Storage/DB/db.sqlite');
+const prettyMilliseconds = require('pretty-ms');
 
 module.exports = class extends Command {
 
@@ -74,47 +74,74 @@ module.exports = class extends Command {
 				.addField(`**${this.client.user.username} - Play**`,
 					`**◎ Error:** There is a 30 second cooldown for this command!\nDJ's are exempt from the cooldown.`);
 			message.channel.send(embed).then((m) => m.delete({ timeout: 15000 }));
-		} else {
-			if (!args[0]) {
-				const embed = new MessageEmbed()
+			return;
+		} // add check if role was added after cooldown bub
+
+		if (!args[0]) {
+			const embed = new MessageEmbed()
+				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+				.addField(`**${this.client.user.username} - Play**`,
+					`**◎ Error:** Please provide an URL or a search term.`);
+			message.channel.send(embed).then((m) => m.delete({ timeout: 15000 }));
+			return;
+		}
+
+		const player = this.client.manager.create({
+			guild: message.guild.id,
+			voiceChannel: message.member.voice.channel.id,
+			textChannel: message.channel.id
+		});
+
+		await this.client.manager.search(args.join(' '), message.author).then(async (res) => {
+			if (res.loadType === 'NO_MATCHES' || res.loadType === 'LOAD_FAILED') {
+				if (message.member.guild.me.hasPermission('MANAGE_MESSAGES')) {
+					message.delete();
+				}
+				const noTrack = new MessageEmbed()
+					.setAuthor('Error', 'https://upload.wikimedia.org/wikipedia/commons/7/73/YouTube_Music.png')
 					.setColor(this.client.utils.color(message.guild.me.displayHexColor))
-					.addField(`**${this.client.user.username} - Play**`,
-						`**◎ Error:** Please provide an URL or a search term.`);
-				message.channel.send(embed).then((m) => m.delete({ timeout: 15000 }));
+					.setDescription(`No tracks found.`);
+				message.channel.send(noTrack).then((m) => m.delete({ timeout: 15000 }));
 				return;
 			}
-
-			const player = this.client.music.players.spawn({
-				guild: message.guild,
-				textChannel: message.channel,
-				voiceChannel: channel
-			});
-
-			this.client.music.search(args.join(' '), message.author).then(async (res) => {
-				switch (res.loadType) {
-					case 'TRACK_LOADED': {
-						player.queue.add(res.tracks[0]);
+			player.set('textChannel', message.channel);
+			switch (res.loadType) {
+				case 'TRACK_LOADED': {
+					if (res.tracks[0].duration >= 600000) {
+						const embed1 = new MessageEmbed()
+							.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+							.addField(`**${this.client.user.username} - Play**`,
+								`**◎ Error:** Duration is over 10 minutes! Cancelling playback`);
+						message.channel.send(embed1).then((me) => me.delete({ timeout: 15000 }));
+						if (player.queue.size === 0) {
+							player.destroy(message.guild.id);
+						}
+						return;
+					}
+					player.queue.add(res.tracks[0]);
+					player.connect();
+					if (!player.playing && !player.paused && player.queue.size === 1) player.play();
+					if (player.queue.size !== 1) {
 						const trackloade = new MessageEmbed()
 							.setAuthor('Enqueueing.', 'https://upload.wikimedia.org/wikipedia/commons/7/73/YouTube_Music.png')
 							.setColor(this.client.utils.color(message.guild.me.displayHexColor))
-							.setDescription(`\`${res.tracks[0].title}\`\nDuration: \`${Utils.formatTime(res.tracks[0].duration, true)}\`\nRequested by: ${message.author}`);
+							.setDescription(`\`${res.tracks[0].title}\`\nDuration: \`${prettyMilliseconds(res.tracks[0].duration, { colonNotation: true })}\`\nRequested by: ${message.author}`);
 						message.channel.send(trackloade);
-						if (!player.playing) player.play();
-						break;
 					}
+					break;
+				}
 
-					case 'SEARCH_RESULT': {
-						let index = 1;
-						const tracks = res.tracks.slice(0, 5);
-						const embed = new MessageEmbed()
-							.setAuthor('Search Results.')
-							.setColor(this.client.utils.color(message.guild.me.displayHexColor))
-							.setThumbnail('https://upload.wikimedia.org/wikipedia/commons/7/73/YouTube_Music.png')
-							.setDescription(tracks.map((video) => `**${index++} -** ${video.title} - \`${Utils.formatTime(video.duration, true)}\``))
-							.setFooter("Type the track number you wish to play. You have 30 seconds to respond.\nType 'cancel' to cancel the selection");
+				case 'SEARCH_RESULT': {
+					let index = 1;
+					const tracks = res.tracks.slice(0, 5);
+					const embed = new MessageEmbed()
+						.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+						.addField(`**${this.client.user.username} - Play**`,
+							tracks.map((video) => `**◎ ${index++} -** ${video.title} - \`${prettyMilliseconds(video.duration, { colonNotation: true })}\``))
+						.setThumbnail('https://upload.wikimedia.org/wikipedia/commons/7/73/YouTube_Music.png')
+						.setFooter("Type the track number you wish to play. You have 30 seconds to respond.\nType 'cancel' to cancel the selection");
 
-						await message.channel.send(embed);
-
+					await message.channel.send(embed).then((searchEmbed) => {
 						const collector = message.channel.createMessageCollector((m) => m.author.id === message.author.id && new RegExp('^([1-5]|cancel)$', 'i').test(m.content), {
 							time: 30000,
 							max: 1
@@ -124,23 +151,34 @@ module.exports = class extends Command {
 							if (/cancel/i.test(m.content)) return collector.stop('cancelled');
 							const track = tracks[Number(m.content) - 1];
 							if (track.duration >= 600000) {
+								if (message.member.guild.me.hasPermission('MANAGE_MESSAGES')) {
+									searchEmbed.delete();
+									m.delete();
+								}
 								const embed1 = new MessageEmbed()
 									.setColor(this.client.utils.color(message.guild.me.displayHexColor))
 									.addField(`**${this.client.user.username} - Play**`,
 										`**◎ Error:** Duration is over 10 minutes! Cancelling playback`);
 								message.channel.send(embed1).then((me) => me.delete({ timeout: 15000 }));
-								if (player.queue.size <= 0) {
-									this.client.music.players.destroy(message.guild.id);
+								if (player.queue.size === 0) {
+									player.destroy(message.guild.id);
 								}
 								return;
 							}
+							if (message.member.guild.me.hasPermission('MANAGE_MESSAGES')) {
+								searchEmbed.delete();
+								m.delete();
+							}
 							player.queue.add(track);
-							if (!player.playing) player.play();
-							const trackloade = new MessageEmbed()
-								.setAuthor('Enqueuing Track.', 'https://upload.wikimedia.org/wikipedia/commons/7/73/YouTube_Music.png')
-								.setColor(this.client.utils.color(message.guild.me.displayHexColor))
-								.setDescription(`\`${track.title}\`\nDuration: \`${Utils.formatTime(track.duration, true)}\`\nRequested by: ${message.author}`);
-							message.channel.send(trackloade);
+							player.connect();
+							if (!player.playing && !player.paused && player.queue.size === 1) player.play();
+							if (player.queue.size !== 1) {
+								const trackloade = new MessageEmbed()
+									.setAuthor('Enqueuing Track.', 'https://upload.wikimedia.org/wikipedia/commons/7/73/YouTube_Music.png')
+									.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+									.setDescription(`\`${track.title}\`\nDuration: \`${prettyMilliseconds(track.duration, { colonNotation: true })}\`\nRequested by: ${message.author}`);
+								message.channel.send(trackloade);
+							}
 						});
 
 						collector.on('end', (_, reason) => {
@@ -151,44 +189,48 @@ module.exports = class extends Command {
 									.setColor(this.client.utils.color(message.guild.me.displayHexColor))
 									.setDescription(`Search results cancelled.\nReason: \`${upperReason}\``);
 								message.channel.send(cancelE).then((msg) => msg.delete({ timeout: 15000 }));
-								if (player.queue.size <= 0) {
-									this.client.music.players.destroy(message.guild.id);
+								if (message.member.guild.me.hasPermission('MANAGE_MESSAGES')) {
+									searchEmbed.delete();
+								}
+								if (player.queue.size === 0) {
+									player.destroy(message.guild.id);
 								}
 								return;
 							}
 						});
-						break;
-					}
-
-					case 'PLAYLIST_LOADED': {
-						res.playlist.tracks.forEach((track) => player.queue.add(track));
-						const duration = Utils.formatTime(res.playlist.tracks.reduce((acc, cur) => ({
-							duration: acc.duration + cur.duration
-						})).duration, true);
-						if (player.queue.length < 1) return;
-						if (!player.playing) player.play();
+					});
+					break;
+				}
+				case 'PLAYLIST_LOADED': {
+					res.tracks.forEach((track) => player.queue.add(track));
+					const duration = prettyMilliseconds(res.tracks.reduce((acc, cur) => ({
+						duration: acc.duration + cur.duration
+					})).duration, { colonNotation: true });
+					player.connect();
+					if (!player.playing && !player.paused && player.queue.size === res.tracks.length) player.play();
+					if (player.queue.size !== 1) {
 						const playlistload = new MessageEmbed()
 							.setAuthor('Enqueuing Playlist.', 'https://upload.wikimedia.org/wikipedia/commons/7/73/YouTube_Music.png')
 							.setColor(this.client.utils.color(message.guild.me.displayHexColor))
-							.setDescription(`Enqueuing \`${res.playlist.tracks.length}\` \`${duration}\` tracks in playlist \`${res.playlist.info.name}\``);
+							.setDescription(`Enqueuing \`${res.tracks.length}\` tracks in playlist \`${res.playlist.name}\`\nTotal duration: \`${duration}\``);
 						message.channel.send(playlistload);
-						break;
 					}
+					break;
 				}
-			}).catch((err) => message.channel.send(`\`${err.message}\``));
-			if (message.member.roles.cache.has(role.id)) {
-				return;
 			}
-			talkedRecently.add(message.author.id);
-			setTimeout(() => {
-				const embed1 = new MessageEmbed()
-					.setColor(this.client.utils.color(message.guild.me.displayHexColor))
-					.addField(`**${this.client.user.username} - Play**`,
-						`**◎ Success:** <@${message.author.id}> You can now use the play command again.`);
-				message.channel.send(embed1).then((me) => me.delete({ timeout: 15000 }));
-				talkedRecently.delete(message.author.id);
-			}, 30000);
+		}).catch((err) => message.channel.send(`\`${err.message}\``));
+		if (message.member.roles.cache.has(role.id)) {
+			return;
 		}
+		talkedRecently.add(message.author.id);
+		setTimeout(() => {
+			const embed1 = new MessageEmbed()
+				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+				.addField(`**${this.client.user.username} - Play**`,
+					`**◎ Success:** <@${message.author.id}> You can now use the play command again.`);
+			message.channel.send(embed1).then((me) => me.delete({ timeout: 15000 }));
+			talkedRecently.delete(message.author.id);
+		}, 30000);
 	}
 
 };
