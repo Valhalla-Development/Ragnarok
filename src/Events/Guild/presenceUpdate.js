9283283dab322c1e9ed56307962c7b0c63120db5
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-09-02 23:42:03 +0100

    For AirReps

---
+++ src/Events/Guild/presenceUpdate.js
@@ -0,0 +1,43 @@
+const Event = require('../../Structures/Event');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class extends Event {
+
+	// this is just for AirReps!
+
+	async run(oldPresence, newPresence) {
+		if (oldPresence.guild.id !== '495602800802398212') return;
+
+		const Towy = this.client.users.cache.find((a) => a.id === '178843418061963264');
+		const Ragnar = this.client.users.cache.find((a) => a.id === '151516555757223936');
+
+		const offlineEmbed = new MessageEmbed()
+			.setTitle('Ragnarok Report')
+			.setDescription(`<@${newPresence.userID}> is **OFFLINE**\n${Towy}, ${Ragnar}`)
+			.setColor('#ff2f2f')
+			.setTimestamp();
+
+		const onlineEmbed = new MessageEmbed()
+			.setTitle('Ragnarok Report')
+			.setDescription(`<@${newPresence.userID}> is **ONLINE**`)
+			.setColor('#27d200')
+			.setTimestamp();
+
+		// Status other than offline
+		const statusList = ['online', 'idle', 'dnd'];
+
+		const airRepsBotId = '664924910048641044';
+
+		const channelid = this.client.channels.cache.find((a) => a.id === '657342849737687041');
+		if (airRepsBotId.includes(newPresence.userID)) {
+			if (oldPresence.status === newPresence.status) return;
+			if (statusList.includes(oldPresence.status) && statusList.includes(newPresence.status)) return;
+			if (newPresence.status === 'offline') {
+				channelid.send(offlineEmbed);
+			} else {
+				channelid.send(onlineEmbed);
+			}
+		}
+	}
+
+};
