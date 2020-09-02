8814cdcb7ce424c7ed2acbfd8f6e7c67b97c13e0
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-09-02 20:52:59 +0100

    voiceStateUpdate event (check if everyone leaves the bot channel when playing music)

---
+++ src/Events/Guild/voiceStateUpdate.js
@@ -0,0 +1,25 @@
+const Event = require('../../Structures/Event');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class extends Event {
+
+	async run(oldState, newState) {
+		const player = this.client.manager.players.get(newState.guild.id);
+		if (player) {
+			// if oldState channel ID does not equal the player voiceChannel, return
+			if (oldState.channelID !== player.voiceChannel) {
+				return;
+			}
+			const userCount = oldState.channel.members.size;
+			if (userCount <= 1) {
+				const embed = new MessageEmbed()
+					.setColor(this.client.utils.color(oldState.guild.me.displayHexColor))
+					.addField(`**${this.client.user.username} - Music**`,
+						`**◎ Error:** No users in voice channel, ending playback.`);
+				this.client.channels.cache.get(player.textChannel).send(embed).then((m) => m.delete({ timeout: 15000 }));
+				player.destroy();
+			}
+		}
+	}
+
+};
