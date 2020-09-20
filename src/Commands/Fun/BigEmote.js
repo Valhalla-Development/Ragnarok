88bdf1faa71a5416842ac01fb8bb2f830ad56977
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-09-20 17:53:22 +0100

    new big emote command

---
+++ src/Commands/Fun/BigEmote.js
@@ -0,0 +1,60 @@
+const Command = require('../../Structures/Command');
+const { MessageEmbed, Util } = require('discord.js');
+const { parse } = require('twemoji-parser');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			description: 'Displays an emote at full size.',
+			category: 'Fun',
+			usage: '<emote>'
+		});
+	}
+
+	async run(message, args) {
+		const emoji = args[0];
+
+		if (!emoji) {
+			this.client.utils.messageDelete(message, 10000);
+
+			const embed = new MessageEmbed()
+				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+				.addField(`**${this.client.user.username} - BigEmote**`,
+					`**◎ Error:** Incorrect usage! Please specify an emote!`);
+			message.channel.send(embed).then((m) => this.client.utils.deletableCheck(m, 10000));
+			return;
+		}
+
+		const custom = Util.parseEmoji(emoji);
+
+		if (custom.id) {
+			const embed = new MessageEmbed()
+				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+				.setTitle(`**${this.client.user.username} - BigEmote**`)
+				.setImage(`https://cdn.discordapp.com/emojis/${custom.id}.${custom.animated ? 'gif' : 'png'}`);
+			message.channel.send(embed);
+			return;
+		} else {
+			const parsed = parse(emoji, { assetType: 'png' });
+			if (!parsed[0]) {
+				this.client.utils.messageDelete(message, 10000);
+
+				const embed = new MessageEmbed()
+					.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+					.addField(`**${this.client.user.username} - BigEmote**`,
+						`**◎ Error:** Invalid emoji!`);
+				message.channel.send(embed).then((m) => this.client.utils.deletableCheck(m, 10000));
+				return;
+			}
+
+			const embed = new MessageEmbed()
+				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+				.setTitle(`**${this.client.user.username} - BigEmote**`)
+				.setImage(parsed[0].url);
+			message.channel.send(embed);
+			return;
+		}
+	}
+
+};
