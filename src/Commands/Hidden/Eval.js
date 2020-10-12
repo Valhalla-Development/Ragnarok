0fefd1c650bee51a2d20bfa0dbd1994aa1ca3c27
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-10-12 01:20:10 +0100

    rewrote eval command

--- src/Commands/Hidden/Eval.js
+++ src/Commands/Hidden/Eval.js
@@ -1,8 +1,13 @@
+/* eslint-disable arrow-body-style */
 /* eslint-disable no-unused-vars */
 const Command = require('../../Structures/Command');
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, MessageAttachment } = require('discord.js');
+const { inspect } = require('util');
+const { Type } = require('@extreme_hero/deeptype');
 const SQLite = require('better-sqlite3');
 const db = new SQLite('./Storage/DB/db.sqlite');
+const Hastebin = require('hastebin.js');
+const haste = new Hastebin({ url: 'https://pastie.io' });
 
 module.exports = class extends Command {
 
@@ -16,7 +21,7 @@ module.exports = class extends Command {
 	}
 
 	async run(message, args) {
-		if (args.length < 1) {
+		if (!args.length) {
 			this.client.utils.messageDelete(message, 10000);
 
 			const incorrectFormat = new MessageEmbed()
@@ -27,45 +32,59 @@ module.exports = class extends Command {
 			return;
 		}
 
-		function clean(text) {
-			if (typeof text === 'string') {
-				return text
-					.replace(/'/g, `\`${String.fromCharCode(8203)}`)
-					.replace(/@/g, `@${String.fromCharCode(8203)}`);
-			}
-			return text;
-		}
-
-		const argresult = args.join(' ');
-
+		let code = args.join(' ');
+		code = code.replace(/[“”]/g, '"').replace(/[‘’]/g, "'");
+		let evaled;
 		try {
-			let evaled = eval(argresult);
+			const start = process.hrtime();
+			evaled = eval(code);
+			if (evaled instanceof Promise) {
+				evaled = await evaled;
+			}
+			const stop = process.hrtime(start);
+			const success = new MessageEmbed()
+				.addField(`${this.client.user.username} - Eval`, [
+					`**◎ Output:** \`\`\`js\n${this.clean(inspect(evaled, { depth: 0 }))}\n\`\`\``,
+					`**◎ Type:** \`\`\`ts\n${new Type(evaled).is}\n\`\`\``
+				])
+				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+				.setFooter(`Time Taken: ${(((stop[0] * 1e9) + stop[1])) / 1e6}s`);
 
-			if (typeof evaled !== 'string') evaled = require('util').inspect(evaled);
-			if (evaled.includes(this.client.token)) {
-				console.log(`\n${message.author.username}#${message.author.discriminator} Tried to get the bot token on ${message.guild.name} (ServerID: ${message.guild.id}).\n`);
+			if (success.fields[0].value.length > 1024) {
+				await haste.post(this.clean(inspect(evaled, { depth: 1 })), 'js')
+					.then((link) => {
+						const hastEmb = new MessageEmbed()
+							.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+							.addField(`**${this.client.user.username} - Eval**`,
+								`**◎ Link:** ${link}`)
+							.setURL(link)
+							.setFooter('Embed field limit reached, posting to pastie.io');
+						message.channel.send(hastEmb);
+					}).catch(() => {
+						return;
+					});
 				return;
 			}
-
-			const embed = new MessageEmbed()
-				.setAuthor(`${this.client.user.username} - Eval`, this.client.user.displayAvatarURL({ dynamic: true }))
-				.addFields({ name: `${message.author.username} - JavaScript Eval Success:`, value: '** **' },
-					{ name: ':inbox_tray: **INPUT**', value: `\`\`\`${args.join(' ')}\`\`\`` },
-					{ name: ':outbox_tray: **OUTPUT**', value: `\`\`\`${clean(evaled)}\`\`\`` })
-				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
-				.setTimestamp();
-			message.channel.send({
-				embed
-			});
+			await message.channel.send(success);
 		} catch (err) {
-			message.channel.send(new MessageEmbed()
-				.setAuthor(`${this.client.user.username} - Eval Error`, this.client.user.displayAvatarURL({ dynamic: true }))
-				.addFields({ name: `${message.author.username} - JavaScript Eval Error:`, value: 'There was a problem with the code you tried to run!' },
-					{ name: ':no_entry: ERROR', value: `\`\`\`${clean(err)}\`\`\`` })
-				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
-				.setFooter(message.createdAt, message.author.avatarURL())
-			).catch((error) => message.channel.send(`**ERROR:** ${error.message}`));
+			const error = new MessageEmbed()
+				.addField(`${this.client.user.username} - Eval`, [
+					`**◎ Error:** \`\`\`x1\n${this.clean(err)}\n\`\`\``
+				])
+				.setColor(this.client.utils.color(message.guild.me.displayHexColor));
+			message.channel.send(error);
+			return;
 		}
+	}
+
+	clean(text) {
+		if (typeof text === 'string') {
+			text = text
+				.replace(/`/g, `\`${String.fromCharCode(8203)}`)
+				.replace(/@/g, `@${String.fromCharCode(8203)}`)
+				.replace(new RegExp(this.client.token, 'gi'), 'u steal token! succ u!');
+		}
+		return text;
 	}
 
 };
