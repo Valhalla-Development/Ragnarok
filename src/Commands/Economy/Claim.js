aff42db86c65f83ef4082e2675a27d8f47ddf754
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-10-18 19:00:40 +0100

    Fixed claim display negative time remaining

--- src/Commands/Economy/Claim.js
+++ src/Commands/Economy/Claim.js
@@ -157,6 +157,41 @@ module.exports = class extends Command {
 					hourly: null,
 					id: `${message.author.id}-${message.guild.id}`
 				});
+
+				const hourlyAmount = Math.floor(Math.random() * (150 - 50 + 1) + 50); // * (max - min + 1) + min);
+
+				const endTime = new Date().getTime() + 3600000;
+
+				const totaCalc2 = balance.total + hourlyAmount;
+				const addAut = {
+					id: `${message.author.id}-${message.guild.id}`,
+					user: message.author.id,
+					guild: message.guild.id,
+					hourly: endTime,
+					daily: balance.daily,
+					weekly: balance.weekly,
+					monthly: balance.monthly,
+					yearly: balance.yearly,
+					stealcool: balance.stealcool,
+					boosts: balance.boosts,
+					cash: balance.cash + hourlyAmount,
+					bank: balance.bank,
+					total: totaCalc2,
+					fishcool: balance.fishcool,
+					farmcool: balance.farmcool,
+					items: balance.items,
+					claimNewUser: balance.claimNewUser
+				};
+
+				this.client.setBalance.run(addAut);
+
+				const depArg = new MessageEmbed()
+					.setAuthor(`${message.author.username}`, message.author.avatarURL())
+					.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+					.addField(`**${this.client.user.username} - Hourly**`,
+						`**◎ Success:** You have received your hourly sum of: <:coin:706659001164628008> ${hourlyAmount.toLocaleString('en')}.`);
+				message.channel.send(depArg);
+				return;
 			}
 
 			if (balance.hourly !== null) {
@@ -166,47 +201,55 @@ module.exports = class extends Command {
 					.setAuthor(`${message.author.tag}`, message.author.avatarURL())
 					.setColor(this.client.utils.color(message.guild.me.displayHexColor))
 					.addField(`**${this.client.user.username} - Hourly**`,
-						`**◎ Error:** Please wait another \`${Date.now() > balance.hourly ? 'Available!' : ms(balance.hourly - date.getTime(), { long: true })}\` before using this command.`);
+						`**◎ Error:** Please wait another \`${ms(balance.hourly - date.getTime(), { long: true })}\` before using this command.`);
 				message.channel.send(embed).then((m) => this.client.utils.deletableCheck(m, 10000));
 				return;
 			}
-
-			const hourlyAmount = Math.floor(Math.random() * (150 - 50 + 1) + 50); // * (max - min + 1) + min);
-
-			const endTime = new Date().getTime() + 3600000;
-
-			const totaCalc2 = balance.total + hourlyAmount;
-			const addAut = {
-				id: `${message.author.id}-${message.guild.id}`,
-				user: message.author.id,
-				guild: message.guild.id,
-				hourly: endTime,
-				daily: balance.daily,
-				weekly: balance.weekly,
-				monthly: balance.monthly,
-				yearly: balance.yearly,
-				stealcool: balance.stealcool,
-				boosts: balance.boosts,
-				cash: balance.cash + hourlyAmount,
-				bank: balance.bank,
-				total: totaCalc2,
-				fishcool: balance.fishcool,
-				farmcool: balance.farmcool,
-				items: balance.items,
-				claimNewUser: balance.claimNewUser
-			};
-
-			this.client.setBalance.run(addAut);
-
-			const depArg = new MessageEmbed()
-				.setAuthor(`${message.author.username}`, message.author.avatarURL())
-				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
-				.addField(`**${this.client.user.username} - Hourly**`,
-					`**◎ Success:** You have received your hourly sum of: <:coin:706659001164628008> ${hourlyAmount.toLocaleString('en')}.`);
-			message.channel.send(depArg);
 		}
 
 		if (args[0] === 'daily') {
+			if (Date.now() > balance.daily) {
+				await db.prepare('UPDATE balance SET daily = (@daily) WHERE id = (@id);').run({
+					daily: null,
+					id: `${message.author.id}-${message.guild.id}`
+				});
+
+				const dailyAmount = Math.floor(Math.random() * (300 - 150 + 1) + 150); // * (max - min + 1) + min);
+
+				const endTime = new Date().getTime() + 86400000;
+
+				const totaCalc2 = balance.total + dailyAmount;
+				const addAut = {
+					id: `${message.author.id}-${message.guild.id}`,
+					user: message.author.id,
+					guild: message.guild.id,
+					hourly: balance.hourly,
+					daily: endTime,
+					weekly: balance.weekly,
+					monthly: balance.monthly,
+					yearly: balance.yearly,
+					stealcool: balance.stealcool,
+					boosts: balance.boosts,
+					cash: balance.cash + dailyAmount,
+					bank: balance.bank,
+					total: totaCalc2,
+					fishcool: balance.fishcool,
+					farmcool: balance.farmcool,
+					items: balance.items,
+					claimNewUser: balance.claimNewUser
+				};
+
+				this.client.setBalance.run(addAut);
+
+				const depArg = new MessageEmbed()
+					.setAuthor(`${message.author.username}`, message.author.avatarURL())
+					.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+					.addField(`**${this.client.user.username} - Daily**`,
+						`**◎ Success:** You have received your daily sum of: <:coin:706659001164628008> ${dailyAmount.toLocaleString('en')}.`);
+				message.channel.send(depArg);
+				return;
+			}
+
 			if (balance.daily !== null) {
 				this.client.utils.messageDelete(message, 10000);
 
@@ -214,47 +257,55 @@ module.exports = class extends Command {
 					.setAuthor(`${message.author.tag}`, message.author.avatarURL())
 					.setColor(this.client.utils.color(message.guild.me.displayHexColor))
 					.addField(`**${this.client.user.username} - Daily**`,
-						`**◎ Error:** Please wait another \`${Date.now() > balance.daily ? 'Available!' : ms(balance.daily - date.getTime(), { long: true })}\` before using this command.`);
+						`**◎ Error:** Please wait another \`${ms(balance.daily - date.getTime(), { long: true })}\` before using this command.`);
 				message.channel.send(embed).then((m) => this.client.utils.deletableCheck(m, 10000));
 				return;
 			}
-
-			const dailyAmount = Math.floor(Math.random() * (300 - 150 + 1) + 150); // * (max - min + 1) + min);
-
-			const endTime = new Date().getTime() + 86400000;
-
-			const totaCalc2 = balance.total + dailyAmount;
-			const addAut = {
-				id: `${message.author.id}-${message.guild.id}`,
-				user: message.author.id,
-				guild: message.guild.id,
-				hourly: balance.hourly,
-				daily: endTime,
-				weekly: balance.weekly,
-				monthly: balance.monthly,
-				yearly: balance.yearly,
-				stealcool: balance.stealcool,
-				boosts: balance.boosts,
-				cash: balance.cash + dailyAmount,
-				bank: balance.bank,
-				total: totaCalc2,
-				fishcool: balance.fishcool,
-				farmcool: balance.farmcool,
-				items: balance.items,
-				claimNewUser: balance.claimNewUser
-			};
-
-			this.client.setBalance.run(addAut);
-
-			const depArg = new MessageEmbed()
-				.setAuthor(`${message.author.username}`, message.author.avatarURL())
-				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
-				.addField(`**${this.client.user.username} - Daily**`,
-					`**◎ Success:** You have received your daily sum of: <:coin:706659001164628008> ${dailyAmount.toLocaleString('en')}.`);
-			message.channel.send(depArg);
 		}
 
 		if (args[0] === 'weekly') {
+			if (Date.now() > balance.weekly) {
+				await db.prepare('UPDATE balance SET weekly = (@weekly) WHERE id = (@id);').run({
+					weekly: null,
+					id: `${message.author.id}-${message.guild.id}`
+				});
+
+				const weeklyAmount = Math.floor(Math.random() * (1000 - 750 + 1) + 750); // * (max - min + 1) + min);
+
+				const endTime = new Date().getTime() + 604800000;
+
+				const totaCalc2 = balance.total + weeklyAmount;
+				const addAut = {
+					id: `${message.author.id}-${message.guild.id}`,
+					user: message.author.id,
+					guild: message.guild.id,
+					hourly: balance.hourly,
+					daily: balance.daily,
+					weekly: endTime,
+					monthly: balance.monthly,
+					yearly: balance.yearly,
+					stealcool: balance.stealcool,
+					boosts: balance.boosts,
+					cash: balance.cash + weeklyAmount,
+					bank: balance.bank,
+					total: totaCalc2,
+					fishcool: balance.fishcool,
+					farmcool: balance.farmcool,
+					items: balance.items,
+					claimNewUser: balance.claimNewUser
+				};
+
+				this.client.setBalance.run(addAut);
+
+				const depArg = new MessageEmbed()
+					.setAuthor(`${message.author.username}`, message.author.avatarURL())
+					.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+					.addField(`**${this.client.user.username} - Weeky**`,
+						`**◎ Success:** You have received your weekly sum of: <:coin:706659001164628008> ${weeklyAmount.toLocaleString('en')}.`);
+				message.channel.send(depArg);
+				return;
+			}
+
 			if (balance.weekly !== null) {
 				this.client.utils.messageDelete(message, 10000);
 
@@ -266,43 +317,51 @@ module.exports = class extends Command {
 				message.channel.send(embed).then((m) => this.client.utils.deletableCheck(m, 10000));
 				return;
 			}
-
-			const weeklyAmount = Math.floor(Math.random() * (1000 - 750 + 1) + 750); // * (max - min + 1) + min);
-
-			const endTime = new Date().getTime() + 604800000;
-
-			const totaCalc2 = balance.total + weeklyAmount;
-			const addAut = {
-				id: `${message.author.id}-${message.guild.id}`,
-				user: message.author.id,
-				guild: message.guild.id,
-				hourly: balance.hourly,
-				daily: balance.daily,
-				weekly: endTime,
-				monthly: balance.monthly,
-				yearly: balance.yearly,
-				stealcool: balance.stealcool,
-				boosts: balance.boosts,
-				cash: balance.cash + weeklyAmount,
-				bank: balance.bank,
-				total: totaCalc2,
-				fishcool: balance.fishcool,
-				farmcool: balance.farmcool,
-				items: balance.items,
-				claimNewUser: balance.claimNewUser
-			};
-
-			this.client.setBalance.run(addAut);
-
-			const depArg = new MessageEmbed()
-				.setAuthor(`${message.author.username}`, message.author.avatarURL())
-				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
-				.addField(`**${this.client.user.username} - Weeky**`,
-					`**◎ Success:** You have received your weekly sum of: <:coin:706659001164628008> ${weeklyAmount.toLocaleString('en')}.`);
-			message.channel.send(depArg);
 		}
 
 		if (args[0] === 'monthly') {
+			if (Date.now() > balance.monthly) {
+				await db.prepare('UPDATE balance SET monthly = (@monthly) WHERE id = (@id);').run({
+					monthly: null,
+					id: `${message.author.id}-${message.guild.id}`
+				});
+
+				const monthlyAmount = Math.floor(Math.random() * (6000 - 4000 + 1) + 4000); // * (max - min + 1) + min);
+
+				const endTime = new Date().getTime() + 2629800000;
+
+				const totaCalc2 = balance.total + monthlyAmount;
+				const addAut = {
+					id: `${message.author.id}-${message.guild.id}`,
+					user: message.author.id,
+					guild: message.guild.id,
+					hourly: balance.hourly,
+					daily: balance.daily,
+					weekly: balance.weekly,
+					monthly: endTime,
+					yearly: balance.yearly,
+					stealcool: balance.stealcool,
+					boosts: balance.boosts,
+					cash: balance.cash + monthlyAmount,
+					bank: balance.bank,
+					total: totaCalc2,
+					fishcool: balance.fishcool,
+					farmcool: balance.farmcool,
+					items: balance.items,
+					claimNewUser: balance.claimNewUser
+				};
+
+				this.client.setBalance.run(addAut);
+
+				const depArg = new MessageEmbed()
+					.setAuthor(`${message.author.username}`, message.author.avatarURL())
+					.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+					.addField(`**${this.client.user.username} - Monthly**`,
+						`**◎ Success:** You have received your monthly sum of: <:coin:706659001164628008> ${monthlyAmount.toLocaleString('en')}.`);
+				message.channel.send(depArg);
+				return;
+			}
+
 			if (balance.monthly !== null) {
 				this.client.utils.messageDelete(message, 10000);
 
@@ -314,43 +373,51 @@ module.exports = class extends Command {
 				message.channel.send(embed).then((m) => this.client.utils.deletableCheck(m, 10000));
 				return;
 			}
-
-			const monthlyAmount = Math.floor(Math.random() * (6000 - 4000 + 1) + 4000); // * (max - min + 1) + min);
-
-			const endTime = new Date().getTime() + 2629800000;
-
-			const totaCalc2 = balance.total + monthlyAmount;
-			const addAut = {
-				id: `${message.author.id}-${message.guild.id}`,
-				user: message.author.id,
-				guild: message.guild.id,
-				hourly: balance.hourly,
-				daily: balance.daily,
-				weekly: balance.weekly,
-				monthly: endTime,
-				yearly: balance.yearly,
-				stealcool: balance.stealcool,
-				boosts: balance.boosts,
-				cash: balance.cash + monthlyAmount,
-				bank: balance.bank,
-				total: totaCalc2,
-				fishcool: balance.fishcool,
-				farmcool: balance.farmcool,
-				items: balance.items,
-				claimNewUser: balance.claimNewUser
-			};
-
-			this.client.setBalance.run(addAut);
-
-			const depArg = new MessageEmbed()
-				.setAuthor(`${message.author.username}`, message.author.avatarURL())
-				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
-				.addField(`**${this.client.user.username} - Monthly**`,
-					`**◎ Success:** You have received your monthly sum of: <:coin:706659001164628008> ${monthlyAmount.toLocaleString('en')}.`);
-			message.channel.send(depArg);
 		}
 
 		if (args[0] === 'yearly') {
+			if (Date.now() > balance.yearly) {
+				await db.prepare('UPDATE balance SET yearly = (@yearly) WHERE id = (@id);').run({
+					yearly: null,
+					id: `${message.author.id}-${message.guild.id}`
+				});
+
+				const yearlyAmount = Math.floor(Math.random() * (50000 - 47500 + 1) + 47500); // * (max - min + 1) + min);
+
+				const endTime = new Date().getTime() + 31557600000;
+
+				const totaCalc2 = balance.total + yearlyAmount;
+				const addAut = {
+					id: `${message.author.id}-${message.guild.id}`,
+					user: message.author.id,
+					guild: message.guild.id,
+					hourly: balance.hourly,
+					daily: balance.daily,
+					weekly: balance.weekly,
+					monthly: balance.monthly,
+					yearly: endTime,
+					stealcool: balance.stealcool,
+					boosts: balance.boosts,
+					cash: balance.cash + yearlyAmount,
+					bank: balance.bank,
+					total: totaCalc2,
+					fishcool: balance.fishcool,
+					farmcool: balance.farmcool,
+					items: balance.items,
+					claimNewUser: balance.claimNewUser
+				};
+
+				this.client.setBalance.run(addAut);
+
+				const depArg = new MessageEmbed()
+					.setAuthor(`${message.author.username}`, message.author.avatarURL())
+					.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+					.addField(`**${this.client.user.username} - Yearly**`,
+						`**◎ Success:** You have received your yearly sum of: <:coin:706659001164628008> ${yearlyAmount.toLocaleString('en')}.`);
+				message.channel.send(depArg);
+				return;
+			}
+
 			if (balance.yearly !== null) {
 				this.client.utils.messageDelete(message, 10000);
 
@@ -362,40 +429,6 @@ module.exports = class extends Command {
 				message.channel.send(embed).then((m) => this.client.utils.deletableCheck(m, 10000));
 				return;
 			}
-
-			const yearlyAmount = Math.floor(Math.random() * (50000 - 47500 + 1) + 47500); // * (max - min + 1) + min);
-
-			const endTime = new Date().getTime() + 31557600000;
-
-			const totaCalc2 = balance.total + yearlyAmount;
-			const addAut = {
-				id: `${message.author.id}-${message.guild.id}`,
-				user: message.author.id,
-				guild: message.guild.id,
-				hourly: balance.hourly,
-				daily: balance.daily,
-				weekly: balance.weekly,
-				monthly: balance.monthly,
-				yearly: endTime,
-				stealcool: balance.stealcool,
-				boosts: balance.boosts,
-				cash: balance.cash + yearlyAmount,
-				bank: balance.bank,
-				total: totaCalc2,
-				fishcool: balance.fishcool,
-				farmcool: balance.farmcool,
-				items: balance.items,
-				claimNewUser: balance.claimNewUser
-			};
-
-			this.client.setBalance.run(addAut);
-
-			const depArg = new MessageEmbed()
-				.setAuthor(`${message.author.username}`, message.author.avatarURL())
-				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
-				.addField(`**${this.client.user.username} - Yearly**`,
-					`**◎ Success:** You have received your yearly sum of: <:coin:706659001164628008> ${yearlyAmount.toLocaleString('en')}.`);
-			message.channel.send(depArg);
 		}
 	}
 
