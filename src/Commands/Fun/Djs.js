b5e01105e9762286ab8581d618fdee516501c66e
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2021-03-19 03:05:36 +0000

    cleaned up djs code

--- src/Commands/Fun/Djs.js
+++ src/Commands/Fun/Djs.js
@@ -12,8 +12,8 @@ module.exports = class extends Command {
 		});
 	}
 
-	async run(message, args) {
-		if (!args[0]) {
+	async run(message, ...query) {
+		if (!query) {
 			this.client.utils.messageDelete(message, 10000);
 
 			const noInput = new MessageEmbed()
@@ -24,35 +24,39 @@ module.exports = class extends Command {
 			return;
 		}
 
-		const query = args.join(' ');
 		const url = `https://djsdocs.sorta.moe/v2/embed?src=stable&q=${encodeURIComponent(query)}`;
 
-		fetch(url)
-			.then(res => res.json())
-			.then(embed => {
-				if (embed && !embed.error) {
-					message.channel.send({ embed });
-				} else {
-					this.client.utils.messageDelete(message, 10000);
+		const docFetch = await fetch(url);
+		const embed = await docFetch.json();
 
-					const noResult = new MessageEmbed()
-						.setColor(this.client.utils.color(message.guild.me.displayHexColor))
-						.addField(`**${this.client.user.username} - Discord.js**`,
-							`**◎ Error:** I found no results for\n\`${query}\``);
-					message.channel.send(noResult).then((m) => this.client.utils.deletableCheck(m, 10000));
-				}
-			})
-			.catch(err => {
-				console.error(err);
+		if (!embed || embed.error) {
+			this.client.utils.messageDelete(message, 10000);
 
-				this.client.utils.messageDelete(message, 10000);
+			const noResult = new MessageEmbed()
+				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+				.addField(`**${this.client.user.username} - Discord.js**`,
+					`**◎ Error:** I found no results for\n\`${query}\``);
+			message.channel.send(noResult).then((m) => this.client.utils.deletableCheck(m, 10000));
+			return;
+		}
 
-				const error = new MessageEmbed()
-					.setColor(this.client.utils.color(message.guild.me.displayHexColor))
-					.addField(`**${this.client.user.username} - Discord.js**`,
-						`**◎ Error:** An error occured :slight_frown:`);
-				message.channel.send(error).then((m) => this.client.utils.deletableCheck(m, 10000));
-			});
+		const msg = await message.channel.send({ embed });
+		msg.react('🗑');
+
+		let react;
+		try {
+			react = await msg.awaitReactions(
+				(reaction, user) => reaction.emoji.name === '🗑' && user.id === message.author.id,
+				{ max: 1, time: 10000, errors: ['time'] }
+			);
+		} catch (error) {
+			msg.reactions.removeAll();
+		}
+
+		if (react && react.first()) {
+			this.client.utils.deletableCheck(msg, 0);
+			this.client.utils.messageDelete(message, 0);
+		}
 	}
 
 };
