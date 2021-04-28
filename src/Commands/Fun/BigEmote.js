24e59b9f3b999d4bcb59c031fd489e6ee8fe4715
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2021-04-28 03:20:40 +0100

    delete message on bigemote command

--- src/Commands/Fun/BigEmote.js
+++ src/Commands/Fun/BigEmote.js
@@ -26,12 +26,14 @@ module.exports = class extends Command {
 			return;
 		}
 
+		this.client.utils.messageDelete(message, 0);
+
 		const custom = Util.parseEmoji(emoji);
 
 		if (custom.id) {
 			const embed = new MessageEmbed()
 				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
-				.setTitle(`**${this.client.user.username} - BigEmote**`)
+				.setTitle(`BigEmote - Requesed By ${message.guild.members.cache.get(message.author.id).nickname ? message.guild.members.cache.get(message.author.id).nickname : message.author.username}`)
 				.setImage(`https://cdn.discordapp.com/emojis/${custom.id}.${custom.animated ? 'gif' : 'png'}`);
 			message.channel.send(embed);
 			return;
@@ -42,7 +44,7 @@ module.exports = class extends Command {
 
 				const embed = new MessageEmbed()
 					.setColor(this.client.utils.color(message.guild.me.displayHexColor))
-					.addField(`**${this.client.user.username} - BigEmote**`,
+					.addField(`BigEmote - Requesed By ${message.guild.members.cache.get(message.author.id).nickname ? message.guild.members.cache.get(message.author.id).nickname : message.author.username}`,
 						`**◎ Error:** Invalid emoji!`);
 				message.channel.send(embed).then((m) => this.client.utils.deletableCheck(m, 10000));
 				return;
@@ -50,7 +52,7 @@ module.exports = class extends Command {
 
 			const embed = new MessageEmbed()
 				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
-				.setTitle(`**${this.client.user.username} - BigEmote**`)
+				.setTitle(`BigEmote - Requesed By ${message.guild.members.cache.get(message.author.id).nickname ? message.guild.members.cache.get(message.author.id).nickname : message.author.username}`)
 				.setImage(parsed[0].url);
 			message.channel.send(embed);
 			return;
