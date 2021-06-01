cf96c4ccfdaa20ab4493f73b88e3121fa5d3a128
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2021-06-01 19:04:46 +0100

    Fix invalid variables on items command

--- src/Commands/Economy/Items.js
+++ src/Commands/Economy/Items.js
@@ -15,7 +15,7 @@ module.exports = class extends Command {
 		});
 	}
 
-	async run(message, args) {
+	async run(message) {
 		const prefixgrab = db.prepare('SELECT prefix FROM setprefix WHERE guildid = ?').get(message.guild.id);
 		const { prefix } = prefixgrab;
 
@@ -39,6 +39,7 @@ module.exports = class extends Command {
 		let foundItemList = JSON.parse(balance.items);
 		let foundBoostList = JSON.parse(balance.boosts);
 		let foundPlotList = JSON.parse(balance.farmPlot);
+		let foundHarvestList = JSON.parse(balance.harvestedCrops);
 
 		if (!foundBoostList) {
 			foundBoostList = {};
@@ -50,6 +51,10 @@ module.exports = class extends Command {
 
 		if (!foundPlotList) {
 			foundPlotList = [];
+		}
+
+		if (!foundHarvestList) {
+			foundHarvestList = [];
 		}
 
 		let troutPrice;
@@ -76,10 +81,10 @@ module.exports = class extends Command {
 		if (foundItemList.treasure) treasurePrice = this.client.ecoPrices.treasure * Number(foundItemList.treasure);
 
 		if (foundItemList.goldBar) goldBarPrice = this.client.ecoPrices.goldBar * Number(foundItemList.goldBar);
-		if (foundPlotList.corn) cornPrice = this.client.ecoPrices.corn * Number(foundPlotList.corn);
-		if (foundPlotList.corn) wheatPrice = this.client.ecoPrices.wheat * Number(foundPlotList.corn);
-		if (foundPlotList.potato) potatoesPrice = this.client.ecoPrices.potatoes * Number(foundPlotList.potato);
-		if (foundPlotList.tomato) tomatoesPrice = this.client.ecoPrices.tomatoes * Number(foundPlotList.tomato);
+		if (foundHarvestList.filter(key => key.cropType === 'corn').length) cornPrice = this.client.ecoPrices.corn * Number(foundHarvestList.filter(key => key.cropType === 'corn').length);
+		if (foundHarvestList.filter(key => key.cropType === 'wheat').length) wheatPrice = this.client.ecoPrices.wheat * Number(foundHarvestList.filter(key => key.cropType === 'wheat').length);
+		if (foundHarvestList.filter(key => key.cropType === 'potato').length) potatoesPrice = this.client.ecoPrices.potatoes * Number(foundHarvestList.filter(key => key.cropType === 'potato').length);
+		if (foundHarvestList.filter(key => key.cropType === 'tomato').length) tomatoesPrice = this.client.ecoPrices.tomatoes * Number(foundHarvestList.filter(key => key.cropType === 'tomato').length);
 		if (foundItemList.goldNugget) goldNuggetPrice = this.client.ecoPrices.goldNugget * Number(foundItemList.goldNugget);
 		if (foundItemList.barley) barleyPrice = this.client.ecoPrices.barley * Number(foundItemList.barley);
 		if (foundItemList.spinach) spinachPrice = this.client.ecoPrices.spinach * Number(foundItemList.spinach);
@@ -94,10 +99,12 @@ module.exports = class extends Command {
 		if (foundItemList.treasure) fullPrice += Number(foundItemList.treasure) * this.client.ecoPrices.treasure;
 
 		if (foundItemList.goldBar) fullPrice += Number(foundItemList.goldBar) * this.client.ecoPrices.goldBar;
-		if (foundPlotList.corn) fullPrice += Number(foundPlotList.corn) * this.client.ecoPrices.corn;
-		if (foundPlotList.corn) fullPrice += Number(foundPlotList.corn) * this.client.ecoPrices.wheat;
-		if (foundPlotList.potato) fullPrice += Number(foundPlotList.potato) * this.client.ecoPrices.potatoes;
-		if (foundPlotList.tomato) fullPrice += Number(foundPlotList.tomato) * this.client.ecoPrices.tomatoes;
+		if (foundHarvestList) {
+			fullPrice += Number(foundHarvestList.filter(key => key.cropType === 'corn').length) * this.client.ecoPrices.corn;
+			fullPrice += Number(foundHarvestList.filter(key => key.cropType === 'wheat').length) * this.client.ecoPrices.wheat;
+			fullPrice += Number(foundHarvestList.filter(key => key.cropType === 'potato').length) * this.client.ecoPrices.potatoes;
+			fullPrice += Number(foundHarvestList.filter(key => key.cropType === 'tomato').length) * this.client.ecoPrices.tomatoes;
+		}
 		if (foundItemList.goldNugget) fullPrice += Number(foundItemList.goldNugget) * this.client.ecoPrices.goldNugget;
 		if (foundItemList.barley) fullPrice += Number(foundItemList.barley) * this.client.ecoPrices.barley;
 		if (foundItemList.spinach) fullPrice += Number(foundItemList.spinach) * this.client.ecoPrices.spinach;
@@ -152,25 +159,11 @@ module.exports = class extends Command {
 
 		let currentTotalFarm = 0;
 
-		if (foundPlotList.corn) {
-			currentTotalFarm += Number(foundPlotList.corn);
-		} else {
-			currentTotalFarm += Number(0);
-		}
-		if (foundPlotList.corn) {
-			currentTotalFarm += Number(foundPlotList.corn);
-		} else {
-			currentTotalFarm += Number(0);
-		}
-		if (foundPlotList.potato) {
-			currentTotalFarm += Number(foundPlotList.potato);
-		} else {
-			currentTotalFarm += Number(0);
-		}
-		if (foundPlotList.tomato) {
-			currentTotalFarm += Number(foundPlotList.tomato);
-		} else {
-			currentTotalFarm += Number(0);
+		if (foundHarvestList) {
+			currentTotalFarm += Number(foundHarvestList.filter(key => key.cropType === 'corn').length);
+			currentTotalFarm += Number(foundHarvestList.filter(key => key.cropType === 'wheat').length);
+			currentTotalFarm += Number(foundHarvestList.filter(key => key.cropType === 'potato').length);
+			currentTotalFarm += Number(foundHarvestList.filter(key => key.cropType === 'tomato').length);
 		}
 
 		let fields;
@@ -186,10 +179,10 @@ module.exports = class extends Command {
 		} else {
 			fields = [
 				`**◎ Crops:**`,
-				`\u3000 Corn: Own ${foundPlotList.corn === undefined ? `\`0\`` : `\`${foundPlotList.corn}\` - <:coin:706659001164628008> \`${cornPrice.toLocaleString('en')}\``}`,
-				`\u3000 Wheat: Own ${foundPlotList.corn === undefined ? `\`0\`` : `\`${foundPlotList.corn}\` - <:coin:706659001164628008> \`${wheatPrice.toLocaleString('en')}\``}`,
-				`\u3000 Potatoes: Own ${foundPlotList.potato === undefined ? `\`0\`` : `\`${foundPlotList.potato} \`- <:coin:706659001164628008> \`${potatoesPrice.toLocaleString('en')}\``}`,
-				`\u3000 Tomatoes: Own ${foundPlotList.tomato === undefined ? `\`0\`` : `\`${foundPlotList.tomato}\` - <:coin:706659001164628008> \`${tomatoesPrice.toLocaleString('en')}\``}`,
+				`\u3000 Corn: Own ${!foundHarvestList.filter(key => key.cropType === 'corn').length ? `\`0\`` : `\`${foundHarvestList.filter(key => key.cropType === 'corn').length}\` - <:coin:706659001164628008> \`${cornPrice.toLocaleString('en')}\``}`,
+				`\u3000 Wheat: Own ${!foundHarvestList.filter(key => key.cropType === 'wheat').length ? `\`0\`` : `\`${foundHarvestList.filter(key => key.cropType === 'wheat').length}\` - <:coin:706659001164628008> \`${wheatPrice.toLocaleString('en')}\``}`,
+				`\u3000 Potatoes: Own ${!foundHarvestList.filter(key => key.cropType === 'potato').length ? `\`0\`` : `\`${foundHarvestList.filter(key => key.cropType === 'potato').length}\` - <:coin:706659001164628008> \`${potatoesPrice.toLocaleString('en')}\``}`,
+				`\u3000 Tomatoes: Own ${!foundHarvestList.filter(key => key.cropType === 'tomato').length ? `\`0\`` : `\`${foundHarvestList.filter(key => key.cropType === 'tomato').length}\` - <:coin:706659001164628008> \`${tomatoesPrice.toLocaleString('en')}\``}`,
 				`\u200b`,
 				`**◎ Seeds:**`,
 				`\u3000 Corn: Own ${foundItemList.cornSeeds === undefined ? `\`0\`` : `\`${foundItemList.cornSeeds}\``}`,
@@ -199,36 +192,34 @@ module.exports = class extends Command {
 			];
 		}
 
-		if (!args.length) {
-			const embed = new MessageEmbed()
-				.setAuthor(`${message.author.tag}`, message.author.avatarURL())
-				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
-				.addField(`**${this.client.user.username} - Items**`, [
-					`**◎ Fish:**`,
-					`\u3000 Trout: Own ${foundItemList.trout === undefined ? `\`0\`` : `\`${foundItemList.trout}\` - <:coin:706659001164628008> \`${troutPrice.toLocaleString('en')}\``}`,
-					`\u3000 King Salmon: Own ${foundItemList.kingSalmon === undefined ? `\`0\`` : `\`${foundItemList.kingSalmon}\` - <:coin:706659001164628008> \`${salmonPrice.toLocaleString('en')}\``}`,
-					`\u3000 Swordfish: Own ${foundItemList.swordfish === undefined ? `\`0\`` : `\`${foundItemList.swordfish} \`- <:coin:706659001164628008> \`${swordFishPrice.toLocaleString('en')}\``}`,
-					`\u3000 Pufferfish: Own ${foundItemList.pufferfish === undefined ? `\`0\`` : `\`${foundItemList.pufferfish}\` - <:coin:706659001164628008> \`${pufferFishPrice.toLocaleString('en')}\``}`,
-					`\u200b`,
-					`${fields.join('\n')}`,
-					`\u200b`,
-					`**◎ Treasure:**`,
-					`\u3000 Treasure Chest: Own ${foundItemList.treasure === undefined ? `\`0\`` : `\`${foundItemList.treasure}\` - <:coin:706659001164628008> \`${treasurePrice.toLocaleString('en')}\``}`,
-					`\u3000 Gold Bar: Own ${foundItemList.goldBar === undefined ? `\`0\`` : `\`${foundItemList.goldBar}\` - <:coin:706659001164628008> \`${goldBarPrice.toLocaleString('en')}\``}`,
-					`\u3000 Gold Nugget: Own ${foundItemList.goldNugget === undefined ? `\`0\`` : `\`${foundItemList.goldNugget}\` - <:coin:706659001164628008> \`${goldNuggetPrice.toLocaleString('en')}\``}`,
-					`\u200b`,
-					`**◎ Permanent Items:**`,
-					`\u3000 ${!foundItemList.fishingRod ? `\`${prefix}shop buy rod\` - <:coin:706659001164628008> \`${fishingPrice.toLocaleString('en')}\`` : `Fishing Rod - \`Owned\``}`,
-					`\u3000 Fish Bag - ${!foundBoostList.fishBag ? `\`Not Owned\` - Buy fishing rod to aquire` : `\`Owned\` - Current capacity: \`${Number(currentTotalFish)}\`/\`${foundBoostList.fishBag}\``}`,
-					`\u3000 ${!foundItemList.farmingTools ? `\`${prefix}shop buy tools\` - <:coin:706659001164628008> \`${farmingPrice.toLocaleString('en')}\`` : `Farming Tools - \`Owned\``}`,
-					`\u3000 Seed Bag - ${!foundBoostList.seedBag ? `\`Not Owned\` - Buy farming tools to aquire` : `\`Owned\` - Current capacity: \`${Number(currentTotalSeeds)}\`/\`${foundBoostList.seedBag}\``}`,
-					`\u3000 Farm Bag - ${!foundBoostList.farmBag ? `\`Not Owned\` - Buy farming tools to aquire` : `\`Owned\` - Current capacity: \`${Number(currentTotalFarm)}\`/\`${foundBoostList.farmBag}\``}`
-				]);
-			if (fullPrice > 0) {
-				embed.setFooter(`Total Value: ${fullPrice.toLocaleString('en')}`);
-			}
-			message.channel.send(embed);
+		const embed = new MessageEmbed()
+			.setAuthor(`${message.author.tag}`, message.author.avatarURL())
+			.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+			.addField(`**${this.client.user.username} - Items**`, [
+				`**◎ Fish:**`,
+				`\u3000 Trout: Own ${foundItemList.trout === undefined ? `\`0\`` : `\`${foundItemList.trout}\` - <:coin:706659001164628008> \`${troutPrice.toLocaleString('en')}\``}`,
+				`\u3000 King Salmon: Own ${foundItemList.kingSalmon === undefined ? `\`0\`` : `\`${foundItemList.kingSalmon}\` - <:coin:706659001164628008> \`${salmonPrice.toLocaleString('en')}\``}`,
+				`\u3000 Swordfish: Own ${foundItemList.swordfish === undefined ? `\`0\`` : `\`${foundItemList.swordfish} \`- <:coin:706659001164628008> \`${swordFishPrice.toLocaleString('en')}\``}`,
+				`\u3000 Pufferfish: Own ${foundItemList.pufferfish === undefined ? `\`0\`` : `\`${foundItemList.pufferfish}\` - <:coin:706659001164628008> \`${pufferFishPrice.toLocaleString('en')}\``}`,
+				`\u200b`,
+				`${fields.join('\n')}`,
+				`\u200b`,
+				`**◎ Treasure:**`,
+				`\u3000 Treasure Chest: Own ${foundItemList.treasure === undefined ? `\`0\`` : `\`${foundItemList.treasure}\` - <:coin:706659001164628008> \`${treasurePrice.toLocaleString('en')}\``}`,
+				`\u3000 Gold Bar: Own ${foundItemList.goldBar === undefined ? `\`0\`` : `\`${foundItemList.goldBar}\` - <:coin:706659001164628008> \`${goldBarPrice.toLocaleString('en')}\``}`,
+				`\u3000 Gold Nugget: Own ${foundItemList.goldNugget === undefined ? `\`0\`` : `\`${foundItemList.goldNugget}\` - <:coin:706659001164628008> \`${goldNuggetPrice.toLocaleString('en')}\``}`,
+				`\u200b`,
+				`**◎ Permanent Items:**`,
+				`\u3000 ${!foundItemList.fishingRod ? `\`${prefix}shop buy rod\` - <:coin:706659001164628008> \`${fishingPrice.toLocaleString('en')}\`` : `Fishing Rod - \`Owned\``}`,
+				`\u3000 Fish Bag - ${!foundBoostList.fishBag ? `\`Not Owned\` - Buy fishing rod to aquire` : `\`Owned\` - Current capacity: \`${Number(currentTotalFish)}\`/\`${foundBoostList.fishBag}\``}`,
+				`\u3000 ${!foundItemList.farmingTools ? `\`${prefix}shop buy tools\` - <:coin:706659001164628008> \`${farmingPrice.toLocaleString('en')}\`` : `Farming Tools - \`Owned\``}`,
+				`\u3000 Seed Bag - ${!foundBoostList.seedBag ? `\`Not Owned\` - Buy farming tools to aquire` : `\`Owned\` - Current capacity: \`${Number(currentTotalSeeds)}\`/\`${foundBoostList.seedBag}\``}`,
+				`\u3000 Farm Bag - ${!foundBoostList.farmBag ? `\`Not Owned\` - Buy farming tools to aquire` : `\`Owned\` - Current capacity: \`${Number(currentTotalFarm)}\`/\`${foundBoostList.farmBag}\``}`
+			]);
+		if (fullPrice > 0) {
+			embed.setFooter(`Total Value: ${fullPrice.toLocaleString('en')}`);
 		}
+		message.channel.send(embed);
 	}
 
 };
