2731b4dcabfb1034f869991f07ad9990a78d7a27
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2021-06-01 17:48:37 +0100

    rewrite djs command

--- src/Commands/Fun/Djs.js
+++ src/Commands/Fun/Djs.js
@@ -1,6 +1,10 @@
+/* eslint-disable prefer-destructuring */
 const Command = require('../../Structures/Command');
 const fetch = require('node-fetch');
 const { MessageEmbed } = require('discord.js');
+const { URLSearchParams } = require('url');
+
+const API_URL = 'https://djsdocs.sorta.moe/v2/embed';
 
 module.exports = class extends Command {
 
@@ -8,12 +12,12 @@ module.exports = class extends Command {
 		super(...args, {
 			description: 'Displays search results in the D.js documentation.',
 			category: 'Fun',
-			usage: '<input>'
+			usage: '<query> [--type stable|master|rpc|commando]'
 		});
 	}
 
-	async run(message, ...query) {
-		if (!query) {
+	async run(message, args) {
+		if (!args[0]) {
 			this.client.utils.messageDelete(message, 10000);
 
 			const noInput = new MessageEmbed()
@@ -24,39 +28,45 @@ module.exports = class extends Command {
 			return;
 		}
 
-		const url = `https://djsdocs.sorta.moe/v2/embed?src=stable&q=${encodeURIComponent(query)}`;
-
-		const docFetch = await fetch(url);
-		const embed = await docFetch.json();
-
-		if (!embed || embed.error) {
-			this.client.utils.messageDelete(message, 10000);
-
-			const noResult = new MessageEmbed()
+		if (args[0] === 'djs') {
+			const noInput = new MessageEmbed()
 				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
-				.addField(`**${this.client.user.username} - Discord.js**`,
-					`**◎ Error:** I found no results for\n\`${query}\``);
-			message.channel.send(noResult).then((m) => this.client.utils.deletableCheck(m, 10000));
+				.setDescription(`**${this.client.user.username} - Discord.js**`)
+				.setImage('https://i.imgflip.com/2guzlr.jpg');
+			message.channel.send(noInput);
 			return;
 		}
 
-		const msg = await message.channel.send({ embed });
-		msg.react('🗑');
+		const types = ['stable', 'master', 'rpc', 'commando', 'akairo-master'];
+		let type;
 
-		let react;
-		try {
-			react = await msg.awaitReactions(
-				(reaction, user) => reaction.emoji.name === '🗑' && user.id === message.author.id,
-				{ max: 1, time: 10000, errors: ['time'] }
-			);
-		} catch (error) {
-			msg.reactions.removeAll();
+		if (args[1] === '--type' && types.includes(args[2])) {
+			type = args[2];
+		} else {
+			type = 'stable';
 		}
 
-		if (react && react.first()) {
-			this.client.utils.deletableCheck(msg, 0);
-			this.client.utils.messageDelete(message, 0);
+		const qs = new URLSearchParams({
+			src: type,
+			q: args[0].replace(/#/g, '.'),
+			force: 'false'
+		});
+
+		const res = await fetch(`${API_URL}?${qs}`).then(r => r.json());
+		if (!res) {
+			this.client.utils.messageDelete(message, 10000);
+
+			const noInput = new MessageEmbed()
+				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+				.addField(`**${this.client.user.username} - Discord.js**`,
+					`**◎ Error:** I couldn't find anything for \`${args[0]}\``);
+			message.channel.send(noInput).then((m) => this.client.utils.deletableCheck(m, 10000));
+			return;
 		}
+
+		const embed = new MessageEmbed(res).setColor(this.client.utils.color(message.guild.me.displayHexColor));
+
+		message.channel.send(embed);
 	}
 
 };
