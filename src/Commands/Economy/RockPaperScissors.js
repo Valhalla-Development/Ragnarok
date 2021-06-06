022dea3801c6133cfbe3e056079b15ffc2462d0c
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2021-06-06 20:29:36 +0100

    new rock paper scissors command

---
+++ src/Commands/Economy/RockPaperScissors.js
@@ -0,0 +1,285 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable no-unused-expressions */
+const Command = require('../../Structures/Command');
+const { MessageEmbed } = require('discord.js');
+const SQLite = require('better-sqlite3');
+const db = new SQLite('./Storage/DB/db.sqlite');
+const { MessageButton, MessageActionRow } = require('discord-buttons');
+const comCooldown = new Set();
+const comCooldownSeconds = 10;
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['rps'],
+			description: 'Play a game of Rock Paper Scissors!',
+			category: 'Economy'
+		});
+	}
+
+	async run(message, args) {
+		const prefixgrab = db.prepare('SELECT prefix FROM setprefix WHERE guildid = ?').get(message.guild.id);
+		const { prefix } = prefixgrab;
+
+		const balance = this.client.getBalance.get(`${message.author.id}-${message.guild.id}`);
+
+		if (!balance) {
+			this.client.utils.messageDelete(message, 10000);
+
+			const limitE = new MessageEmbed()
+				.setAuthor(`${message.author.tag}`, message.author.avatarURL())
+				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+				.addField(`**${this.client.user.username} - Coin Flip**`,
+					`**◎ Error:** You do not have any balance!`);
+			message.channel.send(limitE).then((m) => this.client.utils.deletableCheck(m, 10000));
+			return;
+		}
+
+		if (!args[0]) {
+			this.client.utils.messageDelete(message, 10000);
+
+			const embed = new MessageEmbed()
+				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+				.addField(`**${this.client.user.username} - Coin Flip**`,
+					`**◎ Error:** Please input an amount you wish to bet.`);
+			message.channel.send(embed).then((m) => this.client.utils.deletableCheck(m, 10000));
+			return;
+		}
+
+		if (comCooldown.has(message.author.id)) {
+			this.client.utils.messageDelete(message, 10000);
+
+			const embed = new MessageEmbed()
+				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+				.addField(`**${this.client.user.username} - Coin Flip**`,
+					`**◎ Error:** You can only run one instance of this game!.`);
+			message.channel.send(embed).then((m) => this.client.utils.deletableCheck(m, 10000));
+			return;
+		}
+
+		let rps;
+
+		isNaN(args[0]) && args[0] === 'all' ? rps = balance.bank : rps = args[0];
+
+		if (isNaN(rps)) {
+			this.client.utils.messageDelete(message, 10000);
+
+			const wrongUsage = new MessageEmbed()
+				.setAuthor(`${message.author.tag}`, message.author.avatarURL())
+				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+				.addField(`**${this.client.user.username} - Coin Flip**`,
+					`**◎ Error:** An example of this command is: \`${prefix}rps 100\`\nAlternatively, you can run \`${prefix}rps all\``);
+			message.channel.send(wrongUsage).then((m) => this.client.utils.deletableCheck(m, 10000));
+			return;
+		}
+
+		if (Number(rps) < 10) {
+			this.client.utils.messageDelete(message, 10000);
+
+			const wrongUsage = new MessageEmbed()
+				.setAuthor(`${message.author.tag}`, message.author.avatarURL())
+				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+				.addField(`**${this.client.user.username} - Coin Flip**`,
+					`**◎ Error:** Please enter a value of at least <:coin:706659001164628008> \`10\`. Please try again with a valid amount.`);
+			message.channel.send(wrongUsage).then((m) => this.client.utils.deletableCheck(m, 10000));
+			return;
+		}
+
+		if (Number(rps) > balance.bank) {
+			this.client.utils.messageDelete(message, 10000);
+
+			const wrongUsage = new MessageEmbed()
+				.setAuthor(`${message.author.tag}`, message.author.avatarURL())
+				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+				.addField(`**${this.client.user.username} - Coin Flip**`,
+					`**◎ Error:** You do not have enough to bet <:coin:706659001164628008> \`${Number(rps).toLocaleString('en')}\`, you have <:coin:706659001164628008> \`${Number(balance.bank).toLocaleString('en')}\` available in your bank.`);
+			message.channel.send(wrongUsage).then((m) => this.client.utils.deletableCheck(m, 10000));
+			return;
+		}
+
+		const Rock = new MessageButton()
+			.setStyle('blurple')
+			.setEmoji('🪨')
+			.setLabel('Rock')
+			.setID('rock');
+
+		const Paper = new MessageButton()
+			.setStyle('blurple')
+			.setEmoji('🧻')
+			.setLabel('Paper')
+			.setID('paper');
+
+		const Scissors = new MessageButton()
+			.setStyle('blurple')
+			.setEmoji('✂️')
+			.setLabel('Scissors')
+			.setID('scissors');
+
+		const Cancel = new MessageButton()
+			.setStyle('red')
+			.setLabel('Cancel')
+			.setID('cancel');
+
+		const RockNew = new MessageButton()
+			.setStyle('blurple')
+			.setEmoji('🪨')
+			.setLabel('Rock')
+			.setID('rock')
+			.setDisabled();
+
+		const PaperNew = new MessageButton()
+			.setStyle('blurple')
+			.setEmoji('🧻')
+			.setLabel('Paper')
+			.setID('paper')
+			.setDisabled();
+
+		const ScissorsNew = new MessageButton()
+			.setStyle('blurple')
+			.setEmoji('✂️')
+			.setLabel('Scissors')
+			.setID('scissors')
+			.setDisabled();
+
+		const CancelNew = new MessageButton()
+			.setStyle('red')
+			.setLabel('Cancel')
+			.setID('cancel')
+			.setDisabled();
+
+		const houseBet = Number(rps);
+
+		const choices = ['rock', 'paper', 'scissors'];
+		const index = Math.floor(Math.random() * 3);
+		const ai = choices[index];
+
+		const group1 = new MessageActionRow().addComponents([Rock, Paper, Scissors, Cancel]);
+
+		const group2 = new MessageActionRow().addComponents([[RockNew, PaperNew, ScissorsNew, CancelNew]]);
+
+		const initial = new MessageEmbed()
+			.setAuthor(`${message.author.tag}`, message.author.avatarURL())
+			.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+			.addField(`**${this.client.user.username} - Rock Paper Scissors**`,
+				`**◎** ${message.author} bet <:coin:706659001164628008> \`${Number(rps).toLocaleString('en')}\``);
+
+		const win = new MessageEmbed()
+			.setAuthor(`${message.author.tag}`, message.author.avatarURL())
+			.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+			.addField(`**${this.client.user.username} - Rock Paper Scissors**`,
+				`**◎** ${message.author} Won! <:coin:706659001164628008> \`${houseBet.toLocaleString('en')}\` has been credited to your bank!`);
+
+		const lose = new MessageEmbed()
+			.setAuthor(`${message.author.tag}`, message.author.avatarURL())
+			.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+			.addField(`**${this.client.user.username} - Rock Paper Scissors**`,
+				`**◎** ${message.author} Lost! <:coin:706659001164628008> \`${rps.toLocaleString('en')}\``);
+
+		const tie = new MessageEmbed()
+			.setAuthor(`${message.author.tag}`, message.author.avatarURL())
+			.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+			.addField(`**${this.client.user.username} - Rock Paper Scissors**`,
+				`**◎** ${message.author} Tied! Your wager has been returned to your bank.`);
+
+		message.channel.send({ components: [group1], embed: initial }).then((m) => {
+			const filter = (button) => button.clicker.user.id === message.author.id;
+
+			const collector = m.createButtonCollector(filter, {
+				time: 30000
+			});
+
+			if (!comCooldown.has(message.author.id)) {
+				comCooldown.add(message.author.id);
+			}
+			setTimeout(() => {
+				if (comCooldown.has(message.author.id)) {
+					comCooldown.delete(message.author.id);
+				}
+			}, comCooldownSeconds * 1000);
+
+			collector.on('collect', async (button) => {
+				if (button.id === 'cancel') {
+					collector.stop('cancel');
+				}
+
+				switch (ai) {
+					case 'rock':
+						if (button.id === 'rock') {
+							m.edit({ component: group2, embed: tie });
+							collector.stop('tie');
+						}
+						if (button.id === 'paper') {
+							m.edit({ component: group2, embed: win });
+							collector.stop('win');
+						}
+						if (button.id === 'scissors') {
+							m.edit({ component: group2, embed: lose });
+							collector.stop('lose');
+						}
+						break;
+					case 'paper':
+						if (button.id === 'rock') {
+							m.edit({ component: group2, embed: lose });
+							collector.stop('lose');
+						}
+						if (button.id === 'paper') {
+							m.edit({ component: group2, embed: tie });
+							collector.stop('tie');
+						}
+						if (button.id === 'scissors') {
+							m.edit({ component: group2, embed: win });
+							collector.stop('win');
+						}
+						collector.stop('gameEnd');
+						break;
+					case 'scissors':
+						if (button.id === 'rock') {
+							m.edit({ component: group2, embed: win });
+							collector.stop('win');
+						}
+						if (button.id === 'paper') {
+							m.edit({ component: group2, embed: lose });
+							collector.stop('lose');
+						}
+						if (button.id === 'scissors') {
+							m.edit({ component: group2, embed: tie });
+							collector.stop('tie');
+						}
+						collector.stop('gameEnd');
+						break;
+				}
+			});
+			collector.on('end', (_, reason) => {
+				if (comCooldown.has(message.author.id)) {
+					comCooldown.delete(message.author.id);
+				}
+
+				if (reason === 'win') {
+					balance.bank += houseBet;
+					balance.total += houseBet;
+					this.client.setBalance.run(balance);
+				}
+				if (reason === 'lose') {
+					balance.bank -= rps;
+					balance.total -= rps;
+					this.client.setBalance.run(balance);
+				}
+
+				if (reason === 'cancel' || reason === 'time') {
+					this.client.utils.messageDelete(message, 0);
+					this.client.utils.messageDelete(m, 0);
+
+					const limitE = new MessageEmbed()
+						.setAuthor(`${message.author.tag}`, message.author.avatarURL())
+						.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+						.addField(`**${this.client.user.username} - Coin Flip**`,
+							`**◎ Success:** Your bet was cancelled, your money has been returned.`);
+					message.channel.send(limitE).then((ca) => this.client.utils.deletableCheck(ca, 10000));
+					return;
+				}
+			});
+		});
+	}
+
+};
