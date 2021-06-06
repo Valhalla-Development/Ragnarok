282ba7c132dd7a883f2c949b6c47182e4ee3bda4
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2021-06-06 19:29:27 +0100

    added button to invite command

--- src/Commands/Informative/Invite.js
+++ src/Commands/Informative/Invite.js
@@ -1,4 +1,5 @@
 const Command = require('../../Structures/Command');
+const { MessageButton, MessageActionRow } = require('discord-buttons');
 const { MessageEmbed } = require('discord.js');
 
 module.exports = class extends Command {
@@ -14,9 +15,18 @@ module.exports = class extends Command {
 	async run(message) {
 		const embed = new MessageEmbed()
 			.setColor(this.client.utils.color(message.guild.me.displayHexColor))
-			.addField(`**${this.client.user.username} - Invite**`,
-				`**◎ [Bot Invite Link](https://discordapp.com/oauth2/authorize?client_id=${this.client.user.id}&scope=bot%20applications.commands&permissions=1580723711)**`);
-		message.channel.send(embed);
+			.addField(`**${this.client.user.username} - Ping**`,
+				`Want to invite ${this.client.user}?`);
+
+		const buttonA = new MessageButton()
+			.setStyle('url')
+			.setLabel('Invite')
+			.setURL('https://discordapp.com/oauth2/authorize?client_id=508756879564865539&scope=bot%20applications.commands&permissions=1580723711');
+
+		const row = new MessageActionRow()
+			.addComponent(buttonA);
+
+		await message.channel.send({ component: row, embed: embed });
 	}
 
 };
