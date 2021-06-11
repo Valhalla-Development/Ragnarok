a60538397c82cbfa2ecf794f30222a8635ebccda
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2021-06-11 20:42:39 +0100

    add new generators commands

---
+++ src/Commands/Generators/Bed.js
@@ -0,0 +1,35 @@
+const Command = require('../../Structures/Command');
+const { MessageEmbed, MessageAttachment } = require('discord.js');
+const DIG = require('discord-image-generation');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			description: 'Generate a bed image!',
+			category: 'Generators',
+			usage: '<@tag>'
+		});
+	}
+
+	async run(message) {
+		this.client.utils.messageDelete(message, 0);
+
+		const user = message.mentions.members.first();
+
+		if (!user) {
+			const incorrectFormat = new MessageEmbed()
+				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+				.addField(`**${this.client.user.username} - Bed**`,
+					`**◎ Error:** Incorrect usage! Please tag a user!`);
+			message.channel.send(incorrectFormat).then((m) => this.client.utils.deletableCheck(m, 10000));
+			return;
+		}
+
+		const img = await new DIG.Bed().getImage(message.author.displayAvatarURL({ dynamic: false, format: 'png' }), user.user.displayAvatarURL({ dynamic: false, format: 'png' }));
+		const attach = new MessageAttachment(img, 'Bed.png');
+		message.channel.send(attach);
+		return;
+	}
+
+};
---
+++ src/Commands/Generators/Jail.js
@@ -0,0 +1,31 @@
+const Command = require('../../Structures/Command');
+const { MessageAttachment } = require('discord.js');
+const DIG = require('discord-image-generation');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			description: 'Generate a Jail image!',
+			category: 'Generators',
+			usage: '[@tag]'
+		});
+	}
+
+	async run(message) {
+		this.client.utils.messageDelete(message, 0);
+		let avatar;
+
+		if (message.mentions.members.first()) {
+			avatar = message.mentions.members.first().user.displayAvatarURL({ dynamic: false, format: 'png' });
+		} else {
+			avatar = message.author.displayAvatarURL({ dynamic: false, format: 'png' });
+		}
+
+		const img = await new DIG.Jail().getImage(avatar);
+		const attach = new MessageAttachment(img, 'Jail.png');
+		message.channel.send(attach);
+		return;
+	}
+
+};
