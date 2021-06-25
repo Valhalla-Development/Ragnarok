d3792796d04c842b10d478906d1a47d52e6aaca2
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2021-06-25 16:15:38 +0100

    migrated invite to native buttons

--- src/Commands/Informative/Invite.js
+++ src/Commands/Informative/Invite.js
@@ -1,5 +1,5 @@
 const Command = require('../../Structures/Command');
-const { MessageButton, MessageActionRow } = require('discord-buttons');
+const { MessageButton, MessageActionRow } = require('discord.js');
 const { MessageEmbed } = require('discord.js');
 
 module.exports = class extends Command {
@@ -19,14 +19,14 @@ module.exports = class extends Command {
 				`Want to invite ${this.client.user}?`);
 
 		const buttonA = new MessageButton()
-			.setStyle('url')
+			.setStyle('LINK')
 			.setLabel('Invite')
 			.setURL('https://discordapp.com/oauth2/authorize?client_id=508756879564865539&scope=bot%20applications.commands&permissions=1580723711');
 
 		const row = new MessageActionRow()
-			.addComponent(buttonA);
+			.addComponents(buttonA);
 
-		await message.channel.send({ component: row, embeds: [embed] });
+		await message.channel.send({ components: [row], embeds: [embed] });
 	}
 
 };
