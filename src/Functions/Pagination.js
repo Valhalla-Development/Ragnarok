06a20adb18e599cdd90bfa9b3b0604f9604f79c1
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2021-06-25 15:37:38 +0100

    wrote my own pagination!

---
+++ src/Functions/Pagination.js
@@ -0,0 +1,101 @@
+const { MessageButton, MessageActionRow } = require('discord.js');
+
+module.exports = class pagination {
+
+	constructor(client) {
+		this.client = client;
+	}
+
+	async pagination(message, embeds, emojiNext, emojiHome, emojiBack) {
+		const back = new MessageButton()
+			.setCustomID('back')
+			.setEmoji(emojiBack || '◀️')
+			.setStyle('PRIMARY')
+			.setDisabled(true);
+
+		const home = new MessageButton()
+			.setCustomID('home')
+			.setEmoji(emojiHome || '🏠')
+			.setStyle('PRIMARY')
+			.setDisabled(true);
+
+		const next = new MessageButton()
+			.setCustomID('next')
+			.setEmoji(emojiNext || '▶️')
+			.setStyle('PRIMARY');
+
+		const row = new MessageActionRow()
+			.addComponents(back, home, next);
+
+		const m = await message.channel.send({ embeds: [embeds[0]], components: [row] });
+
+		const filter = i => i.user.id === message.author.id;
+
+		const collector = m.createMessageComponentInteractionCollector(filter, { time: 30000 });
+
+		let currentPage = 0;
+
+		collector.on('collect', async b => {
+			collector.resetTimer();
+
+			if (b.customID === 'back') {
+				if (currentPage !== 0) {
+					if (currentPage === embeds.length - 1) {
+						next.setDisabled(false);
+					}
+
+					--currentPage;
+
+					if (currentPage === 0) {
+						back.setDisabled(true);
+						home.setDisabled(true);
+					}
+
+					const rowNew = new MessageActionRow()
+						.addComponents(back, home, next);
+
+					await b.update({ embeds: [embeds[currentPage]], components: [rowNew] });
+				}
+			}
+
+			if (b.customID === 'next') {
+				if (currentPage < embeds.length - 1) {
+					currentPage++;
+
+					if (currentPage === embeds.length - 1) {
+						next.setDisabled(true);
+					}
+
+					home.setDisabled(false);
+					back.setDisabled(false);
+
+					const rowNew = new MessageActionRow()
+						.addComponents(back, home, next);
+
+					await b.update({ embeds: [embeds[currentPage]], components: [rowNew] });
+				}
+			}
+
+			if (b.customID === 'home') {
+				currentPage = 0;
+				home.setDisabled(true);
+				back.setDisabled(true);
+				next.setDisabled(false);
+
+				const rowNew = new MessageActionRow()
+					.addComponents(back, home, next);
+
+				await b.update({ embeds: [embeds[currentPage]], components: [rowNew] });
+			}
+		});
+
+		collector.on('end', () => {
+			if (m && m.deletable) {
+				m.delete();
+			}
+		});
+
+		collector.on('error', (e) => console.log(e));
+	}
+
+};
