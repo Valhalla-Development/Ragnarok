675a319c35cfa9fca6f930aba4a2c40fe7a0383b
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2021-08-13 18:45:25 +0100

    update discord intents

--- src/Structures/RagnarokClient.js
+++ src/Structures/RagnarokClient.js
@@ -8,7 +8,7 @@ Canvas.registerFont('./Storage/Canvas/Fonts/Notethis.ttf', {
 });
 const { GiveawaysManager } = require('discord-giveaways');
 const db = require('quick.db');
-if (!db.get('giveaways')) db.set('giveaways', []);
+if (!Array.isArray(db.get('giveaways'))) db.set('giveaways', []);
 const { Manager } = require('erela.js');
 const Spotify = require('erela.js-spotify');
 const prettyMilliseconds = require('pretty-ms');
@@ -17,7 +17,7 @@ const { stripIndents } = require('common-tags');
 module.exports = class RagnarokClient extends Client {
 
 	constructor(options = {}) {
-		super({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_EMOJIS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGE_TYPING] });
+		super({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING] });
 		this.validate(options);
 
 		this.commands = new Collection();
@@ -260,7 +260,8 @@ module.exports = class RagnarokClient extends Client {
 			}
 
 			async deleteGiveaway(messageID) {
-				const newGiveawaysArray = db.get('giveaways').filter((giveaway) => giveaway.messageID !== messageID);
+				const giveaways = db.get('giveaways');
+				const newGiveawaysArray = giveaways.filter((giveaway) => giveaway.messageID !== messageID);
 				db.set('giveaways', newGiveawaysArray);
 				return true;
 			}
@@ -269,7 +270,6 @@ module.exports = class RagnarokClient extends Client {
 
 		// Create a new instance of your new class
 		const manager = new GiveawayManagerWithOwnDatabase(this, {
-			storage: false,
 			updateCountdownEvery: 5000,
 			default: {
 				botsCanWin: false,
