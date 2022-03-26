0a6e3312a8edec09fd4d34028b902b367bc568e0
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2022-03-26 17:17:04 +0000

    new command: addRole

---
+++ src/Commands/Moderation/AddRole.js
@@ -0,0 +1,92 @@
+const Command = require('../../Structures/Command');
+const { MessageEmbed } = require('discord.js');
+const SQLite = require('better-sqlite3');
+const db = new SQLite('./Storage/DB/db.sqlite');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			description: 'Adds a role to @user/all users',
+			category: 'Moderation',
+			usage: '<@user/all> <role-id>',
+			userPerms: ['MANAGE_ROLES', 'MANAGE_GUILD'],
+			ownerOnly: true
+		});
+	}
+
+	async run(message, args) {
+		const prefixgrab = db.prepare('SELECT prefix FROM setprefix WHERE guildid = ?').get(message.guild.id);
+		const { prefix } = prefixgrab;
+
+		const member = message.mentions.members.first() || message.guild.members.cache.find(usr => usr.id === args[0]);
+
+		if (!member) {
+			this.client.utils.messageDelete(message, 10000);
+
+			const embed = new MessageEmbed()
+				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+				.addField(`**${this.client.user.username} - Add Role**`,
+					`**◎ Error:** Correct usage: \`${prefix}addrole <@user> <role-id>\` or \`${prefix}addrole <all> <role-id>\``);
+			message.channel.send({ embeds: [embed] }).then((m) => this.client.utils.deletableCheck(m, 10000));
+			return;
+		}
+
+		const role = message.mentions.roles.first();
+
+		if (!role) {
+			this.client.utils.messageDelete(message, 10000);
+
+			const embed = new MessageEmbed()
+				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+				.addField(`**${this.client.user.username} - Add Role**`,
+					`**◎ Error:** Correct usage: \`${prefix}addrole <@user> <role-id>\` or \`${prefix}addrole <all> <role-id>\``);
+			message.channel.send({ embeds: [embed] }).then((m) => this.client.utils.deletableCheck(m, 10000));
+			return;
+		}
+
+		if (role.position >= message.member.roles.highest.position) {
+			this.client.utils.messageDelete(message, 10000);
+
+			const embed = new MessageEmbed()
+				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+				.addField(`**${this.client.user.username} - Add Role**`,
+					`**◎ Error:** You cannot give a user a role that is equal or greater than your own!`);
+			message.channel.send({ embeds: [embed] }).then((m) => this.client.utils.deletableCheck(m, 10000));
+			return;
+		}
+
+		// Check if the user already has the role
+		if (member.roles.cache.has(role.id)) {
+			this.client.utils.messageDelete(message, 10000);
+
+			const embed = new MessageEmbed()
+				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+				.addField(`**${this.client.user.username} - Add Role**`,
+					`**◎ Error:** ${member} already has the role: ${role}`);
+			message.channel.send({ embeds: [embed] }).then((m) => this.client.utils.deletableCheck(m, 10000));
+			return;
+		}
+
+		try {
+			await member.roles.add(role);
+
+			const embed = new MessageEmbed()
+				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+				.addField(`**${this.client.user.username} - Add Role**`,
+					`**◎ Success:** I have added the ${role} role to ${member}`);
+			message.channel.send({ embeds: [embed] }).then((m) => this.client.utils.deletableCheck(m, 10000));
+			return;
+		} catch {
+			this.client.utils.messageDelete(message, 10000);
+
+			const embed = new MessageEmbed()
+				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+				.addField(`**${this.client.user.username} - Add Role**`,
+					`**◎ Error:** An error occured.`);
+			message.channel.send({ embeds: [embed] }).then((m) => this.client.utils.deletableCheck(m, 10000));
+			return;
+		}
+	}
+
+};
