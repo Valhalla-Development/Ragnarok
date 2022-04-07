7397046f849d4503f18180e4993d66f5f9b118b5
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2022-04-07 15:30:09 +0100

    allow using reply command with message link

--- src/Commands/Moderation/Reply.js
+++ src/Commands/Moderation/Reply.js
@@ -1,5 +1,8 @@
+/* eslint-disable consistent-return */
 const Command = require('../../Structures/Command');
 const { MessageEmbed } = require('discord.js');
+const SQLite = require('better-sqlite3');
+const db = new SQLite('./Storage/DB/db.sqlite');
 
 module.exports = class extends Command {
 
@@ -14,6 +17,9 @@ module.exports = class extends Command {
 	}
 
 	async run(message, args) {
+		const prefixgrab = db.prepare('SELECT prefix FROM setprefix WHERE guildid = ?').get(message.guild.id);
+		const { prefix } = prefixgrab;
+
 		this.client.utils.messageDelete(message, 0);
 
 		if (args[0] === undefined) {
@@ -22,8 +28,48 @@ module.exports = class extends Command {
 			const embed = new MessageEmbed()
 				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
 				.addField(`**${this.client.user.username} - Reply**`,
-					`**◎ Error:** You need to input a channel ID!\n(This is requried to prevent the bot from spamming the API, searching for a specific message within several channels.)`);
+					`**◎ Error:** Incorrect Usage, examples follow:\n\n\`${prefix}reply <channel id> <message id> <text>\`\n\`${prefix}reply <message link> <text>\``);
 			message.channel.send({ embeds: [embed] }).then((m) => this.client.utils.deletableCheck(m, 10000));
+			return;
+		}
+
+		if (returnValuesFromLink()) {
+			if (args[1] === undefined) {
+				this.client.utils.messageDelete(message, 10000);
+
+				const embed = new MessageEmbed()
+					.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+					.addField(`**${this.client.user.username} - Reply**`,
+						`**◎ Error:** You need to input text you wish to reply with!`);
+				message.channel.send({ embeds: [embed] }).then((m) => this.client.utils.deletableCheck(m, 10000));
+				return;
+			}
+
+			if (returnValuesFromLink().guildID !== message.guild.id) {
+				this.client.utils.messageDelete(message, 10000);
+
+				const embed = new MessageEmbed()
+					.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+					.addField(`**${this.client.user.username} - Reply**`,
+						`**◎ Error:** I can not find the message you linked within this server.`);
+				message.channel.send({ embeds: [embed] }).then((m) => this.client.utils.deletableCheck(m, 10000));
+				return;
+			}
+
+			const fetchChannel = message.guild.channels.cache.get(returnValuesFromLink().channelID);
+
+			fetchChannel.messages.fetch(returnValuesFromLink().messageID).then(async (msg) => {
+				msg.reply({ content: `${args.slice(1).join(' ')}` });
+			}).catch(() => {
+				this.client.utils.messageDelete(message, 10000);
+
+				const embed = new MessageEmbed()
+					.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+					.addField(`**${this.client.user.username} - Reply**`,
+						`**◎ Error:** An error occured while trying to reply to the message!\nHas the message been deleted?`);
+				message.channel.send({ embeds: [embed] }).then((m) => this.client.utils.deletableCheck(m, 10000));
+				return;
+			});
 			return;
 		}
 
@@ -63,7 +109,7 @@ module.exports = class extends Command {
 		}
 
 		channel.messages.fetch(args[1]).then(async (msg) => {
-			msg.reply(args.slice(2).join(' '));
+			msg.reply({ content: `${args.slice(2).join(' ')}` });
 		}).catch(() => {
 			this.client.utils.messageDelete(message, 10000);
 
@@ -74,6 +120,114 @@ module.exports = class extends Command {
 			message.channel.send({ embeds: [embed] }).then((m) => this.client.utils.deletableCheck(m, 10000));
 			return;
 		});
+
+		function returnValuesFromLink() {
+			const mainReg = /https:\/\/discordapp\.com\/channels\/\d{1,18}\/\d{1,18}\/\d{1,18}/;
+			const ptbReg = /https:\/\/ptb\.discordapp\.com\/channels\/\d{1,18}\/\d{1,18}\/\d{1,18}/;
+			const disReg = /https:\/\/discord\.com\/channels\/\d{1,18}\/\d{1,18}\/\d{1,18}/;
+			const ptReg = /https:\/\/ptb\.discord\.com\/channels\/\d{1,18}\/\d{1,18}\/\d{1,18}/;
+			const caReg = /https:\/\/canary\.discord\.com\/channels\/\d{1,18}\/\d{1,18}\/\d{1,18}/;
+			const canApReg = /https:\/\/canary\.discordapp\.com\/channels\/\d{1,18}\/\d{1,18}\/\d{1,18}/;
+
+			const mainCheck = mainReg.test(args[0].toLowerCase());
+			const ptbCheck = ptbReg.test(args[0].toLowerCase());
+			const disCheck = disReg.test(args[0].toLowerCase());
+			const ptbdisCheck = ptReg.test(args[0].toLowerCase());
+			const canCheck = caReg.test(args[0].toLowerCase());
+			const canACheck = canApReg.test(args[0].toLowerCase());
+
+			const exec = mainReg.exec(args[0].toLowerCase()) || ptbReg.exec(args[0].toLowerCase()) || disReg.exec(args[0].toLowerCase()) || ptReg.exec(args[0].toLowerCase()) || caReg.exec(args[0].toLowerCase()) || canApReg.exec(args[0].toLowerCase());
+
+			let guildID;
+			let channelID;
+			let messageID;
+
+			if (mainCheck || ptbCheck || disCheck || ptbdisCheck || canCheck || canACheck) {
+				const mainGlob = /https:\/\/discordapp\.com\/channels\/\d{1,18}\/\d{1,18}\/\d{1,18}/g;
+				const ptbGlob = /https:\/\/ptb\.discordapp\.com\/channels\/\d{1,18}\/\d{1,18}\/\d{1,18}/g;
+				const disGlob = /https:\/\/discord\.com\/channels\/\d{1,18}\/\d{1,18}\/\d{1,18}/g;
+				const ptGlob = /https:\/\/ptb\.discord\.com\/channels\/\d{1,18}\/\d{1,18}\/\d{1,18}/g;
+				const caGlob = /https:\/\/canary\.discord\.com\/channels\/\d{1,18}\/\d{1,18}\/\d{1,18}/g;
+				const canAGlob = /https:\/\/canary\.discordapp\.com\/channels\/\d{1,18}\/\d{1,18}\/\d{1,18}/g;
+
+
+				let lengthMain;
+				let lengthPtb;
+
+				if (mainGlob.test(args[0].toLowerCase()) === true) {
+					lengthMain = args[0].toLowerCase().match(mainGlob).length;
+				} else {
+					lengthMain = 0;
+				}
+				if (ptbGlob.test(args[0].toLowerCase()) === true) {
+					lengthPtb = args[0].toLowerCase().match(ptbGlob).length;
+				} else {
+					lengthPtb = 0;
+				}
+				if (disGlob.test(args[0].toLowerCase()) === true) {
+					lengthMain = args[0].toLowerCase().match(disGlob).length;
+				} else {
+					lengthMain = 0;
+				}
+				if (ptGlob.test(args[0].toLowerCase()) === true) {
+					lengthPtb = args[0].toLowerCase().match(ptGlob).length;
+				} else {
+					lengthPtb = 0;
+				}
+				if (caGlob.test(args[0].toLowerCase()) === true) {
+					lengthMain = args[0].toLowerCase().match(caGlob).length;
+				} else {
+					lengthMain = 0;
+				}
+				if (canAGlob.test(args[0].toLowerCase()) === true) {
+					lengthPtb = args[0].toLowerCase().match(canAGlob).length;
+				} else {
+					lengthPtb = 0;
+				}
+
+				if (lengthMain + lengthPtb > 1) return;
+
+				const mesLink = exec[0];
+				if (mainCheck) {
+					guildID = mesLink.substring(32, mesLink.length - 38);
+					channelID = mesLink.substring(51, mesLink.length - 19);
+					messageID = mesLink.substring(70);
+				} else if (ptbCheck) {
+					guildID = mesLink.substring(36, mesLink.length - 38);
+					channelID = mesLink.substring(55, mesLink.length - 19);
+					messageID = mesLink.substring(74);
+				} else if (disCheck) {
+					guildID = mesLink.substring(29, mesLink.length - 38);
+					channelID = mesLink.substring(48, mesLink.length - 19);
+					messageID = mesLink.substring(67);
+				} else if (ptbdisCheck) {
+					guildID = mesLink.substring(33, mesLink.length - 38);
+					channelID = mesLink.substring(52, mesLink.length - 19);
+					messageID = mesLink.substring(71);
+				} else if (canCheck) {
+					guildID = mesLink.substring(36, mesLink.length - 38);
+					channelID = mesLink.substring(55, mesLink.length - 19);
+					messageID = mesLink.substring(74);
+				} else if (canACheck) {
+					guildID = mesLink.substring(39, mesLink.length - 38);
+					channelID = mesLink.substring(58, mesLink.length - 19);
+					messageID = mesLink.substring(77);
+				}
+
+				let data;
+
+				if (guildID && channelID && messageID) {
+					data = {
+						guildID: guildID,
+						channelID: channelID,
+						messageID: messageID
+					};
+					return data;
+				} else {
+					return false;
+				}
+			}
+		}
 	}
 
 };
