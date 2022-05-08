0785d0ea2e36b913f12fd08d4c5b5abc37172f45
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2022-05-08 18:32:30 +0100

    fix meme button 'next meme' sending duplicate memes

--- src/Commands/Fun/Meme.js
+++ src/Commands/Fun/Meme.js
@@ -1,8 +1,8 @@
 const Command = require('../../Structures/Command');
 const { MessageEmbed, MessageButton, MessageActionRow } = require('discord.js');
-const fetch = require('node-fetch-cjs');
 const comCooldown = new Set();
 const comCooldownSeconds = 10;
+const RedditImageFetcher = require('reddit-image-fetcher');
 
 const subreddits = [
 	'memes',
@@ -12,14 +12,6 @@ const subreddits = [
 	'meirl',
 	'me_irl',
 	'funny'
-];
-
-const allowedExt = [
-	'.jpg',
-	'.jpeg',
-	'.gif',
-	'.gifv',
-	'.png'
 ];
 
 module.exports = class extends Command {
@@ -44,46 +36,26 @@ module.exports = class extends Command {
 			return;
 		}
 
+		const newMemes = await getNewMeme();
+
 		const msg = await message.channel.send({ content: 'Generating...' });
 		message.channel.sendTyping();
 
-		const res = await fetch.default(`https://www.reddit.com/r/${subreddits[Math.floor(Math.random() * subreddits.length)]}/hot.json`);
-		const { data } = await res.json();
-
-		function clean(url) {
-			const lastOf = url.lastIndexOf('.');
-			const output = url.substring(lastOf);
-			return allowedExt.includes(output);
+		async function getMeme() {
+			return RedditImageFetcher.fetch({
+				type: 'custom',
+				total: 1,
+				subreddit: subreddits
+			});
 		}
 
-		const allowed = data.children.filter((post) => clean(post.data.url));
+		const meme = await getMeme();
 
-		const safe = message.channel.nsfw ? allowed : allowed.filter((post) => !post.data.over_18);
-
-		if (!safe.length) {
-			this.client.utils.messageDelete(message, 10000);
-
-			const noPost = new MessageEmbed()
-				.setColor(this.client.utils.color(message.guild.me.displayHexColor))
-				.addField(`**${this.client.user.username} - Meme**`,
-					`**◎ Error:** I could not find a post.`);
-			message.channel.send({ embeds: [noPost] }).then((m) => this.client.utils.deletableCheck(m, 10000));
-			return;
-		}
-
-		const post = safe[Math.floor(Math.random() * safe.length)];
-		let postURL;
-
-		if (post.data.url.slice(-4) === 'gifv') {
-			postURL = post.data.url.slice(0, -1);
-		} else {
-			postURL = post.data.url;
-		}
 		const embed = new MessageEmbed()
 			.setColor(this.client.utils.color(message.guild.me.displayHexColor))
-			.setAuthor({ name: `${post.data.title}`, url: `https://reddit.com${post.data.permalink}`, iconURL: message.author.displayAvatarURL({ dynamic: true }) })
-			.setImage(postURL)
-			.setFooter({ text: `👍 ${post.data.ups} | 💬 ${post.data.num_comments}` });
+			.setAuthor({ name: `${meme[0].title}`, url: `${meme[0].postLink}`, iconURL: message.author.displayAvatarURL({ dynamic: true }) })
+			.setImage(meme[0].image)
+			.setFooter({ text: `👍 ${meme[0].upvotes}` });
 
 		const buttonA = new MessageButton()
 			.setStyle('PRIMARY')
@@ -121,21 +93,28 @@ module.exports = class extends Command {
 			collector.resetTimer();
 
 			if (b.customId === 'nxtmeme') {
-				const postNew = safe[Math.floor(Math.random() * safe.length)];
-				let postURLNew;
+				// Pick a random meme
+				const randomMeme = newMemes[Math.floor(Math.random() * newMemes.length)];
 
-				if (post.data.url.slice(-4) === 'gifv') {
-					postURLNew = postNew.data.url.slice(0, -1);
-				} else {
-					postURLNew = postNew.data.url;
+				// Remove the used meme from the list
+				newMemes.splice(newMemes.indexOf(randomMeme), 1);
+
+				// If there are no more memes, remove the button
+				if (newMemes.length === 0) {
+					const newMeme = new MessageEmbed()
+						.setColor(this.client.utils.color(message.guild.me.displayHexColor))
+						.setAuthor({ name: `${randomMeme.title}`, url: `${randomMeme.postLink}`, iconURL: message.author.displayAvatarURL({ dynamic: true }) })
+						.setImage(randomMeme.image)
+						.setFooter({ text: `👍 ${randomMeme.upvotes}` });
+					await b.update({ embeds: [newMeme], components: [] });
+					return;
 				}
 
 				const newMeme = new MessageEmbed()
 					.setColor(this.client.utils.color(message.guild.me.displayHexColor))
-					.setAuthor({ name: `${postNew.data.title}`, url: `https://reddit.com${postNew.data.permalink}`, iconURL: message.author.displayAvatarURL({ dynamic: true }) })
-					.setImage(postURLNew)
-					.setFooter({ text: `👍 ${postNew.data.ups} | 💬 ${postNew.data.num_comments}` });
-
+					.setAuthor({ name: `${randomMeme.title}`, url: `${randomMeme.postLink}`, iconURL: message.author.displayAvatarURL({ dynamic: true }) })
+					.setImage(randomMeme.image)
+					.setFooter({ text: `👍 ${randomMeme.upvotes}` });
 				await b.update({ embeds: [newMeme], components: [row] });
 			}
 		});
@@ -147,6 +126,14 @@ module.exports = class extends Command {
 		});
 
 		this.client.utils.deletableCheck(msg, 0);
+
+		async function getNewMeme() {
+			return RedditImageFetcher.fetch({
+				type: 'custom',
+				total: 50,
+				subreddit: subreddits
+			});
+		}
 	}
 
 };
