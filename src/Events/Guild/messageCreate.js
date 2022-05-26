0bb35a09c83327016c807da95cd0897d6b803df1
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2022-05-26 13:45:10 +0100

    added perm catch for missing 'SEND_MESSAGES'

--- src/Events/Guild/messageCreate.js
+++ src/Events/Guild/messageCreate.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-depth */
 /* eslint-disable consistent-return */
 /* eslint-disable no-inline-comments */
 /* eslint-disable no-mixed-operators */
@@ -904,6 +905,14 @@ module.exports = class extends Event {
 			if (botPermCheck) {
 				const missing = message.channel.permissionsFor(this.client.user).missing(botPermCheck);
 				if (missing.length) {
+					if (missing.includes('SEND_MESSAGES')) {
+						const errorMsg = `'[PERMISSIONS ERROR]' An attempt to run command: '${cmd}' in guild '${message.guild.name}' was made, but I am missing the 'SEND_MESSAGES' permission.`;
+						console.error(`[\x1b[31mPERMISSIONS ERROR\x1b[0m] An attempt to run command: '\x1b[92m${cmd}\x1b[0m' in guild \x1b[31m${message.guild.name}\x1b[0m was made, but I am missing the '\x1b[92mSEND_MESSAGES\x1b[0m' permission.`);
+						const channel = this.client.channels.cache.get('685973401772621843');
+						if (!channel) return;
+						channel.send(`\`\`\`js\n${errorMsg}\`\`\``);
+						return;
+					}
 					const embed = new MessageEmbed()
 						.setColor(this.client.utils.color(message.guild.me.displayHexColor))
 						.addField(`**${this.client.user.username} - ${this.client.utils.capitalise(command.name)}**`,
