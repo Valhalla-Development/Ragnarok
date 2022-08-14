0e58ff7cb06de8272b94649ec6147cf26e485fa0
Ragnar Lothbrok <hudsonjd@live.co.uk>
2022-08-14 18:24:49 +0100

    #createWebhook() (and other text-based channels) now takes channel and name

--- src/Functions/Sudo.js
+++ src/Functions/Sudo.js
@@ -5,7 +5,7 @@ module.exports = class sudo {
 	}
 
 	async sudo(message, text, user) {
-		message.channel.createWebhook(user.user.username, { avatar: user.user.displayAvatarURL({ dynamic: true }) }).then(webhook => {
+		message.channel.createWebhook({ name: user.user.username, channel: message.channel, avatar: user.user.displayAvatarURL({ extension: 'png' }) }).then(webhook => {
 			webhook.send(text);
 			setTimeout(() => {
 				webhook.delete();
