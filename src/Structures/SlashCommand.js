f1cd001c96c68a312ab2aa0155f112e15f9b7ad4
Ragnar Lothbrok <hudsonjd@live.co.uk>
2022-12-22 17:52:14 +0000

    Improved slash command structure/class

--- src/Structures/SlashCommand.js
+++ src/Structures/SlashCommand.js
@@ -1,16 +1,20 @@
 import { PermissionsBitField } from 'discord.js';
 
 export const SlashCommand = class SlashCommand {
-  constructor(client, name, options = {}) {
+  constructor(
+    client,
+    name,
+    { type = 1, ownerOnly = null, description = 'No description provided', category, options = [], userPerms, botPerms } = {}
+  ) {
     this.client = client;
-    this.name = options.name || name;
-    this.type = options.type || 1;
-    this.ownerOnly = options.ownerOnly || null;
-    this.description = options.description || 'No description provided';
-    this.category = options.category;
-    this.options = options.options || [];
-    this.userPerms = new PermissionsBitField(options.userPerms).freeze();
-    this.botPerms = new PermissionsBitField(options.botPerms).freeze();
+    this.name = name;
+    this.type = type;
+    this.ownerOnly = ownerOnly;
+    this.description = description;
+    this.category = category;
+    this.options = options;
+    this.userPerms = new PermissionsBitField(userPerms).freeze();
+    this.botPerms = new PermissionsBitField(botPerms).freeze();
   }
 
   // eslint-disable-next-line no-unused-vars
