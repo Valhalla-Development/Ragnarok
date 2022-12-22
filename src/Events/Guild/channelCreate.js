8f4d75e85c7c002148ed641f97d256111aae7fa3
Ragnar Lothbrok <hudsonjd@live.co.uk>
2022-12-22 17:54:22 +0000

    Improved general variable naming

--- src/Events/Guild/channelCreate.js
+++ src/Events/Guild/channelCreate.js
@@ -9,45 +9,46 @@ export const EventF = class extends Event {
   async run(channel) {
     if (channel.type === ChannelType.DM) return;
 
-    const id = db.prepare(`SELECT channel FROM logging WHERE guildid = ${channel.guild.id};`).get();
-    if (!id) return;
+    const channelId = db.prepare(`SELECT channel FROM logging WHERE guildid = ${channel.guild.id};`).get();
+    if (!channelId) return;
 
-    const logs = id.channel;
+    const logs = channelId.channel;
     if (!logs) return;
 
     const chnCheck = this.client.channels.cache.get(logs);
     if (!chnCheck) {
       db.prepare('DELETE FROM logging WHERE guildid = ?').run(channel.guild.id);
+      return;
     }
 
-    const logembed = new EmbedBuilder()
+    const logEmbed = new EmbedBuilder()
       .setColor(this.client.utils.color(channel.guild.members.me.displayHexColor))
       .setAuthor({
         name: `${channel.guild.name}`,
         iconURL: channel.guild.iconURL()
       })
       .setTitle('Channel Created')
-      .setFooter({ text: `ID: ${channel.id}` })
+      .setFooter({ text: `ID: ${channel.id}` }) //! TEST
       .setTimestamp();
 
-    let updateM;
+    let updateMessage;
 
     if (channel.type === ChannelType.GuildText) {
-      updateM = `**◎ Text Channel Created:**\n<#${channel.id}>`;
-      logembed.setDescription(updateM);
-      this.client.channels.cache.get(logs).send({ embeds: [logembed] });
+      updateMessage = `**◎ Text Channel Created:**\n<#${channel.id}>`;
+      logEmbed.setDescription(updateMessage);
+      this.client.channels.cache.get(logs).send({ embeds: [logEmbed] });
     }
 
     if (channel.type === ChannelType.GuildVoice) {
-      updateM = `**◎ Voice Channel Created:**\n\`${channel.name}\``;
-      logembed.setDescription(updateM);
-      this.client.channels.cache.get(logs).send({ embeds: [logembed] });
+      updateMessage = `**◎ Voice Channel Created:**\n\`${channel.name}\``;
+      logEmbed.setDescription(updateMessage);
+      this.client.channels.cache.get(logs).send({ embeds: [logEmbed] });
     }
 
     if (channel.type === ChannelType.GuildCategory) {
-      updateM = `**◎ Category Created:**\n\`${channel.name}\``;
-      logembed.setDescription(updateM);
-      this.client.channels.cache.get(logs).send({ embeds: [logembed] });
+      updateMessage = `**◎ Category Created:**\n\`${channel.name}\``;
+      logEmbed.setDescription(updateMessage);
+      this.client.channels.cache.get(logs).send({ embeds: [logEmbed] });
     }
   }
 };
