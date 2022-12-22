45b9bb966d1b1bcd36a25ea2dfa1dbf572c21af5
Ragnar Lothbrok <hudsonjd@live.co.uk>
2022-12-22 17:35:47 +0000

    Improve class ProgressBar on level card

--- src/SlashCommands/Fun/Level.js
+++ src/SlashCommands/Fun/Level.js
@@ -400,43 +400,54 @@ export const SlashCommandF = class extends SlashCommand {
 
     class ProgressBar {
       constructor(dimension, color, percentage) {
-        ({ x: this.x, y: this.y, width: this.w, height: this.h } = dimension);
+        this.dim = dimension;
         this.color = color;
         this.percentage = percentage / 100;
-        this.p;
       }
 
       draw() {
-        this.p = this.percentage * this.w;
-        if (this.p <= this.h) {
+        const p = this.percentage * this.dim.width;
+        ctx.fillStyle = this.color;
+
+        if (p <= this.dim.height) {
+          // draw left arc
           ctx.beginPath();
           ctx.arc(
-            this.h / 2 + this.x,
-            this.h / 2 + this.y,
-            this.h / 2,
-            Math.PI - Math.acos((this.h - this.p) / this.h),
-            Math.PI + Math.acos((this.h - this.p) / this.h)
+            this.dim.height / 2 + this.dim.x,
+            this.dim.height / 2 + this.dim.y,
+            this.dim.height / 2,
+            Math.PI - Math.acos((this.dim.height - p) / this.dim.height),
+            Math.PI + Math.acos((this.dim.height - p) / this.dim.height)
           );
           ctx.save();
+
+          // draw right arc
           ctx.scale(-1, 1);
           ctx.arc(
-            this.h / 2 - this.p - this.x,
-            this.h / 2 + this.y,
-            this.h / 2,
-            Math.PI - Math.acos((this.h - this.p) / this.h),
-            Math.PI + Math.acos((this.h - this.p) / this.h)
+            this.dim.height / 2 - p - this.dim.x,
+            this.dim.height / 2 + this.dim.y,
+            this.dim.height / 2,
+            Math.PI - Math.acos((this.dim.height - p) / this.dim.height),
+            Math.PI + Math.acos((this.dim.height - p) / this.dim.height)
           );
           ctx.restore();
           ctx.closePath();
         } else {
+          // draw left arc
           ctx.beginPath();
-          ctx.arc(this.h / 2 + this.x, this.h / 2 + this.y, this.h / 2, Math.PI / 2, (3 / 2) * Math.PI);
-          ctx.lineTo(this.p - this.h + this.x, 0 + this.y);
-          ctx.arc(this.p - this.h / 2 + this.x, this.h / 2 + this.y, this.h / 2, (3 / 2) * Math.PI, Math.PI / 2);
-          ctx.lineTo(this.h / 2 + this.x, this.h + this.y);
+          ctx.arc(this.dim.height / 2 + this.dim.x, this.dim.height / 2 + this.dim.y, this.dim.height / 2, Math.PI / 2, (3 / 2) * Math.PI);
+
+          // draw rectangle
+          ctx.lineTo(p - this.dim.height + this.dim.x, 0 + this.dim.y);
+
+          // draw right arc
+          ctx.arc(p - this.dim.height / 2 + this.dim.x, this.dim.height / 2 + this.dim.y, this.dim.height / 2, (3 / 2) * Math.PI, Math.PI / 2);
+
+          // close path
+          ctx.lineTo(this.dim.height / 2 + this.dim.x, this.dim.height + this.dim.y);
           ctx.closePath();
         }
-        ctx.fillStyle = this.color;
+
         ctx.fill();
       }
     }
