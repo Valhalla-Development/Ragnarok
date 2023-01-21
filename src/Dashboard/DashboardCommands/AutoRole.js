4594e160feda45614b9dbf3ab3687d9341185c50
Ragnar Lothbrok <hudsonjd@live.co.uk>
2023-01-21 23:11:39 +0000

    Fixed Autorole dashboard command

--- src/Dashboard/DashboardCommands/AutoRole.js
+++ src/Dashboard/DashboardCommands/AutoRole.js
@@ -1,46 +1,77 @@
+/* eslint-disable consistent-return */
 /* eslint-disable no-unused-vars */
 import DBD from 'discord-dashboard';
 import AutoRoleSchema from '../../Mongo/Schemas/AutoRole.js';
 
 export default (client) => {
   const AutoRole = {
-    //! DOESNT WORK
     categoryId: 'AutoRole',
     categoryName: 'Auto Role',
     categoryDescription: 'Set the role members will be given once they join your server.',
     categoryImageURL: 'https://ragnarokbot.com/assets/img/functions/moderation.png',
+    refreshOnSave: true,
     getActualSet: async ({ guild }) => {
       const result = await AutoRoleSchema.findOne({ guildId: guild.id });
-      const set = result ? { test: result.role } : { test: null };
+      const set = result ? result.role : null;
+      const status = !!result;
 
       return [
         {
           optionId: 'autoRole',
-          data: {
-            test: '1234',
-            test2: '4321'
-          }
+          data: set
+        },
+        {
+          optionId: 'autoRoleToggle',
+          data: status
         }
       ];
     },
     setNew: async ({ guild, user, data }) => {
-      const toggle = data[0].data;
+      const result = await AutoRoleSchema.findOne({ guildId: guild.id });
 
-      if (toggle === true) {
-        await new AutoRoleSchema({
-          guildId: guild.id,
-          status: toggle
-        }).save();
-      } else {
+      if (data.some((option) => option.optionId === 'autoRoleToggle' && option.data === false)) {
         await AutoRoleSchema.deleteOne({ guildId: guild.id });
+        return;
+      }
+
+      if (
+        data.some((option) => option.optionId === 'autoRoleToggle' && option.data === true) &&
+        !data.some((option) => option.optionId === 'autoRole')
+      ) {
+        return { error: 'Please set a role!' };
+      }
+
+      const autoRoleObject = data.find((obj) => obj.optionId === 'autoRole');
+      if (autoRoleObject?.data) {
+        if (!result) {
+          await new AutoRoleSchema({
+            guildId: guild.id,
+            role: autoRoleObject.data
+          }).save();
+        } else {
+          await AutoRoleSchema.findOneAndUpdate(
+            {
+              guildId: guild.id
+            },
+            {
+              role: autoRoleObject.data
+            }
+          );
+        }
       }
     },
     categoryOptionsList: [
       {
         optionId: 'autoRole',
+        optionName: 'Role',
+        optionDescription: 'Select the role to set.',
+        optionType: DBD.formTypes.rolesSelect()
+      },
+      {
+        optionId: 'autoRoleToggle',
         optionName: 'Toggle',
-        optionDescription: 'Toggle the Anti Anti Scam Module.',
-        optionType: DBD.formTypes.select({ Polish: 'pl', English: 'en', French: 'fr' }, false)
+        optionDescription: 'Toggle the AutoRole.',
+        optionType: DBD.formTypes.switch()
       }
     ]
   };
