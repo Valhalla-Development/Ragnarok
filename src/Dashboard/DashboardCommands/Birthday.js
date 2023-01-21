fab4cdf9f1898dd41625738fa1da5fb870f00388
Ragnar Lothbrok <hudsonjd@live.co.uk>
2023-01-21 23:11:51 +0000

    Wrote dashboard command: Birthday

---
+++ src/Dashboard/DashboardCommands/Birthday.js
@@ -0,0 +1,103 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-unused-vars */
+import DBD from 'discord-dashboard';
+import { ChannelType } from 'discord.js';
+import BirthdayConfigSchema from '../../Mongo/Schemas/BirthdayConfig.js';
+
+export default (client) => {
+  const Birthday = {
+    categoryId: 'Birthday',
+    categoryName: 'Birthday Config',
+    categoryDescription: 'Configure the Birthday module.',
+    categoryImageURL: 'https://ragnarokbot.com/assets/img/functions/moderation.png',
+    refreshOnSave: true,
+    getActualSet: async ({ guild }) => {
+      const result = await BirthdayConfigSchema.findOne({ guildId: guild.id });
+      const channelStatus = result ? result.channel : null;
+      const roleStatus = result ? result.role : null;
+      const status = !!result;
+
+      return [
+        {
+          optionId: 'birthdayChannel',
+          data: channelStatus
+        },
+        {
+          optionId: 'birthdayRole',
+          data: roleStatus
+        },
+        {
+          optionId: 'birthdayToggle',
+          data: status
+        }
+      ];
+    },
+    setNew: async ({ guild, user, data }) => {
+      const result = await BirthdayConfigSchema.findOne({ guildId: guild.id });
+
+      if (data.some((option) => option.optionId === 'birthdayToggle' && option.data === false)) {
+        await BirthdayConfigSchema.deleteOne({ guildId: guild.id });
+        return;
+      }
+
+      if (data.some((option) => option.optionId === 'birthdayRole') && !data.some((option) => option.optionId === 'birthdayChannel')) {
+        if (!result?.channel) {
+          return { error: 'Please set a channel before setting the role!' };
+        }
+      }
+
+      if (!data.some((option) => option.optionId === 'birthdayChannel')) {
+        if (!result?.channel) {
+          return { error: 'Please set a channel.' };
+        }
+      }
+
+      let birthdayRol = result?.role;
+      let birthdayChan = result?.channel;
+      const birthdayroleObj = data.find((obj) => obj.optionId === 'birthdayRole');
+      const birthdayChannelObj = data.find((obj) => obj.optionId === 'birthdayChannel');
+      birthdayRol = birthdayroleObj?.data || birthdayRol;
+      birthdayChan = birthdayChannelObj?.data || birthdayChan;
+      console.log(result);
+      if (!result) {
+        await new BirthdayConfigSchema({
+          guildId: guild.id,
+          channel: birthdayChan,
+          role: birthdayRol
+        }).save();
+      } else {
+        await BirthdayConfigSchema.findOneAndUpdate(
+          {
+            guildId: guild.id
+          },
+          {
+            channel: birthdayChan,
+            role: birthdayRol
+          }
+        );
+      }
+    },
+    categoryOptionsList: [
+      {
+        optionId: 'birthdayChannel',
+        optionName: 'Channel',
+        optionDescription: 'Select the channel to set.',
+        optionType: DBD.formTypes.channelsSelect(false, [ChannelType.GuildText], false, false)
+      },
+      {
+        optionId: 'birthdayRole',
+        optionName: 'Role',
+        optionDescription: 'Select the role to set.',
+        optionType: DBD.formTypes.rolesSelect()
+      },
+      {
+        optionId: 'birthdayToggle',
+        optionName: 'Toggle',
+        optionDescription: 'Toggle the Birthday module.',
+        optionType: DBD.formTypes.switch()
+      }
+    ]
+  };
+
+  return Birthday;
+};
