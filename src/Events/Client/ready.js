92335739df6e1bd8488c853148c82b3a2656c5b7
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-03-16 14:37:38 +0000

    Updated word of the day cron to be more efficient

--- src/Events/Client/ready.js
+++ src/Events/Client/ready.js
@@ -366,77 +366,56 @@ export const EventF = class extends Event {
       true
     );
 
+    const replEm = (str) => {
+      const boldStart = /<em>/g;
+      const boldEnd = /<\/em>/g;
+      const nonLinkBold = /em>/g;
+      return str.replace(boldStart, '**').replace(boldEnd, '**').replace(nonLinkBold, '**');
+    };
+
     const twentyFourTimer = new CronJob(
       '0 13 * * *',
       async () => {
-        // Run every 24 hours
-        const guild = this.client.guilds.cache.get('657235952116170794');
-        if (!guild) return;
-        const chn = guild.channels.cache.get('663193215943311373');
-        if (!chn) return;
-
-        function replEm(str) {
-          const boldStart = /<em>/g;
-          const boldEnd = /<\/em>/g;
-          const nonLinkBold = /em>/g;
-          return str.replace(boldStart, '**').replace(boldEnd, '**').replace(nonLinkBold, '**');
-        }
-
         try {
+          const guild = this.client.guilds.cache.get('657235952116170794');
+          const chn = guild?.channels.cache.get('663193215943311373');
+          if (!chn) return;
+
           const url = 'https://www.merriam-webster.com/word-of-the-day';
           const response = await fetch(url);
 
-          // Use if statements to check for specific error conditions.
           if (!response.ok) {
-            // Handle the error...
+            return console.log('Failed to fetch word of the day:', response.statusText);
           }
-
-          const arr = [];
 
           const body = await response.text();
           const $ = load(body);
 
-          // Word
           const wordClass = $('.word-and-pronunciation');
           const word = wordClass.find('h1').text();
-
-          // Word Attributes
           const typeFetch = $('.main-attr');
           const type = typeFetch.text();
           const syllablesFetch = $('.word-syllables');
           const syllables = syllablesFetch.text();
 
-          // Definiton
-          const wordDef = $('.wod-definition-container');
-          if (wordDef) {
-            const def = wordDef.html();
+          const arr = [];
 
-            // Use if statements to check for specific error conditions.
-            if (def) {
-              const wordDefSplit1 = def.substring(def.indexOf('<p>') + 3);
-              const wordDefSplit2 = wordDefSplit1.split('</p>')[0];
-              const repl = replEm(wordDefSplit2);
-              const output = repl.replace(/<a href="([^"]+)">([^<]+)<\/a>/g, '[**$2**]($1)');
-              arr.push({ name: '**Definition:**', value: `>>> *${replEm(output)}*` });
-            } else {
-              // Handle the error...
-            }
+          const wordDef = $('.wod-definition-container');
+          if (wordDef.length) {
+            const def = wordDef.html();
+            const wordDefSplit1 = def.substring(def.indexOf('<p>') + 3);
+            const wordDefSplit2 = wordDefSplit1.split('</p>')[0];
+            const repl = replEm(wordDefSplit2);
+            const output = repl.replace(/<a href="([^"]+)">([^<]+)<\/a>/g, '[**$2**]($1)');
+            arr.push({ name: '**Definition:**', value: `>>> *${replEm(output)}*` });
           }
 
-          // Example
           const wordEx = $('.wod-definition-container p:eq(1)');
-          if (wordEx) {
+          if (wordEx.length) {
             const def = wordEx.html();
             const output = def.substring(3).replace(/<a href="([^"]+)">([^<]+)<\/a>/g, '[**$2**]($1)');
-
-            // Use if statements to check for specific error conditions.
-            if (def) {
-              arr.push({ name: '**Example:**', value: `>>> ${replEm(output)}` });
-            } else {
-              // Handle the error...
-            }
+            arr.push({ name: '**Example:**', value: `>>> ${replEm(output)}` });
           }
-
           // Embed
           const embed = new EmbedBuilder()
             .setColor(this.client.utils.color(guild.members.me.displayHexColor))
