e0b7d0a4862bb04b45f3a72aa6df5c61ee0055a3
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-04-23 14:51:03 +0100

    Create a command to generate known scam links

---
+++ src/SlashCommands/Hidden/Spen.js
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import fetch from 'node-fetch';
+import SlashCommand from '../../Structures/SlashCommand.js';
+
+export const SlashCommandF = class extends SlashCommand {
+  constructor(...args) {
+    super(...args, {
+      description: 'Create array from list',
+      category: 'Hidden',
+      ownerOnly: true
+    });
+  }
+
+  async run() {
+    fetch('https://raw.githubusercontent.com/PyAres/scam-links/master/src/links.txt')
+      .then((res) => res.text())
+      .then((list) => {
+        const array = list.split('\n').map((str) => str.trim());
+        fs.writeFile('Storage/spenLinks.json', JSON.stringify(array, null, 2), { flag: 'w' }, (err) => {
+          if (err) throw err;
+          console.log('The file has been saved!');
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+};
+
+export default SlashCommandF;
