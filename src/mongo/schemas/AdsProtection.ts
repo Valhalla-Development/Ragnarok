89a53197e62c4488fc34cedc8a3f6690c75745ba
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-11-02 18:46:39 +0000

    Create AdsProtection mongoose schema

---
+++ src/mongo/schemas/AdsProtection.ts
@@ -0,0 +1,8 @@
+import { model, Schema } from 'mongoose';
+
+const AdsProtection = new Schema({
+    GuildId: { type: String, unique: true },
+    Status: { type: Boolean, default: null },
+});
+
+export default model('AdsProtection', AdsProtection, 'AdsProtection');
