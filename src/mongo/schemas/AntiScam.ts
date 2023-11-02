79718ea3f3adb1da104a89e34d5ed20a024e7bfe
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-11-02 19:07:36 +0000

    Create AntiScam.ts mongoose schema

---
+++ src/mongo/schemas/AntiScam.ts
@@ -0,0 +1,8 @@
+import { Schema, model } from 'mongoose';
+
+const AntiScam = new Schema({
+    GuildId: { type: String, unique: true },
+    Status: { type: Boolean, default: null },
+});
+
+export default model('AntiScam', AntiScam, 'AntiScam');
