2acc927114aae3f00b5f680e7215fa31b02fb569
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2024-02-10 16:41:49 +0000

    init /serverinfo: roles

---
+++ src/commands/miscellaneous/ServerInfo.ts
@@ -0,0 +1,63 @@
+import {
+    Client, Discord, Slash, SlashChoice, SlashOption,
+} from 'discordx';
+import { Category } from '@discordx/utilities';
+import { ApplicationCommandOptionType, CommandInteraction, EmbedBuilder } from 'discord.js';
+import { color } from '../../utils/Util.js';
+
+@Discord()
+@Category('Miscellaneous')
+export class Ping {
+    /**
+     * Displays stats for the interaction guild.
+     * @param interaction - The command interaction.
+     * @param client - The Discord client.
+     * @param option - The option selected from the command
+     */
+    @Slash({ description: 'Displays stats for the guild.' })
+    async serverinfo(
+        @SlashChoice({ name: 'Server', value: 'server' })
+        @SlashChoice({ name: 'Roles', value: 'roles' })
+        @SlashChoice({ name: 'Emojis', value: 'emojis' })
+        @SlashOption({
+            description: 'Type of request',
+            name: 'option',
+            required: true,
+            type: ApplicationCommandOptionType.String,
+        })
+            option: string,
+            interaction: CommandInteraction,
+            client: Client,
+    ): Promise<void> {
+        if (option === 'roles') {
+            const roles = interaction.guild?.roles.cache
+                .sort((a, b) => b.position - a.position)
+                .map((role) => role.toString())
+                .slice(0, -1);
+
+            if (!roles) {
+                await interaction.reply({ content: 'I was unable to locate any roles.' });
+                return;
+            }
+
+            let roleList = roles.join(', ');
+
+            if (roleList.length > 4000) {
+                roleList = roleList.substring(0, 4000);
+                roleList = roleList.substring(0, roleList.lastIndexOf('<'));
+            }
+
+            const embed = new EmbedBuilder()
+                .setColor(color(interaction.guild!.members.me!.displayHexColor))
+                .setAuthor({ name: `Viewing information for ${interaction.guild?.name}`, iconURL: `${interaction.guild?.iconURL()}` })
+                .setDescription(
+                    `**Server Roles [${roles.length}]**\n${
+                        roles.length <= 25 ? roleList : `${roleList}... and ${roles.length - 25} more`
+                    }`,
+                )
+                .setFooter({ text: `${client.user?.username}`, iconURL: client.user?.displayAvatarURL() });
+
+            await interaction.reply({ embeds: [embed] });
+        }
+    }
+}
