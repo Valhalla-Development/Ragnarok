14415ffc7ad251a349cc9874d2c5cda6520db7aa
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2024-02-17 15:42:57 +0000

    :rocket: feat(Level.ts): rewrote Level mongo schema in TypeScript

---
+++ src/mongo/schemas/Level.ts
@@ -0,0 +1,16 @@
+import { model, Schema } from 'mongoose';
+
+/**
+ * Represents a schema for storing the users Level data
+ */
+const Level = new Schema({
+    IdJoined: { type: String, unique: true },
+    UserId: { type: String, default: null },
+    GuildId: { type: String, default: null },
+    Xp: { type: Number, default: null },
+    Level: { type: Number, default: null },
+    Country: { type: String, default: null },
+    Image: { type: String, default: null },
+});
+
+export default model('Level', Level, 'Level');
