ff8104369bace0ed064cfe154e23651a5ad74fac
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2024-02-17 15:42:39 +0000

    :rocket: feat(Leader.ts): rewrote Leader command in TypeScript

---
+++ src/commands/fun/Leader.ts
@@ -0,0 +1,71 @@
+import { Client, Discord, Slash } from 'discordx';
+import { APIEmbedField, CommandInteraction, EmbedBuilder } from 'discord.js';
+import { Category } from '@discordx/utilities';
+import Level from '../../mongo/schemas/Level.js';
+import LevelConfig from '../../mongo/schemas/LevelConfig.js';
+import { color } from '../../utils/Util.js';
+
+@Discord()
+@Category('Fun')
+export class Leader {
+    /**
+     * Displays the leaderboard for the level system
+     * @param interaction - The command interaction.
+     * @param client - The Discord client.
+     * @param user - Optional user to fetch
+     */
+    @Slash({ description: 'Displays the leaderboard for the level system' })
+    async leader(interaction: CommandInteraction, client: Client): Promise<void> {
+        const levelDb = await LevelConfig.findOne({ GuildId: interaction.guild!.id });
+
+        if (levelDb) {
+            const embed = new EmbedBuilder()
+                .setColor(color(interaction.guild!.members.me!.displayHexColor))
+                .addFields({ name: `**${client.user?.username} - Leader**`, value: '**◎ Error:** Level system is disabled for this guild!' });
+            await interaction.reply({ ephemeral: true, embeds: [embed] });
+            return;
+        }
+
+        const top10 = await Level.find({ GuildId: interaction.guild!.id })
+            .sort({ Xp: -1 })
+            .limit(10);
+
+        if (!top10 || top10.length === 0) {
+            return;
+        }
+
+        let userNames: string = '';
+        let levels: string = '';
+        let xp: string = '';
+
+        await Promise.all(top10.map(async (data, index) => {
+            let fetchUser = interaction.guild!.members.cache.get(data.UserId);
+
+            if (!fetchUser) {
+                try {
+                    fetchUser = await interaction.guild!.members.fetch(data.UserId);
+                } catch {
+                    // Do nothing because I am a monster
+                }
+            }
+
+            if (fetchUser) {
+                userNames += `◎ \`${top10.length - index}\` ${fetchUser}\n`;
+
+                levels += `\`${data.Level}\`\n`;
+
+                xp += `\`${data.Xp.toLocaleString('en')}\`\n`;
+            }
+        }));
+
+        const embed = new EmbedBuilder()
+            .setAuthor({ name: `Leaderboard for ${interaction.guild!.name}`, iconURL: `${interaction.guild!.iconURL({ extension: 'png' })}` })
+            .setColor(color(interaction.guild!.members.me!.displayHexColor))
+            .addFields(
+                { name: 'Top 10', value: userNames, inline: true },
+                { name: 'Level', value: levels, inline: true },
+                { name: 'XP', value: xp, inline: true },
+            );
+        await interaction.reply({ embeds: [embed] });
+    }
+}
