157e8b22c575e9366e33bc39ebb9661310945637
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2024-03-02 18:42:44 +0000

    :rocket: feat(Nuke.ts): rewrote Nuke command in TypeScript

---
+++ src/commands/moderation/Nuke.ts
@@ -0,0 +1,47 @@
+import {
+    Client, Discord, Guard, Slash,
+} from 'discordx';
+import {
+    ApplicationCommandOptionType, ChannelType, codeBlock, CommandInteraction, PermissionsBitField,
+} from 'discord.js';
+import { Category } from '@discordx/utilities';
+import { BotHasPerm } from '../../guards/BotHasPerm.js';
+import { RagnarokEmbed } from '../../utils/Util';
+
+@Discord()
+@Category('Moderation')
+export class Nuke {
+    /**
+     * Nuke a channel.
+     * @param interaction - The command interaction.
+     * @param client - The Discord client.
+     */
+    @Slash({
+        description: 'Nuke a channel.',
+        defaultMemberPermissions: [PermissionsBitField.Flags.Administrator],
+    })
+    @Guard(BotHasPerm([PermissionsBitField.Flags.ManageChannels]))
+    async nuke(interaction: CommandInteraction, client: Client): Promise<void> {
+        if (!interaction.channel || interaction.channel.type !== ChannelType.GuildText) {
+            await RagnarokEmbed(client, interaction, 'Error', 'The interaction channel type was not a text based channel.', true);
+            return;
+        }
+
+        try {
+            const newChannel = await interaction.channel.clone({
+                name: interaction.channel.name,
+                reason: 'Nuked!',
+            });
+
+            await interaction.channel.delete();
+            await newChannel.setParent(interaction.channel.parentId);
+            await newChannel.setPosition(interaction.channel.rawPosition);
+
+            await newChannel.send({ content: 'Channel has been nuked!\nhttps://tenor.com/view/explosion-mushroom-cloud-atomic-bomb-bomb-boom-gif-4464831' });
+        } catch (error) {
+            await interaction.followUp('Something went wrong while nuking the channel. Please try again.');
+
+            await RagnarokEmbed(client, interaction, 'Error', `An error occurred\n${codeBlock('text', `${error}`)}`, true);
+        }
+    }
+}
