46d88dd59b75978aafa0b3707dc57928c2f68767
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2024-03-03 18:12:40 +0000

    :sparkles: (Balance.ts): add a new file `Balance.ts` to store the Balance data for users in a guild using a mongoose schema

---
+++ src/mongo/Balance.ts
@@ -0,0 +1,51 @@
+import { model, Schema } from 'mongoose';
+
+/**
+ * Represents a schema for storing the Balance data for users in a guild.
+ */
+const Balance = new Schema({
+    IdJoined: { type: String, unique: true },
+    UserId: { type: String, default: null },
+    GuildId: { type: String, default: null },
+    Hourly: { type: Number, default: null },
+    Daily: { type: Number, default: null },
+    Weekly: { type: Number, default: null },
+    Monthly: { type: Number, default: null },
+    StealCool: { type: Number, default: null },
+    FishCool: { type: Number, default: null },
+    FarmCool: { type: Number, default: null },
+    Boosts: {
+        FishBag: { type: Number, default: null },
+        SeedBag: { type: Number, default: null },
+        FarmBag: { type: Number, default: null },
+        FarmPlot: { type: Number, default: null },
+    },
+    Items: {
+        Trout: { type: Number, default: 0 },
+        KingSalmon: { type: Number, default: 0 },
+        SwordFish: { type: Number, default: 0 },
+        PufferFish: { type: Number, default: 0 },
+        Treasure: { type: Number, default: 0 },
+        GoldBar: { type: Number, default: 0 },
+        GoldNugget: { type: Number, default: 0 },
+        Barley: { type: Number, default: 0 },
+        Spinach: { type: Number, default: 0 },
+        Strawberries: { type: Number, default: 0 },
+        Lettuce: { type: Number, default: 0 },
+        CornSeeds: { type: Number, default: 0 },
+        WheatSeeds: { type: Number, default: 0 },
+        PotatoSeeds: { type: Number, default: 0 },
+        TomatoSeeds: { type: Number, default: 0 },
+        FarmingTools: { type: Boolean, default: false },
+        FishingRod: { type: Boolean, default: false },
+    },
+    Cash: { type: Number, default: null },
+    Bank: { type: Number, default: null },
+    Total: { type: Number, default: null },
+    ClaimNewUser: { type: Number, default: null },
+    FarmPlot: { type: Array, default: [] },
+    DmHarvest: { type: String, default: null },
+    HarvestedCrops: { type: Array, default: [] },
+});
+
+export default model('Balance', Balance, 'Balance');
