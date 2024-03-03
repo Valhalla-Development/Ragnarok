d6faef0adba2bfa3ee3fe553623a979891f73228
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2024-03-03 19:11:05 +0000

    :sparkles: (Withdraw.ts): add a new command `withdraw` to withdraw money from your bank

---
+++ src/commands/economy/Withdraw.ts
@@ -0,0 +1,56 @@
+import {
+    Client, Discord, Slash, SlashOption,
+} from 'discordx';
+import { ApplicationCommandOptionType, CommandInteraction } from 'discord.js';
+import { Category } from '@discordx/utilities';
+import Balance from '../../mongo/Balance.js';
+import { RagnarokEmbed } from '../../utils/Util.js';
+
+@Discord()
+@Category('Economy')
+export class Withdraw {
+    /**
+     * Withdraw money from the bank
+     * @param interaction - The command interaction.
+     * @param client - The Discord client.
+     * @param amount - Amount to withdraw
+     */
+    @Slash({ description: 'Withdraw money from the bank' })
+    async withdraw(
+        @SlashOption({
+            description: 'Amount to withdraw',
+            name: 'amount',
+            type: ApplicationCommandOptionType.Number,
+            required: true,
+            minValue: 10,
+        })
+            amount: number,
+            interaction: CommandInteraction,
+            client: Client,
+    ): Promise<void> {
+        const balance = await Balance.findOneAndUpdate(
+            { IdJoined: `${interaction.user.id}-${interaction.guild!.id}` },
+            { $setOnInsert: { IdJoined: `${interaction.user.id}-${interaction.guild!.id}` } },
+            {
+                upsert: true,
+                new: true,
+            },
+        );
+
+        if (!balance || !balance.Bank) {
+            await RagnarokEmbed(client, interaction, 'Error', 'You do not have enough money in your bank for this action.', true);
+            return;
+        }
+
+        if (amount > balance.Bank) {
+            await RagnarokEmbed(client, interaction, 'Error', `You only have <:coin:706659001164628008> \`${balance.Bank.toLocaleString('en')}\`. Please try again with a valid amount.`, true);
+            return;
+        }
+
+        balance.Cash += amount;
+        balance.Bank -= amount;
+        await balance.save();
+
+        await RagnarokEmbed(client, interaction, 'Success', `You have withdrawn <:coin:706659001164628008> \`${amount.toLocaleString('en')}\``, true);
+    }
+}
