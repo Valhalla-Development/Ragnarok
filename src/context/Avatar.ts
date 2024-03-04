fb91c8fbd31932954ae0c807ae3aef25b526c5af
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2024-03-04 18:24:59 +0000

    :sparkles: (Avatar.ts): add a new context menu command called "Avatar"

---
+++ src/context/Avatar.ts
@@ -0,0 +1,40 @@
+import { Client, ContextMenu, Discord } from 'discordx';
+import {
+    ApplicationCommandType, EmbedBuilder, GuildMember, UserContextMenuCommandInteraction,
+} from 'discord.js';
+import { color, RagnarokEmbed } from '../utils/Util.js';
+
+@Discord()
+export class AvatarContext {
+    /**
+     * Displays avatar for the author of the interaction or a specified user
+     * @param interaction - The command interaction
+     * @param client - The Discord client.
+     */
+    @ContextMenu({
+        name: 'Avatar',
+        type: ApplicationCommandType.User,
+    })
+    async avatarContext(interaction: UserContextMenuCommandInteraction, client: Client): Promise<void> {
+        const member = interaction.targetMember as GuildMember;
+
+        try {
+            await member.fetch();
+
+            const avatar = member.user.displayAvatarURL({ size: 2048 });
+
+            if (!avatar) {
+                await RagnarokEmbed(client, interaction, 'Error', `${member} does not have an avatar set.`, true);
+                return;
+            }
+
+            const embed = new EmbedBuilder()
+                .setAuthor({ name: `${member.displayName}'s Avatar`, iconURL: avatar })
+                .setImage(avatar)
+                .setColor(color(interaction.guild!.members.me!.displayHexColor));
+            await interaction.reply({ embeds: [embed] });
+        } catch (error) {
+            await RagnarokEmbed(client, interaction, 'Error', `An error occurred fetching ${member}'s avatar`, true);
+        }
+    }
+}
