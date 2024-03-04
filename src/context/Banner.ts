2f982a300133cd1f53db8d349aa55d91c711b203
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2024-03-04 18:37:51 +0000

    ✨ (Banner.ts): add a new context menu command to display a guild member's banner

---
+++ src/context/Banner.ts
@@ -0,0 +1,40 @@
+import { Client, ContextMenu, Discord } from 'discordx';
+import {
+    ApplicationCommandType, EmbedBuilder, GuildMember, UserContextMenuCommandInteraction,
+} from 'discord.js';
+import { color, RagnarokEmbed } from '../utils/Util.js';
+
+@Discord()
+export class BannerContext {
+    /**
+     * Display guild member's banner
+     * @param interaction - The command interaction
+     * @param client - The Discord client.
+     */
+    @ContextMenu({
+        name: 'Banner',
+        type: ApplicationCommandType.User,
+    })
+    async bannerAvatar(interaction: UserContextMenuCommandInteraction, client: Client): Promise<void> {
+        const member = interaction.targetMember as GuildMember;
+
+        try {
+            await member.fetch();
+
+            const banner = member.user.bannerURL({ size: 2048 });
+
+            if (!banner) {
+                await RagnarokEmbed(client, interaction, 'Error', `${member} does not have a banner set.`, true);
+                return;
+            }
+
+            const embed = new EmbedBuilder()
+                .setAuthor({ name: `${member.displayName}'s Banner`, iconURL: banner })
+                .setImage(banner)
+                .setColor(color(interaction.guild!.members.me!.displayHexColor));
+            await interaction.reply({ embeds: [embed] });
+        } catch (error) {
+            await RagnarokEmbed(client, interaction, 'Error', `An error occurred fetching ${member}'s banner`, true);
+        }
+    }
+}
