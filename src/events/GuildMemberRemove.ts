2ec314869caf40ce02a1da0942dc64258699a75a
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2024-04-03 20:20:53 +0100

    📝 (GuildMemberRemove.ts): add a check to see if the user has a ticket and send a message to the corresponding channel if they do

--- src/events/GuildMemberRemove.ts
+++ src/events/GuildMemberRemove.ts
@@ -1,7 +1,9 @@
 import {
     ArgsOf, Client, Discord, On,
 } from 'discordx';
-import { ActivityType, ChannelType } from 'discord.js';
+import { ActivityType, ChannelType, EmbedBuilder } from 'discord.js';
+import { color } from '../utils/Util.js';
+import Tickets from '../mongo/Tickets.js';
 
 /**
  * Discord.js GuildMemberRemove event handler.
@@ -22,5 +24,22 @@ export class GuildMemberRemove {
             name: `${client.guilds.cache.size.toLocaleString('en')} Guilds
             ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString('en')} Users`,
         });
+
+        // Check if the user has a ticket
+        const userTicket = await Tickets.findOne({ GuildId: member.guild.id, AuthorId: member.user.id });
+        if (userTicket) {
+            // Fetch the channel
+            const channel = member.guild.channels.cache.get(userTicket.ChannelId);
+
+            // Check if the channel exists
+            if (channel && channel.type === ChannelType.GuildText) {
+                // Send a message that the user left
+                const existTM = new EmbedBuilder().setColor(color(member.guild!.members.me!.displayHexColor)).addFields({
+                    name: `**${client.user?.username} - Ticket**`,
+                    value: `${member} - \`@${member.user.tag}${member.user.discriminator !== '0' ? `#${member.user.discriminator}` : ''}\` has the left the server\nThey will be added back to the ticket if they rejoin.`,
+                });
+                channel.send({ embeds: [existTM] });
+            }
+        }
     }
 }
