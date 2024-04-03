18590d194213e73fadf6e08d06474cf1d55cd3ad
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2024-04-03 20:21:12 +0100

    ✨ (GuildMemberRemove.ts): add support for sending an embed to a logging channel when a member leaves the guild

--- src/events/GuildMemberRemove.ts
+++ src/events/GuildMemberRemove.ts
@@ -1,7 +1,10 @@
 import {
     ArgsOf, Client, Discord, On,
 } from 'discordx';
-import { ActivityType, ChannelType, EmbedBuilder } from 'discord.js';
+import {
+    ActivityType, ChannelType, EmbedBuilder, PermissionsBitField,
+} from 'discord.js';
+import Logging from '../mongo/Logging.js';
 import { color } from '../utils/Util.js';
 import Tickets from '../mongo/Tickets.js';
 
@@ -39,6 +42,34 @@ export class GuildMemberRemove {
                     value: `${member} - \`@${member.user.tag}${member.user.discriminator !== '0' ? `#${member.user.discriminator}` : ''}\` has the left the server\nThey will be added back to the ticket if they rejoin.`,
                 });
                 channel.send({ embeds: [existTM] });
+            }
+        }
+
+        // If logging is enabled, send an embed to the set channel
+        const logging = await Logging.findOne({ GuildId: member.guild.id });
+        if (logging) {
+            // Fetch the logging channel
+            const channel = member.guild?.channels.cache.get(logging.ChannelId) ?? await member.guild?.channels.fetch(logging.ChannelId);
+
+            // Check if the channel exists, is a text channel, and has the necessary permissions to send messages
+            if (channel && channel.type === ChannelType.GuildText
+                && channel.permissionsFor(channel.guild.members.me!).has(PermissionsBitField.Flags.SendMessages)) {
+                // Create an embed with information about the joined member
+                const embed = new EmbedBuilder()
+                    .setColor('#FE4611')
+                    .setThumbnail(member.user.displayAvatarURL())
+                    .setAuthor({
+                        name: 'Member Left',
+                        iconURL: `${member.user.displayAvatarURL()}`,
+                    })
+                    .setDescription(
+                        `${member} - \`@${member.user.tag}${member.user.discriminator !== '0' ? `#${member.user.discriminator}` : ''}\``,
+                    )
+                    .setFooter({ text: `ID: ${member.user.id}` })
+                    .setTimestamp();
+
+                // Send the embed to the logging channel
+                if (channel) channel.send({ embeds: [embed] });
             }
         }
     }
