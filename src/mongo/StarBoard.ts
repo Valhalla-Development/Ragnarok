0514a7ded13d8189af2be1e67c9924e35c5b8d83
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2024-04-03 19:48:02 +0100

    ✨ (StarBoard.ts): add a new file `StarBoard.ts` to define the schema for storing the state of the StarBoard module in the database. The schema includes fields for `GuildId` and `ChannelId`.

---
+++ src/mongo/StarBoard.ts
@@ -0,0 +1,11 @@
+import { model, Schema } from 'mongoose';
+
+/**
+ * Represents a schema for storing the state of the StarBoard module
+ */
+const StarBoard = new Schema({
+    GuildId: { type: String, unique: true },
+    ChannelId: { type: String, default: null },
+});
+
+export default model('StarBoard', StarBoard, 'StarBoard');
