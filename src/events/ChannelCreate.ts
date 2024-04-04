e0ea400ba5649e62fa48cde788343e0cb5ffb3c0
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2024-04-04 15:39:17 +0100

    ✨ (ChannelCreate.ts): Add ChannelCreate event handler to send logging information to a specified channel when a new channel is created in Discord server.

---
+++ src/events/ChannelCreate.ts
@@ -0,0 +1,45 @@
+import { ArgsOf, Discord, On } from 'discordx';
+import { ChannelType, EmbedBuilder, PermissionsBitField } from 'discord.js';
+import Logging from '../mongo/Logging.js';
+
+/**
+ * Discord.js ChannelCreate event handler.
+ */
+@Discord()
+export class ChannelCreate {
+    /**
+     * Executes when the ChannelCreate event is emitted.
+     * @param channel
+     * @param client - The Discord client.
+     * @returns void
+     */
+    @On({ event: 'channelCreate' })
+    async onChannelCreate([channel]: ArgsOf<'channelCreate'>) {
+        // If logging is enabled, send an embed to the set channel
+        const logging = await Logging.findOne({ GuildId: channel.guild.id });
+        if (logging) {
+            // Fetch the logging channel
+            const chn = channel.guild?.channels.cache.get(logging.ChannelId) ?? await channel.guild?.channels.fetch(logging.ChannelId);
+
+            // Check if the channel exists, is a text channel, and has the necessary permissions to send messages
+            if (chn && chn.type === ChannelType.GuildText
+                && chn.permissionsFor(chn.guild.members.me!).has(PermissionsBitField.Flags.SendMessages)) {
+                const channelType = channel.type === ChannelType.GuildVoice || channel.type === ChannelType.GuildStageVoice ? 'Voice' : 'Text';
+
+                // Create an embed with information about the joined member
+                const embed = new EmbedBuilder()
+                    .setColor('#FE4611')
+                    .setAuthor({
+                        name: `${channelType} Channel Created`,
+                        iconURL: `${channel.guild.iconURL()}`,
+                    })
+                    .setTitle(`${channel} - \`${channelType === 'Text' ? '#' : ''}${channel.name}\``)
+                    .setFooter({ text: `ID: ${channel.id}` })
+                    .setTimestamp();
+
+                // Send the embed to the logging channel
+                if (channel) chn.send({ embeds: [embed] });
+            }
+        }
+    }
+}
