4c668f8d05267919502074963f82f913d0f6e4fb
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2024-04-04 16:49:22 +0100

    ✨ (ChannelUpdate.ts): Add ChannelUpdate event handler to monitor and log changes in Discord channels.

---
+++ src/events/ChannelUpdate.ts
@@ -0,0 +1,82 @@
+import { ArgsOf, Discord, On } from 'discordx';
+import { ChannelType, EmbedBuilder, PermissionsBitField } from 'discord.js';
+import Logging from '../mongo/Logging.js';
+
+/**
+ * Discord.js ChannelUpdate event handler.
+ */
+@Discord()
+export class ChannelUpdate {
+    /**
+     * Executes when the ChannelUpdate event is emitted.
+     * @param oldChannel
+     * @param newChannel
+     * @param client - The Discord client.
+     * @returns void
+     */
+    @On({ event: 'channelUpdate' })
+    async onChannelUpdate([oldChannel, newChannel]: ArgsOf<'channelUpdate'>) {
+        if (oldChannel.type === ChannelType.DM || newChannel.type === ChannelType.DM) return;
+
+        // If logging is enabled, send an embed to the set channel
+        const logging = await Logging.findOne({ GuildId: oldChannel.guild.id });
+        if (logging) {
+            // Fetch the logging channel
+            const chn = oldChannel.guild?.channels.cache.get(logging.ChannelId) ?? await oldChannel.guild?.channels.fetch(logging.ChannelId);
+
+            // Check if the channel exists, is a text channel, and has the necessary permissions to send messages
+            if (chn && chn.type === ChannelType.GuildText
+                && chn.permissionsFor(chn.guild.members.me!).has(PermissionsBitField.Flags.SendMessages)) {
+                const channelChanges: string[] = [];
+
+                const channelType = oldChannel.type === ChannelType.GuildVoice || oldChannel.type === ChannelType.GuildStageVoice ? 'Voice' : (oldChannel.type === ChannelType.GuildCategory ? 'Category' : 'Text');
+
+                if (oldChannel.name !== newChannel.name) {
+                    channelChanges.push(
+                        oldChannel.type === ChannelType.GuildCategory
+                            ? `**${channelType} Name Updated**\nOld:\n\`${oldChannel.name}\`\nNew:\n\`${newChannel.name}\`\n`
+                            : oldChannel.type === ChannelType.GuildVoice
+                                ? `**${channelType} Name Updated**\nOld:\n\`${oldChannel.name}\`\nNew:\n${newChannel} - \`${newChannel.name}\`\n`
+                                : `**${channelType} Name Updated**\nOld:\n\`#${oldChannel.name}\`\nNew:\n${newChannel} - \`#${newChannel.name}\`\n`,
+                    );
+                }
+
+                if (oldChannel.type === ChannelType.GuildText
+                    && newChannel.type === ChannelType.GuildText
+                    && oldChannel.nsfw !== newChannel.nsfw) {
+                    const oldNs = oldChannel.nsfw ? 'Enabled' : 'Disabled';
+                    const newNs = newChannel.nsfw ? 'Enabled' : 'Disabled';
+
+                    channelChanges.push(`**${channelType} NSFW Status Updated**\nOld:\n\`${oldNs}\`\nNew:\n\`${newNs}\`\n`);
+                }
+
+                if (oldChannel.type === ChannelType.GuildText
+                    && newChannel.type === ChannelType.GuildText
+                    && oldChannel.topic !== newChannel.topic) {
+                    if (newChannel.topic) {
+                        const oldTopic = oldChannel.topic === '' ? 'None' : `${oldChannel.topic}`;
+                        const newTopic = newChannel.topic === '' ? 'None' : `${newChannel.topic}`;
+
+                        channelChanges.push(`**${channelType} Topic Updated**\nOld:\n\`${oldTopic}\`\nNew:\n\`${newTopic}\`\n`);
+                    }
+                }
+
+                if (channelChanges.length) {
+                    // Create an embed with information about the joined member
+                    const embed = new EmbedBuilder()
+                        .setColor('#FE4611')
+                        .setAuthor({
+                            name: `${channelType} Channel Updated`,
+                            iconURL: `${oldChannel.guild.iconURL()}`,
+                        })
+                        .setDescription(channelChanges.join('\n'))
+                        .setFooter({ text: `ID: ${newChannel.id}` })
+                        .setTimestamp();
+
+                    // Send the embed to the logging channel
+                    if (chn) chn.send({ embeds: [embed] });
+                }
+            }
+        }
+    }
+}
