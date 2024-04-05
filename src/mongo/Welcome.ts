41571925c52eb2a4437d0d2516aa232fbcd2b1fe
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2024-04-05 16:16:57 +0100

    ✨ (mongo/Welcome.ts): introduce a new schema for storing Welcome data within a guild using Mongoose model and Schema

---
+++ src/mongo/Welcome.ts
@@ -0,0 +1,12 @@
+import { model, Schema } from 'mongoose';
+
+/**
+ * Represents a schema for storing the data for Welcome within a guild.
+ */
+const Welcome = new Schema({
+    GuildId: { type: String, unique: true },
+    ChannelId: { type: String, default: null },
+    Image: { type: String, default: null },
+});
+
+export default model('Welcome', Welcome, 'Welcome');
