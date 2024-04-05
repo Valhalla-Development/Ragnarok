e631ed10b551ae1da6cad354adb5ea5035f6d5fd
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2024-04-05 16:01:30 +0100

    ✨ (GuildBanRemove.ts): Add GuildBanRemove event handler to send an embed to a logging channel when a member is unbanned from a guild.

---
+++ src/events/GuildBanRemove.ts
@@ -0,0 +1,49 @@
+import { ArgsOf, Discord, On } from 'discordx';
+import {
+    AuditLogEvent, ChannelType, EmbedBuilder, PermissionsBitField,
+} from 'discord.js';
+import Logging from '../mongo/Logging.js';
+
+/**
+ * Discord.js GuildBanRemove event handler.
+ */
+@Discord()
+export class GuildBanRemove {
+    /**
+     * Executes when the GuildBanRemove event is emitted.
+     * @param ban
+     * @param client - The Discord client.
+     * @returns void
+     */
+    @On({ event: 'guildBanRemove' })
+    async onGuildBanRemove([ban]: ArgsOf<'guildBanRemove'>) {
+        // If logging is enabled, send an embed to the set channel
+        const logging = await Logging.findOne({ GuildId: ban.guild.id });
+        if (logging) {
+            // Fetch the logging channel
+            const channel = ban.guild?.channels.cache.get(logging.ChannelId) ?? await ban.guild?.channels.fetch(logging.ChannelId);
+
+            const entry = await ban.guild.fetchAuditLogs({ type: AuditLogEvent.MemberBanRemove }).then((audit) => audit.entries.first());
+
+            // Check if the channel exists, is a text channel, and has the necessary permissions to send messages
+            if (channel && channel.type === ChannelType.GuildText
+                && channel.permissionsFor(channel.guild.members.me!).has(PermissionsBitField.Flags.SendMessages)) {
+                // Create an embed with information about the unbanned member
+                const embed = new EmbedBuilder()
+                    .setColor('#FE4611')
+                    .setAuthor({ name: 'Member Unbanned', iconURL: ban.user.displayAvatarURL() })
+                    .setThumbnail(ban.user.displayAvatarURL())
+                    .setDescription(`${ban.user} - \`@${ban.user.tag}${ban.user.discriminator !== '0' ? `#${ban.user.discriminator}` : ''}\``)
+                    .setFooter({ text: `ID: ${ban.user.id}` })
+                    .setTimestamp();
+
+                if (entry && entry.executor) {
+                    embed.addFields({ name: 'Moderator', value: `${entry.executor}` });
+                }
+
+                // Send the embed to the logging channel
+                if (channel) channel.send({ embeds: [embed] });
+            }
+        }
+    }
+}
