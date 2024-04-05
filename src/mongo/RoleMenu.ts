b18b214a44771e63c9b64bc3062da3eec31481e2
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2024-04-05 16:16:39 +0100

    ✨ (RoleMenu.ts): introduce a new schema for storing RoleMenu data within a guild using Mongoose model and Schema.

---
+++ src/mongo/RoleMenu.ts
@@ -0,0 +1,15 @@
+import { model, Schema } from 'mongoose';
+
+/**
+ * Represents a schema for storing the data for the RoleMenu within a guild.
+ */
+const RoleMenu = new Schema({
+    GuildId: { type: String, unique: true },
+    RoleMenuId: {
+        channel: { type: String, default: null },
+        message: { type: String, default: null },
+    },
+    RoleList: { type: Array, default: [] },
+});
+
+export default model('RoleMenu', RoleMenu, 'RoleMenu');
