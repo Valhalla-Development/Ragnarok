44610e2c9770625081b6ed1431eb876ad3acdee0
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2024-04-09 23:52:45 +0100

    ✨ (Welcome.ts): add Image field with default value of null to the Welcome schema in order to store an image URL for welcome messages.

--- src/mongo/Welcome.ts
+++ src/mongo/Welcome.ts
@@ -6,6 +6,7 @@ import { model, Schema } from 'mongoose';
 const Welcome = new Schema({
     GuildId: { type: String, unique: true },
     ChannelId: { type: String, default: null },
+    Image: { type: String, default: null },
 });
 
 export default model('Welcome', Welcome, 'Welcome');
