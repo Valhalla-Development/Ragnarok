17578af2aedcaf3067630bd5b21ff93af5ce8e65
Ragnar Lothbrok <ragnarlothbrokjr@proton.me>
2025-12-16 21:10:23 +0000

    🔧 (Config.ts): Update numeric values in the economy configuration for better readability and consistency.

--- src/utils/economy/Config.ts
+++ src/utils/economy/Config.ts
@@ -10,8 +10,8 @@ export const ecoPrices = {
         hourly: { min: 150, max: 400 },
         daily: { min: 400, max: 800 },
         weekly: { min: 2000, max: 3000 },
-        monthly: { min: 10000, max: 15000 },
-        newUserTime: 172800000, // 2 Days
+        monthly: { min: 10_000, max: 15_000 },
+        newUserTime: 172_800_000, // 2 Days
     },
 
     // Fishing
@@ -20,18 +20,18 @@ export const ecoPrices = {
             fishBagFirst: 200,
             fishBagLimit: 1500,
             fishBagPrice: 800, // Upgrade adds 30 to capacity
-            fishingRod: 25000,
+            fishingRod: 25_000,
         },
         rewards: {
-            treasure: 75000,
+            treasure: 75_000,
             pufferfish: 5000,
             swordfish: 2500,
             kingSalmon: 1000,
             trout: 400,
         },
         cooldowns: {
-            fishWinTime: 240000, // 4 Minutes
-            fishFailTime: 480000, // 8 Minutes
+            fishWinTime: 240_000, // 4 Minutes
+            fishFailTime: 480_000, // 8 Minutes
         },
     },
 
@@ -41,35 +41,35 @@ export const ecoPrices = {
             farmPlotFirst: 150,
             farmPlotLimit: 1200,
             farmPlotPrice: 800, // Upgrade adds 30 to capacity
-            farmingTools: 20000,
+            farmingTools: 20_000,
             farmBagFirst: 150,
-            farmBagLimit: 12000,
+            farmBagLimit: 12_000,
             farmBagPrice: 800, // Upgrade adds 30 to capacity
         },
         rewards: {
-            goldBar: 30000,
+            goldBar: 30_000,
             corn: 800,
             wheat: 600,
             potatoes: 500,
             tomatoes: 450,
         },
         plantingTimes: {
-            cornPlant: 480000, // 8 minutes
-            wheatPlant: 360000, // 6 minutes
-            potatoPlant: 180000, // 3 minutes
-            tomatoPlant: 120000, // 2 minutes
+            cornPlant: 480_000, // 8 minutes
+            wheatPlant: 360_000, // 6 minutes
+            potatoPlant: 180_000, // 3 minutes
+            tomatoPlant: 120_000, // 2 minutes
         },
         decayRate: 0.015, // Lowered decay rate for better long-term value
         farmingWithoutTools: {
-            goldNugget: 15000,
+            goldNugget: 15_000,
             barley: 600,
             spinach: 400,
             strawberries: 200,
             lettuce: 150,
         },
         cooldowns: {
-            farmWinTime: 240000, // 4 Minutes
-            farmFailTime: 480000, // 8 Minutes
+            farmWinTime: 240_000, // 4 Minutes
+            farmFailTime: 480_000, // 8 Minutes
         },
         freeFarmLimit: 10,
     },
