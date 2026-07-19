e0192a3b79f9b9abd705969e9fdea9e5c4cc8c65
Ragnar Lothbrok <ragnarlothbrokjr@proton.me>
2026-07-19 17:42:25 +0100

    ✨ (Honeypot.ts): introduce new fields ActionCount, Mode, and WarningMessageId to the Honeypot schema.

--- src/mongo/Honeypot.ts
+++ src/mongo/Honeypot.ts
@@ -4,8 +4,11 @@ import { model, Schema } from 'mongoose';
  * Represents a schema for storing the Honeypot trap channel data within a guild.
  */
 const Honeypot = new Schema({
+    ActionCount: { default: 0, type: Number },
     ChannelId: { default: null, type: String },
     GuildId: { type: String, unique: true },
+    Mode: { default: 'ban', type: String },
+    WarningMessageId: { default: null, type: String },
 });
 
 export default model('Honeypot', Honeypot, 'Honeypot');
